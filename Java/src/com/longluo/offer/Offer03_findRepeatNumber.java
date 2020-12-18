1c7e52e36b08fa25456322c7dfc5d4a6e6baa725
longluo <calmbody@163.com>
2020-12-18 17:34:29 +0800

    Offer 03 Use Set and HashMap to solve this problem.

---
+++ Java/src/com/longluo/offer/Offer03_findRepeatNumber.java
@@ -0,0 +1,40 @@
+package com.longluo.offer;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 剑指 Offer 03. 数组中重复的数字
+ * 找出数组中重复的数字。
+ * 在一个长度为 n 的数组nums里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，
+ * 也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * <p>
+ * 示例 1：
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ * <p>
+ * 限制：
+ * 2 <= n <= 100000
+ */
+public class Offer03_findRepeatNumber {
+
+    public static int findRepeatNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            } else {
+                set.add(num);
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 3 ?= " + findRepeatNumber(new int[]{2, 3, 1, 0, 2, 5, 3}));
+    }
+}
