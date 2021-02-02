39824e2ee9db6a8c54702eac179065c0dfd0d6b3
longluo <calmbody@163.com>
2021-02-02 23:53:41 +0800

    Problem 424 Slide Window

---
+++ Java/src/com/longluo/leetcode/slidewindow/Problem424_CharacterReplacement.java
@@ -0,0 +1,51 @@
+package com.longluo.leetcode.slidewindow;
+
+/**
+ * 424. 替换后的最长重复字符
+ * 给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换k次。
+ * 在执行上述操作后，找到包含重复字母的最长子串的长度。
+ * 注意：字符串长度 和 k 不会超过 104。
+ * <p>
+ * <p>
+ * 示例 1：
+ * 输入：s = "ABAB", k = 2
+ * 输出：4
+ * 解释：用两个'A'替换为两个'B',反之亦然。
+ * <p>
+ * 示例 2：
+ * 输入：s = "AABABBA", k = 1
+ * 输出：4
+ * <p>
+ * 解释：
+ * 将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+ * 子串 "BBBB" 有最长重复字母, 答案为 4。
+ */
+public class Problem424_CharacterReplacement {
+
+    public static int characterReplacement(String s, int k) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int[] num = new int[26];
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s.charAt(right) - 'A']++;
+            maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s.charAt(left) - 'A']--;
+                left++;
+            }
+            right++;
+        }
+
+        return right - left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("4 ?= " + characterReplacement("ABAB", 2));
+        System.out.println("4 ?= " + characterReplacement("AABABBA", 1));
+    }
+}
