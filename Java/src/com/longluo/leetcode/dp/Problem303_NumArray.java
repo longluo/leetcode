840851d9be1374f2652a6ac13320499c3cb76473
longluo <calmbody@163.com>
2021-03-01 09:42:47 +0800

    Problem 303 DP

---
+++ Java/src/com/longluo/leetcode/dp/Problem303_NumArray.java
@@ -0,0 +1,82 @@
+package com.longluo.leetcode.dp;
+
+/**
+ * 303. 区域和检索 - 数组不可变
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ * 实现 NumArray 类：
+ * <p>
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，
+ * 包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ * <p>
+ * 示例：
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ * <p>
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ * <p>
+ * 提示：
+ * 0 <= nums.length <= 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * 0 <= i <= j < nums.length
+ * 最多调用 10^4 次 sumRange 方法
+ */
+public class Problem303_NumArray {
+
+    static class NumArray {
+        int[] arr;
+        int[] dp;
+
+        public NumArray(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return;
+            }
+            arr = new int[nums.length];
+            System.arraycopy(nums, 0, arr, 0, nums.length);
+            dp = new int[nums.length];
+            dp[0] = arr[0];
+            for (int i = 1; i < nums.length; i++) {
+                dp[i] = dp[i - 1] + nums[i];
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            int total = 0;
+            for (int idx = i; idx <= j; idx++) {
+                total += arr[idx];
+            }
+
+            return total;
+        }
+
+        public int sumRange_dp(int i, int j) {
+            return dp[j] - dp[i] + arr[i];
+        }
+    }
+
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray obj = new NumArray(nums);
+     * int param_1 = obj.sumRange(i,j);
+     */
+
+    public static void main(String[] args) {
+        int[] testArr = new int[]{-2, 0, 3, -5, 2, -1};
+        NumArray numArray = new NumArray(testArr);
+
+        System.out.println("1 ?= " + numArray.sumRange(0, 2));
+        System.out.println("-1 ?= " + numArray.sumRange(2, 5));
+        System.out.println("-3 ?= " + numArray.sumRange(0, 5));
+
+        System.out.println("1 ?= " + numArray.sumRange_dp(0, 2));
+        System.out.println("-1 ?= " + numArray.sumRange_dp(2, 5));
+        System.out.println("-3 ?= " + numArray.sumRange_dp(0, 5));
+    }
+}
