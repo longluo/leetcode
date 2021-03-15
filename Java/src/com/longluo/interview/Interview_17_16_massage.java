e290982d899039118753e7d2fba9ab028f4dbe29
longluo <calmbody@163.com>
2021-03-15 15:44:08 +0800

    Interview 17 16

---
+++ Java/src/com/longluo/interview/Interview_17_16_massage.java
@@ -0,0 +1,53 @@
+package com.longluo.interview;
+
+/**
+ * 面试题 17.16. 按摩师
+ * <p>
+ * 一个有名的按摩师会收到源源不断的预约请求，每个预约都可以选择接或不接。在每次预约服务之间要有休息时间，
+ * 因此她不能接受相邻的预约。
+ * <p>
+ * 给定一个预约请求序列，替按摩师找到最优的预约集合（总预约时间最长），返回总的分钟数。
+ * 注意：本题相对原题稍作改动
+ * <p>
+ * 示例 1：
+ * 输入： [1,2,3,1]
+ * 输出： 4
+ * 解释： 选择 1 号预约和 3 号预约，总时长 = 1 + 3 = 4。
+ * <p>
+ * 示例 2：
+ * 输入： [2,7,9,3,1]
+ * 输出： 12
+ * 解释： 选择 1 号预约、 3 号预约和 5 号预约，总时长 = 2 + 9 + 1 = 12。
+ * <p>
+ * 示例 3：
+ * 输入： [2,1,4,5,3,1,1,3]
+ * 输出： 12
+ * 解释： 选择 1 号预约、 3 号预约、 5 号预约和 8 号预约，总时长 = 2 + 4 + 3 + 3 = 12。
+ */
+public class Interview_17_16_massage {
+
+    public static int massage(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int len = nums.length;
+        int[] dp = new int[len + 1];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+
+        return dp[len - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("4 ?= " + massage(new int[]{1, 2, 3, 1}));
+        System.out.println("12 ?= " + massage(new int[]{2, 7, 9, 3, 1}));
+        System.out.println("12 ?= " + massage(new int[]{2, 1, 4, 5, 3, 1, 1, 3}));
+    }
+}
