8725273bffb636d2de74d1e3810d60f142ee6a3d
longluo <calmbody@163.com>
2021-03-17 14:52:24 +0800

    Problem 115 Dp

---
+++ Java/src/com/longluo/leetcode/dp/Problem115_numDistinct.java
@@ -0,0 +1,78 @@
+package com.longluo.leetcode.dp;
+
+/**
+ * 115. 不同的子序列
+ * 给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。
+ * 字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。
+ * （例如，"ACE" 是 "ABCDE" 的一个子序列，而 "AEC" 不是）
+ * 题目数据保证答案符合 32 位带符号整数范围。
+ * <p>
+ * 示例 1：
+ * 输入：s = "rabbbit", t = "rabbit"
+ * 输出：3
+ * 解释：
+ * 如下图所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+ * (上箭头符号 ^ 表示选取的字母)
+ * rabbbit
+ * ^^^^ ^^
+ * rabbbit
+ * ^^ ^^^^
+ * rabbbit
+ * ^^^ ^^^
+ * <p>
+ * 示例 2：
+ * 输入：s = "babgbag", t = "bag"
+ * 输出：5
+ * 解释：
+ * 如下图所示, 有 5 种可以从 s 中得到 "bag" 的方案。
+ * (上箭头符号 ^ 表示选取的字母)
+ * babgbag
+ * ^^ ^
+ * babgbag
+ * ^^    ^
+ * babgbag
+ * ^    ^^
+ * babgbag
+ * ^  ^^
+ * babgbag
+ * ^^^
+ * <p>
+ * 提示：
+ * 0 <= s.length, t.length <= 1000
+ * s 和 t 由英文字母组成
+ */
+public class Problem115_numDistinct {
+
+    public static int numDistinct(String s, String t) {
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 || t.length() > s.length()) {
+            return 0;
+        }
+
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("3 ?= " + numDistinct("rabbbit", "rabbit"));
+        System.out.println("5 ?= " + numDistinct("babgbag", "bag"));
+    }
+}
