9a361f84b46ca1e23841aede558cb060d0cda55c
longluo <calmbody@163.com>
2021-03-31 10:53:12 +0800

    Problem 90 backtrace

--- Java/src/com/longluo/datastructure/ArrayUtils.java
+++ Java/src/com/longluo/datastructure/ArrayUtils.java
@@ -1,24 +1,82 @@
 package com.longluo.datastructure;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
- *  Array Utils To process The Array
+ * Array Utils To process The Array
  */
 public class ArrayUtils {
 
+    /**
+     * Print the array.
+     */
     public static String print2DArray(int[][] arr) {
         int row = arr.length;
 
         StringBuilder sb = new StringBuilder();
+        sb.append("[");
         for (int i = 0; i < row; i++) {
             sb.append(Arrays.toString(arr[i]));
             if (i < row - 1) {
                 sb.append(",");
             }
         }
-
+        sb.append("]");
         return sb.toString();
     }
 
+    /**
+     * Print the list.
+     */
+    public static String print2DList(List<List<Integer>> input) {
+        if (input == null || input.size() == 0) {
+            return "[]";
+        }
+
+        StringBuilder sb = new StringBuilder(input.size());
+        sb.append("[");
+        for (int i = 0; i < input.size(); i++) {
+            sb.append("[");
+            List<Integer> currList = input.get(i);
+            for (int j = 0; j < currList.size(); j++) {
+                sb.append(currList.get(j));
+                if (j < currList.size() - 1) {
+                    sb.append(",");
+                }
+            }
+            sb.append("]");
+            if (i < input.size() - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("print2DArray -->");
+        int[][] tstArr1 = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
+        System.out.println("[[1, 2, 3],[8, 9, 4],[7, 6, 5]] ?= " + print2DArray(tstArr1));
+
+        System.out.println("print2DList -->");
+        List<List<Integer>> tstList1 = new ArrayList<>();
+        tstList1.add(new ArrayList<>() {{
+            add(1);
+            add(2);
+            add(3);
+        }});
+        tstList1.add(new ArrayList<>() {{
+            add(4);
+            add(5);
+            add(6);
+        }});
+        tstList1.add(new ArrayList<>() {{
+            add(7);
+            add(8);
+            add(9);
+        }});
+        System.out.println(" [[1,2,3][4,5,6][7,8,9]] ?= " + print2DList(tstList1));
+    }
 }
---
+++ Java/src/com/longluo/leetcode/backtrace/Problem90_subsets_ii.java
@@ -0,0 +1,55 @@
+package com.longluo.leetcode.backtrace;
+
+import com.longluo.datastructure.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 90. 子集 II
+ * 给你一个整数数组 nums，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+ * 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,2,2]
+ * 输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * <p>
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ */
+public class Problem90_subsets_ii {
+
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        backtrace(ans, new ArrayList<>(), nums, 0);
+        return ans;
+    }
+
+    public static void backtrace(List<List<Integer>> ans, List<Integer> oneList, int[] numbers, int index) {
+        if (index == numbers.length) {
+            ans.add(new ArrayList<>(oneList));
+            return;
+        }
+
+        oneList.add(numbers[index]);
+        backtrace(ans, oneList, numbers, index + 1);
+        oneList.remove(oneList.size() - 1);
+        backtrace(ans, oneList, numbers, index + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[[],[1],[1,2],[1,2,2],[2],[2,2]] ?= " + ArrayUtils.print2DList(subsetsWithDup(new int[]{1, 2, 2})));
+        System.out.println("[[],[0]] ?= " + ArrayUtils.print2DList(subsetsWithDup(new int[]{0})));
+    }
+}
