56e965aa3eb7498ab09274d50b69ec229e50e874
longluo <calmbody@163.com>
2021-05-03 08:26:53 +0800

    Problem 7 reverse Integer

---
+++ Java/src/com/longluo/leetcode/math/Problem7_reverseInteger.java
@@ -0,0 +1,64 @@
+package com.longluo.leetcode.math;
+
+/**
+ * 7. 整数反转
+ * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * 如果反转后整数超过 32 位的有符号整数的范围 [−2^31,  2^31 − 1] ，就返回 0。
+ * 假设环境不允许存储 64 位整数（有符号或无符号）。
+ * <p>
+ * 示例 1：
+ * 输入：x = 123
+ * 输出：321
+ * <p>
+ * 示例 2：
+ * 输入：x = -123
+ * 输出：-321
+ * <p>
+ * 示例 3：
+ * 输入：x = 120
+ * 输出：21
+ * <p>
+ * 示例 4：
+ * 输入：x = 0
+ * 输出：0
+ * <p>
+ * 提示：
+ * -2^31 <= x <= 2^31 - 1
+ * <p>
+ * https://leetcode-cn.com/problems/reverse-integer/
+ */
+public class Problem7_reverseInteger {
+
+    public static int reverse(int x) {
+        boolean flag = false;
+        if (x < 0) {
+            flag = true;
+            x = -x;
+        }
+
+        int ans = 0;
+        while (x > 0) {
+            int temp = x % 10;
+            if (ans > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            ans = ans * 10 + temp;
+            x /= 10;
+        }
+
+        if (flag) {
+            ans = -ans;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("321 ?= " + reverse(123));
+        System.out.println("-321 ?= " + reverse(-123));
+        System.out.println("21 ?= " + reverse(120));
+        System.out.println("0 ?= " + reverse(0));
+        System.out.println("0 ?= " + reverse(1534236469));
+        System.out.println("Max= " + Integer.MAX_VALUE + ", Min=" + Integer.MIN_VALUE);
+    }
+}
