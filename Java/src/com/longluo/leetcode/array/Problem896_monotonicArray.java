38add1d6987712dba045f38878f8b45ab38de462
longluo <calmbody@163.com>
2021-05-06 09:34:17 +0800

    Problem 896 rename file.

--- Java/src/com/longluo/leetcode/array/Problem896_monotonicArray.java
+++ Java/src/com/longluo/leetcode/array/Problem896_monotonicArray.java
@@ -2,6 +2,7 @@ package com.longluo.leetcode.array;
 
 /**
  * 896. 单调数列
+ * <p>
  * 如果数组是单调递增或单调递减的，那么它是单调的。
  * 如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
  * 当给定的数组 A 是单调数组时返回 true，否则返回 false。
@@ -29,8 +30,10 @@ package com.longluo.leetcode.array;
  * 提示：
  * 1 <= A.length <= 50000
  * -100000 <= A[i] <= 100000
+ * <p>
+ * https://leetcode-cn.com/problems/monotonic-array/
  */
-public class Problem896_isMonotonic {
+public class Problem896_monotonicArray {
 
     public static boolean isMonotonic(int[] A) {
         if (A == null || A.length == 0) {
