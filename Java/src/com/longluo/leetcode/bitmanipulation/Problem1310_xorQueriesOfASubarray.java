6b90e4e271ff5a5c0adb8fe88f2d393678db479b
longluo <calmbody@163.com>
2021-05-13 10:43:13 +0800

    Problem 1310 XOR

--- Java/src/com/longluo/leetcode/bitmanipulation/Problem1310_xorQueriesOfASubarray.java
+++ Java/src/com/longluo/leetcode/bitmanipulation/Problem1310_xorQueriesOfASubarray.java
@@ -1,5 +1,7 @@
 package com.longluo.leetcode.bitmanipulation;
 
+import java.util.Arrays;
+
 /**
  * 1310. 子数组异或查询
  * <p>
@@ -8,7 +10,7 @@ package com.longluo.leetcode.bitmanipulation;
  * 并返回一个包含给定查询 queries 所有结果的数组。
  * <p>
  * 示例 1：
- * 输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+ * 输入：arr = [1,3,4,8], queries = {{0,1},{1,2},{0,3},{3,3}}
  * 输出：[2,7,14,8]
  * 解释：
  * 数组中元素的二进制表示形式是：
@@ -23,7 +25,7 @@ package com.longluo.leetcode.bitmanipulation;
  * [3,3] = 8
  * <p>
  * 示例 2：
- * 输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+ * 输入：arr = [4,8,2,10], queries = {{2,3},{1,3},{0,0},{0,3}}
  * 输出：[8,0,4,4]
  * <p>
  * 提示：
@@ -49,7 +51,7 @@ public class Problem1310_xorQueriesOfASubarray {
             int right = queries[i][1];
             if (left < right) {
                 int temp = arr[left];
-                for (int j = left + 1; j < right; j++) {
+                for (int j = left + 1; j <= right; j++) {
                     temp ^= arr[j];
                 }
                 ans[i] = temp;
@@ -61,7 +63,28 @@ public class Problem1310_xorQueriesOfASubarray {
         return ans;
     }
 
-    public static void main(String[] args) {
+    public static int[] xorQueries_2(int[] arr, int[][] queries) {
+        if (arr == null || arr.length == 0 || queries == null || queries.length == 0) {
+            return new int[]{};
+        }
 
+        int[] xor = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            xor[i + 1] = xor[i] ^ arr[i];
+        }
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            ans[i] = xor[queries[i][0]] ^ xor[queries[i][1] + 1];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[2,7,14,8] ?= " + Arrays.toString(xorQueries(new int[]{1, 3, 4, 8}, new int[][]{{0, 1}, {1, 2}, {0, 3}, {3, 3}})));
+        System.out.println("[2,7,14,8] ?= " + Arrays.toString(xorQueries_2(new int[]{1, 3, 4, 8}, new int[][]{{0, 1}, {1, 2}, {0, 3}, {3, 3}})));
+        System.out.println("[8,0,4,4] ?= " + Arrays.toString(xorQueries(new int[]{4, 8, 2, 10}, new int[][]{{2, 3}, {1, 3}, {0, 0}, {0, 3}})));
+        System.out.println("[8,0,4,4] ?= " + Arrays.toString(xorQueries_2(new int[]{4, 8, 2, 10}, new int[][]{{2, 3}, {1, 3}, {0, 0}, {0, 3}})));
     }
 }
