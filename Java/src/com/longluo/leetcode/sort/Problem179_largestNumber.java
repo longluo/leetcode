a2a6d9cb8f3cecb80e27cd7730cb05eb422d68fe
longluo <calmbody@163.com>
2021-05-13 12:14:50 +0800

    Problem 179

--- Java/src/com/longluo/leetcode/sort/Problem179_largestNumber.java
+++ Java/src/com/longluo/leetcode/sort/Problem179_largestNumber.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 
 /**
  * 179. 最大数
+ * <p>
  * 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
  * 注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
  * <p>
@@ -27,6 +28,8 @@ import java.util.Comparator;
  * 提示：
  * 1 <= nums.length <= 100
  * 0 <= nums[i] <= 10^9
+ * <p>
+ * https://leetcode-cn.com/problems/largest-number/
  */
 public class Problem179_largestNumber {
 
