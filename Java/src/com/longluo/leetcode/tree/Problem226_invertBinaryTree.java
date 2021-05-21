875ff57b2f9abe5d8df0b07dd77968cef01b26ce
longluo <calmbody@163.com>
2021-05-21 08:53:14 +0800

    Problem 226 Invert Binary Tree.

---
+++ Java/src/com/longluo/leetcode/tree/Problem226_invertBinaryTree.java
@@ -0,0 +1,75 @@
+package com.longluo.leetcode.tree;
+
+import com.longluo.datastructure.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 226. 翻转二叉树
+ * <p>
+ * 翻转一棵二叉树。
+ * <p>
+ * 示例：
+ * 输入：
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
+ * 1   3 6   9
+ * 输出：
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ * <p>
+ * https://leetcode-cn.com/problems/invert-binary-tree/
+ */
+public class Problem226_invertBinaryTree {
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static TreeNode invertTree_bfs(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            TreeNode temp = node.right;
+            node.right = node.left;
+            node.left = temp;
+
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
