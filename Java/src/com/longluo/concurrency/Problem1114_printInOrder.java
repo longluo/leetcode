e8ddbf65ff23032b1cdbe58acea0956d43e5725f
longluo <calmbody@163.com>
2021-06-02 20:19:42 +0800

    Problem 1114 MultiThread Add a method.

--- Java/src/com/longluo/concurrency/Problem1114_printInOrder.java
+++ Java/src/com/longluo/concurrency/Problem1114_printInOrder.java
@@ -1,5 +1,7 @@
 package com.longluo.concurrency;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * 1114. 按序打印
  * <p>
@@ -65,6 +67,41 @@ public class Problem1114_printInOrder {
 
         public void third(Runnable printThird) throws InterruptedException {
             while (flag != 3) {
+
+            }
+
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+        }
+    }
+
+    class Foo2 {
+
+        private AtomicInteger firstJobDone = new AtomicInteger(0);
+        private AtomicInteger secondJobDone = new AtomicInteger(0);
+
+        public Foo2() {
+
+        }
+
+        public void first(Runnable printFirst) throws InterruptedException {
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            firstJobDone.incrementAndGet();
+        }
+
+        public void second(Runnable printSecond) throws InterruptedException {
+            while (firstJobDone.get() != 1) {
+
+            }
+
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            secondJobDone.incrementAndGet();
+        }
+
+        public void third(Runnable printThird) throws InterruptedException {
+            while (secondJobDone.get() != 2) {
 
             }
 
