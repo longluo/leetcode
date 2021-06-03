e0968946c29726bff975c67fbc23aed106699fe1
longluo <calmbody@163.com>
2021-06-03 10:34:21 +0800

    Problem 1116

---
+++ Java/src/com/longluo/concurrency/Problem1116_printZeroEvenOdd.java
@@ -0,0 +1,103 @@
+package com.longluo.concurrency;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+/**
+ * 1116. 打印零与奇偶数
+ * <p>
+ * 假设有这么一个类：
+ * <p>
+ * class ZeroEvenOdd {
+ * public ZeroEvenOdd(int n) { ... }      // 构造函数
+ * public void zero(printNumber) { ... }  // 仅打印出 0
+ * public void even(printNumber) { ... }  // 仅打印出 偶数
+ * public void odd(printNumber) { ... }   // 仅打印出 奇数
+ * }
+ * 相同的一个 ZeroEvenOdd 类实例将会传递给三个不同的线程：
+ * <p>
+ * 线程 A 将调用 zero()，它只输出 0 。
+ * 线程 B 将调用 even()，它只输出偶数。
+ * 线程 C 将调用 odd()，它只输出奇数。
+ * 每个线程都有一个 printNumber 方法来输出一个整数。请修改给出的代码以输出整数序列 010203040506... ，其中序列的长度必须为 2n。
+ * <p>
+ * 示例 1：
+ * 输入：n = 2
+ * 输出："0102"
+ * 说明：三条线程异步执行，其中一个调用 zero()，另一个线程调用 even()，最后一个线程调用odd()。正确的输出为 "0102"。
+ * <p>
+ * 示例 2：
+ * 输入：n = 5
+ * 输出："0102030405"
+ * <p>
+ * https://leetcode-cn.com/problems/print-zero-even-odd/
+ */
+public class Problem1116_printZeroEvenOdd {
+
+    static class ZeroEvenOdd {
+
+        private int n;
+
+        private Semaphore zero = new Semaphore(1);
+        private Semaphore even = new Semaphore(0);
+        private Semaphore odd = new Semaphore(0);
+
+        public ZeroEvenOdd(int n) {
+            this.n = n;
+        }
+
+        // printNumber.accept(x) outputs "x", where x is an integer.
+        public void zero(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i++) {
+                zero.acquire();
+                printNumber.accept(0);
+                if (i % 2 == 0) {
+                    even.release();
+                } else {
+                    odd.release();
+                }
+            }
+        }
+
+        public void even(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 2; i <= n; i += 2) {
+                even.acquire();
+                printNumber.accept(i);
+                zero.release();
+            }
+        }
+
+        public void odd(IntConsumer printNumber) throws InterruptedException {
+            for (int i = 1; i <= n; i += 2) {
+                odd.acquire();
+                printNumber.accept(i);
+                zero.release();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(6);
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.zero(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.even(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.odd(System.out::print);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
