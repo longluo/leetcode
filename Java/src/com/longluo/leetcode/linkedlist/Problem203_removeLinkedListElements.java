d49e77f32ed50e1ebeb1ac7441e1e3445960363c
longluo <calmbody@163.com>
2021-06-05 16:47:01 +0800

    Problem 203

--- Java/src/com/longluo/leetcode/linkedlist/Problem203_removeLinkedListElements.java
+++ Java/src/com/longluo/leetcode/linkedlist/Problem203_removeLinkedListElements.java
@@ -1,10 +1,11 @@
 package com.longluo.leetcode.linkedlist;
 
 import com.longluo.datastructure.ListNode;
-import com.longluo.datastructure.Utils;
+import com.longluo.datastructure.LinkedListNodeUtils;
 
 /**
  * 203. 移除链表元素
+ * <p>
  * 给你一个链表的头节点 head 和一个整数 val，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
  * <p>
  * 示例 1：
@@ -24,6 +25,8 @@ import com.longluo.datastructure.Utils;
  * 列表中的节点在范围[0, 10^4]内
  * 1 <= Node.val <= 50
  * 0 <= k <= 50
+ * <p>
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/
  */
 public class Problem203_removeLinkedListElements {
 
@@ -51,14 +54,30 @@ public class Problem203_removeLinkedListElements {
         return sentinel.next;
     }
 
+    public static ListNode removeElements_rec(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+
+        head.next = removeElements(head.next, val);
+        if (head.val == val) {
+            return head.next;
+        } else {
+            return head;
+        }
+    }
+
     public static void main(String[] args) {
-        ListNode test1 = Utils.makeListNode(new int[]{1, 2, 6, 3, 4, 5, 6});
-        System.out.println("[1,2,3,4,5] ?= " + Utils.printLinkedList(removeElements(test1, 6)));
+        ListNode test1 = LinkedListNodeUtils.constructListNode(new int[]{1, 2, 6, 3, 4, 5, 6});
+        System.out.println("[1,2,3,4,5] ?= " + LinkedListNodeUtils.printLinkedList(removeElements(test1, 6)));
+        System.out.println("[1,2,3,4,5] ?= " + LinkedListNodeUtils.printLinkedList(removeElements_rec(test1, 6)));
 
-        ListNode test2 = Utils.makeListNode(new int[]{});
-        System.out.println("[] ?= " + Utils.printLinkedList(removeElements(test2, 1)));
+        ListNode test2 = LinkedListNodeUtils.constructListNode(new int[]{});
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(removeElements(test2, 1)));
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(removeElements_rec(test2, 1)));
 
-        ListNode test3 = Utils.makeListNode(new int[]{7, 7, 7, 7});
-        System.out.println("[] ?= " + Utils.printLinkedList(removeElements(test3, 7)));
+        ListNode test3 = LinkedListNodeUtils.constructListNode(new int[]{7, 7, 7, 7});
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(removeElements(test3, 7)));
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(removeElements_rec(test3, 7)));
     }
 }
