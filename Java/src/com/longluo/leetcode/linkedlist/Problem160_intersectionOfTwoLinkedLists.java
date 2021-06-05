5ae8a6f4108af15450df3736916e25baea0303ed
longluo <calmbody@163.com>
2021-06-05 16:21:43 +0800

    Problem 160

---
+++ Java/src/com/longluo/leetcode/linkedlist/Problem160_intersectionOfTwoLinkedLists.java
@@ -0,0 +1,109 @@
+package com.longluo.leetcode.linkedlist;
+
+import com.longluo.datastructure.LinkedListNodeUtils;
+import com.longluo.datastructure.ListNode;
+
+import java.util.*;
+
+/**
+ * 160. 相交链表
+ * <p>
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+ * 图示两个链表在节点 c1 开始相交：
+ * <p>
+ * 题目数据 保证 整个链式结构中不存在环。
+ * <p>
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>
+ * 示例 1：
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * <p>
+ * 示例 2：
+ * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Intersected at '2'
+ * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。
+ * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ * <p>
+ * 示例 3：
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+ * 由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 这两个链表不相交，因此返回 null 。
+ * <p>
+ * 提示：
+ * listA 中节点数目为 m
+ * listB 中节点数目为 n
+ * 0 <= m, n <= 3 * 10^4
+ * 1 <= Node.val <= 10^5
+ * 0 <= skipA <= m
+ * 0 <= skipB <= n
+ * 如果 listA 和 listB 没有交点，intersectVal 为 0
+ * 如果 listA 和 listB 有交点，intersectVal == listA[skipA + 1] == listB[skipB + 1]
+ * <p>
+ * 进阶：你能否设计一个时间复杂度 O(n) 、仅用 O(1) 内存的解决方案？
+ * <p>
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ */
+public class Problem160_intersectionOfTwoLinkedLists {
+
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pHeadA = headA;
+        ListNode pHeadB = headB;
+
+        while (pHeadA != pHeadB) {
+            if (pHeadA == null) {
+                pHeadA = headB;
+            } else {
+                pHeadA = pHeadA.next;
+            }
+
+            if (pHeadB == null) {
+                pHeadB = headA;
+            } else {
+                pHeadB = pHeadB.next;
+            }
+        }
+
+        return pHeadA;
+    }
+
+    public static ListNode getIntersectionNode_2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        Set<ListNode> nodeSet = new HashSet<>();
+        ListNode temp = headA;
+        while (temp != null) {
+            nodeSet.add(temp);
+            temp = temp.next;
+        }
+
+        temp = headB;
+        while (temp != null) {
+            if (nodeSet.contains(temp)) {
+                return temp;
+            }
+
+            temp = temp.next;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode tstNode1 = LinkedListNodeUtils.constructListNode(new int[]{4, 1, 8, 4, 5});
+        ListNode tstNode2 = LinkedListNodeUtils.constructListNode(new int[]{5, 0, 1, 8, 4, 5});
+        System.out.println("8 ?= " + getIntersectionNode(tstNode1, tstNode2).val);
+    }
+}
