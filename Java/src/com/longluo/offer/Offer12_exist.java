d4f54f8d6b19be4d1490052b5d3eb918131bcf37
longluo <calmbody@163.com>
2021-06-23 16:19:01 +0800

    Offer 12

--- Java/src/com/longluo/offer/Offer12_exist.java
+++ Java/src/com/longluo/offer/Offer12_exist.java
@@ -2,6 +2,7 @@ package com.longluo.offer;
 
 /**
  * 剑指 Offer 12. 矩阵中的路径
+ * <p>
  * 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一格开始，
  * 每一步可以在矩阵中向左、右、上、下移动一格。如果一条路径经过了矩阵的某一格，那么该路径不能再次进入该格子。
  * 例如，在下面的3×4的矩阵中包含一条字符串“bfce”的路径（路径中的字母用加粗标出）。
@@ -23,15 +24,78 @@ package com.longluo.offer;
  * 提示：
  * 1 <= board.length <= 200
  * 1 <= board[i].length <= 200
+ * <p>
+ * https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/
  */
 public class Offer12_exist {
 
     public static boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+
+        int len = word.length();
+        int row = board.length;
+        int col = board[0].length;
+        if (row * col < len) {
+            return false;
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(board, word, 0, i, j)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean dfs(char[][] board, String target, int level, int x, int y) {
+        if (level > target.length()) {
+            return false;
+        }
+
+        if (level == target.length()) {
+            return true;
+        }
+
+        int row = board.length;
+        int col = board[0].length;
+
+        if (x < 0 || x >= row || y < 0 || y >= col) {
+            return false;
+        }
+
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        if (target.charAt(level) != board[x][y]) {
+            return false;
+        }
+
+        char temp = board[x][y];
+        board[x][y] = '/';
+
+        for (int i = 0; i < dirs.length; i++) {
+            int nextX = x + dirs[i][0];
+            int nextY = y + dirs[i][1];
+
+            if (dfs(board, target, level + 1, nextX, nextY)) {
+                return true;
+            }
+        }
+
+        board[x][y] = temp;
 
         return false;
     }
 
     public static void main(String[] args) {
-
+        System.out.println("true ?= " + exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCCED"));
+        System.out.println("false ?= " + exist(new char[][]{{'a', 'b'}, {'c', 'd'}}, "abcd"));
+        System.out.println("true ?= " + exist(new char[][]{{'C', 'A', 'A'}, {'A', 'A', 'A'}, {'B', 'C', 'D'}}, "AAB"));
     }
 }
---
+++ Java/src/com/longluo/offer/README.md
@@ -0,0 +1,16 @@
+# The LeetCode Offer Solutions
+
+- Java
+
+[《剑指Offer》刷题记录](http://www.longluo.me/blog/2020/12/20/Coding-Interviews/)
+
+-------------------
+
+|   Leetcode题目   |     难度     |          解答          |   Source Code   |
+|    :-----        |    :----:    |         :----:         |      :----:     |
+| [剑指Offer 03. 数组中重复的数字](https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/) | Easy | []()  | [Java](https://github.com/longluo/leetcode/blob/master/Java/src/com/longluo/offer/Offer03_findRepeatNumber.java) |
+| [剑指 Offer 12. 矩阵中的路径](https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof/) | Medium | []()  | [Java](https://github.com/longluo/leetcode/tree/master/Java/src/com/longluo/offer/Offer12_exist.java) |
+| [剑指 Offer 15. 二进制中1的个数](https://leetcode-cn.com/problems/er-jin-zhi-zhong-1de-ge-shu-lcof/) | Easy | []()  | [Java](https://github.com/longluo/leetcode/tree/master/Java/src/com/longluo/offer/Offer15_erJinZhiZhong1DeGeShuLcof.java) |
+
+欢迎光临[我的个人网站](http://www.longluo.me)
+
