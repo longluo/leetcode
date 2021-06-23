befc2c9d22ac9c8ad9ddd28518904746910b50b9
longluo <calmbody@163.com>
2021-06-23 09:12:23 +0800

    Offer 15

--- Java/src/com/longluo/offer/Offer15_erJinZhiZhong1DeGeShuLcof.java
+++ Java/src/com/longluo/offer/Offer15_erJinZhiZhong1DeGeShuLcof.java
@@ -41,8 +41,20 @@ public class Offer15_erJinZhiZhong1DeGeShuLcof {
         return count;
     }
 
+    public static int hammingWeight_2(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         System.out.println("2 ?= " + hammingWeight(9));
+        System.out.println("2 ?= " + hammingWeight_2(9));
         System.out.println("1 ?= " + hammingWeight(1));
+        System.out.println("1 ?= " + hammingWeight_2(1));
     }
 }
