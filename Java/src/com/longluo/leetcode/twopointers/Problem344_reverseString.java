f9fcce4072db792f5e46946921bd5163fe429363
longluo <calmbody@163.com>
2021-08-20 10:51:24 +0800

    Problem 344

---
+++ Java/src/com/longluo/leetcode/twopointers/Problem344_reverseString.java
@@ -0,0 +1,56 @@
+package com.longluo.leetcode.twopointers;
+
+/**
+ * 344. 反转字符串
+ * <p>
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ * <p>
+ * 示例 1：
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ * <p>
+ * 示例 2：
+ * 输入：["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ * <p>
+ * https://leetcode-cn.com/problems/reverse-string/
+ */
+public class Problem344_reverseString {
+
+    public static void reverseString(char[] s) {
+        if (s == null || s.length <= 1) {
+            return;
+        }
+
+        int n = s.length;
+        int left = 0;
+        int right = n - 1;
+        while (left < right) {
+            char temp = s[right];
+            s[right] = s[left];
+            s[left] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void reverseString_bl(char[] s) {
+        if (s == null || s.length <= 1) {
+            return;
+        }
+
+        int n = s.length;
+        for (int i = 0; i < n / 2; i++) {
+            char temp = s[i];
+            s[i] = s[n - 1 - i];
+            s[n - 1 - i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(" " + reverseString(new char[]{'h', 'e', 'l', 'l', 'o'}););
+//        System.out.println(" " + reverseString(new char[]{'H', 'a', 'n', 'n', 'a', 'h'}););
+    }
+}
