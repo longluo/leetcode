7010ebf6d720fad25f5e3bb3afa8213d25f2299f
longluo <calmbody@163.com>
2021-09-04 11:18:15 +0800

    剑指Offer 10-II. 青蛙跳台阶问题

--- Java/src/com/longluo/offer/Offer10_numberWays.java
+++ Java/src/com/longluo/offer/Offer10_numberWays.java
@@ -2,8 +2,8 @@ package com.longluo.offer;
 
 /**
  * 剑指 Offer 10-II. 青蛙跳台阶问题
+ *
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
- * <p>
  * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
  * <p>
  * 示例 1：
@@ -20,6 +20,8 @@ package com.longluo.offer;
  * <p>
  * 提示：
  * 0 <= n <= 100
+ *
+ *https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/
  */
 public class Offer10_numberWays {
 
