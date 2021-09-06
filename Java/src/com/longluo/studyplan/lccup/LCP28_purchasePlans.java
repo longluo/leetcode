751d8c6998b23e91a015bad85c4de18323a98e23
longluo <calmbody@163.com>
2021-09-06 23:30:27 +0800

    LCP 28 AC

--- Java/src/com/longluo/studyplan/lccup/LCP28_purchasePlans.java
+++ Java/src/com/longluo/studyplan/lccup/LCP28_purchasePlans.java
@@ -53,8 +53,122 @@ public class LCP28_purchasePlans {
         return ans;
     }
 
+    public static int purchasePlans_1(int[] nums, int target) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        int MOD = 1000000007;
+        int ans = 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            while (right > left && nums[left] + nums[right] > target) {
+                right--;
+            }
+
+            for (int i = left; i < right; i++) {
+                if (nums[i] + nums[right] <= target) {
+                    ans = ans % MOD + 1;
+                }
+            }
+
+            right--;
+        }
+
+        return ans;
+    }
+
+    public static int purchasePlans_2(int[] nums, int target) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        int MOD = 1000000007;
+        int ans = 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) {
+                right = binarySearch(nums, left, right, target - nums[left]);
+            }
+
+            for (int i = left; i < right; i++) {
+                if (nums[i] + nums[right] <= target) {
+                    ans = ans % MOD + 1;
+                }
+            }
+
+            right--;
+        }
+
+        return ans;
+    }
+
+    public static int purchasePlans_3(int[] nums, int target) {
+        if (nums == null || nums.length <= 1) {
+            return 0;
+        }
+
+        int MOD = 1000000007;
+        int ans = 0;
+        Arrays.sort(nums);
+        int len = nums.length;
+        for (int i = len - 1; i > 0; i--) {
+            int remain = target - nums[i];
+            if (remain < nums[0]) {
+                continue;
+            }
+
+            int left = 0;
+            int right = i - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > remain) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans = ans % MOD + right + 1;
+        }
+
+        return ans;
+    }
+
+    public static int binarySearch(int[] arr, int low, int high, int target) {
+        if (low >= high) {
+            return -1;
+        }
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return high;
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + purchasePlans(new int[]{2, 5, 3, 5}, 6));
+        System.out.println("1 ?= " + purchasePlans_1(new int[]{2, 5, 3, 5}, 6));
         System.out.println("4 ?= " + purchasePlans(new int[]{2, 2, 1, 9}, 10));
+        System.out.println("4 ?= " + purchasePlans_1(new int[]{2, 2, 1, 9}, 10));
+        System.out.println("4 ?= " + purchasePlans_2(new int[]{2, 2, 1, 9}, 10));
+        System.out.println("4 ?= " + purchasePlans_3(new int[]{2, 2, 1, 9}, 10));
+        System.out.println("1 ?= " + purchasePlans_3(new int[]{2, 5, 3, 5}, 6));
+        System.out.println("5 ?= " + purchasePlans_3(new int[]{5, 3, 1, 2}, 7));
+        System.out.println("0 ?= " + purchasePlans_1(new int[]{61055, 35718, 71455, 34429, 97039, 63942, 37037, 88911}, 17209));
+        System.out.println("0 ?= " + purchasePlans_2(new int[]{61055, 35718, 71455, 34429, 97039, 63942, 37037, 88911}, 17209));
+        System.out.println("0 ?= " + purchasePlans_3(new int[]{61055, 35718, 71455, 34429, 97039, 63942, 37037, 88911}, 17209));
     }
 }
