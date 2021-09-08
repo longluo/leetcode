c46f389982beb19f0b2a18fcabfdd509b1a448b3
longluo <calmbody@163.com>
2021-09-08 23:48:16 +0800

    Problem 206 add url

---
+++ Java/src/com/longluo/leetcode/linkedlist/Problem206_reverseLinkedList.java
@@ -0,0 +1,68 @@
+package com.longluo.leetcode.linkedlist;
+
+import com.longluo.datastructure.LinkedListNodeUtils;
+import com.longluo.datastructure.ListNode;
+
+/**
+ * 206. 反转链表
+ * <p>
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * <p>
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ * <p>
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ */
+public class Problem206_reverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode test1 = LinkedListNodeUtils.constructListNode(new int[]{});
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(reverseList(test1)));
+        System.out.println("[] ?= " + LinkedListNodeUtils.printLinkedList(reverseList2(test1)));
+
+        ListNode test2 = LinkedListNodeUtils.constructListNode(new int[]{1});
+        System.out.println("[1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList(test2)));
+        System.out.println("[1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList2(test2)));
+
+        ListNode test3 = LinkedListNodeUtils.constructListNode(new int[]{1, 2});
+        System.out.println("[2,1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList(test3)));
+        System.out.println("[2,1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList2(test3)));
+
+        ListNode test4 = LinkedListNodeUtils.constructListNode(new int[]{1, 2, 3});
+        System.out.println("[3,2,1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList(test4)));
+        System.out.println("[3,2,1] ?= " + LinkedListNodeUtils.printLinkedList(reverseList2(test4)));
+    }
+}
