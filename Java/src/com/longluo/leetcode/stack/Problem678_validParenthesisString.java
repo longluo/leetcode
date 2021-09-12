c8d3c6ba38e366eaa4d3aa6cc8b5ba38ad9d230d
longluo <calmbody@163.com>
2021-09-12 22:29:30 +0800

    Problem 678

--- Java/src/com/longluo/leetcode/stack/Problem678_validParenthesisString.java
+++ Java/src/com/longluo/leetcode/stack/Problem678_validParenthesisString.java
@@ -36,11 +36,30 @@ public class Problem678_validParenthesisString {
         }
 
         int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '*') {
+                dp[i][i] = true;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            char c1 = s.charAt(i - 1), c2 = s.charAt(i);
+            dp[i - 1][i] = (c1 == '(' || c1 == '*') && (c2 == ')' || c2 == '*');
+        }
+        for (int i = n - 3; i >= 0; i--) {
+            char c1 = s.charAt(i);
+            for (int j = i + 2; j < n; j++) {
+                char c2 = s.charAt(j);
+                if ((c1 == '(' || c1 == '*') && (c2 == ')' || c2 == '*')) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+                for (int k = i; k < j && !dp[i][j]; k++) {
+                    dp[i][j] = dp[i][k] && dp[k + 1][j];
+                }
+            }
+        }
 
-
-
-
-        return false;
+        return dp[0][n - 1];
     }
 
     public static void main(String[] args) {
