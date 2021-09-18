b1a0d1a9df250beecb06e63253e9e8dd6b3bc610
longluo <calmbody@163.com>
2021-09-18 20:26:36 +0800

    Offer 54

---
+++ Java/src/com/longluo/offer/Offer54_kthLargest.java
@@ -0,0 +1,69 @@
+package com.longluo.offer;
+
+import com.longluo.datastructure.TreeNode;
+import com.longluo.datastructure.TreeUtils;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 剑指 Offer 54. 二叉搜索树的第k大节点
+ * <p>
+ * 给定一棵二叉搜索树，请找出其中第k大的节点。
+ * <p>
+ * 示例 1:
+ * 输入: root = [3,1,4,null,2], k = 1
+ * 3
+ * / \
+ * 1   4
+ * \
+ * 2
+ * 输出: 4
+ * <p>
+ * 示例 2:
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * 输出: 4
+ * <p>
+ * 限制：
+ * 1 ≤ k ≤ 二叉搜索树元素个数
+ * <p>
+ * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+ */
+public class Offer54_kthLargest {
+
+    public static List<Integer> numList = new LinkedList<>();
+
+    public static int kthLargest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+
+        getNumList(root);
+        Collections.sort(numList);
+        int size = numList.size();
+        return numList.get(size - k);
+    }
+
+    public static void getNumList(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        numList.add(root.val);
+        getNumList(root.left);
+        getNumList(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode tstNode1 = TreeUtils.constructTree(new Integer[]{3, 1, 4, null, 2});
+        System.out.println("4 ?= " + kthLargest(tstNode1, 1));
+    }
+}
