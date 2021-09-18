36759fd0ab79479ed9e2b7a1d33d752bb11fde15
longluo <calmbody@163.com>
2021-09-18 20:41:32 +0800

    Offer 54 dfs 100%

--- Java/src/com/longluo/offer/Offer54_kthLargest.java
+++ Java/src/com/longluo/offer/Offer54_kthLargest.java
@@ -82,8 +82,37 @@ public class Offer54_kthLargest {
         getNumList(root.right);
     }
 
+    public static int ans = 0;
+    public static int kThVal = 0;
+
+    public static int kthLargest_3(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+
+        kThVal = k;
+        dfs(root);
+        return ans;
+    }
+
+    public static void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        dfs(root.right);
+        kThVal--;
+        if (kThVal == 0) {
+            ans = root.val;
+            return;
+        }
+        dfs(root.left);
+    }
+
     public static void main(String[] args) {
         TreeNode tstNode1 = TreeUtils.constructTree(new Integer[]{3, 1, 4, null, 2});
         System.out.println("4 ?= " + kthLargest(tstNode1, 1));
+        System.out.println("4 ?= " + kthLargest_2(tstNode1, 1));
+        System.out.println("4 ?= " + kthLargest_3(tstNode1, 1));
     }
 }
