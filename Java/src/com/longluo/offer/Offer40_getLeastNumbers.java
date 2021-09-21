798bdfe625c1e45e736ded5228709b5e24593f48
longluo <calmbody@163.com>
2021-09-21 22:12:07 +0800

    Offer 40 Max Heap

--- Java/src/com/longluo/offer/Offer40_getLeastNumbers.java
+++ Java/src/com/longluo/offer/Offer40_getLeastNumbers.java
@@ -74,9 +74,45 @@ public class Offer40_getLeastNumbers {
         return ans;
     }
 
+    public static int[] getLeastNumbers_2(int[] arr, int k) {
+        if (arr == null || arr.length == 0 || k == 0) {
+            return new int[]{};
+        }
+
+        if (k >= arr.length) {
+            return arr;
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < k; i++) {
+            priorityQueue.offer(arr[i]);
+        }
+
+        for (int i = k; i < arr.length; i++) {
+            if (arr[i] < priorityQueue.peek()) {
+                priorityQueue.poll();
+                priorityQueue.offer(arr[i]);
+            }
+        }
+
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = priorityQueue.poll();
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("[1, 2] ?= " + Arrays.toString(getLeastNumbers(new int[]{3, 2, 1}, 2)));
         System.out.println("[1, 2] ?= " + Arrays.toString(getLeastNumbers_1(new int[]{3, 2, 1}, 2)));
+        System.out.println("[1, 2] ?= " + Arrays.toString(getLeastNumbers_2(new int[]{3, 2, 1}, 2)));
         System.out.println("[0] ?= " + Arrays.toString(getLeastNumbers(new int[]{0, 1, 2, 1}, 1)));
         System.out.println("[0] ?= " + Arrays.toString(getLeastNumbers_1(new int[]{0, 1, 2, 1}, 1)));
     }
