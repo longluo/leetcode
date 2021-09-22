679b445c731f080cd5dc2856e99b986d04e90713
longluo <calmbody@163.com>
2021-09-22 12:55:39 +0800

    Problem 725 Answer

--- Java/src/com/longluo/leetcode/linkedlist/Problem725_splitListToParts.java
+++ Java/src/com/longluo/leetcode/linkedlist/Problem725_splitListToParts.java
@@ -114,6 +114,31 @@ public class Problem725_splitListToParts {
         return ans;
     }
 
+    public ListNode[] splitListToParts_1(ListNode head, int k) {
+        int n = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        int quotient = n / k, remainder = n % k;
+
+        ListNode[] parts = new ListNode[k];
+        ListNode curr = head;
+        for (int i = 0; i < k && curr != null; i++) {
+            parts[i] = curr;
+            int partSize = quotient + (i < remainder ? 1 : 0);
+            for (int j = 1; j < partSize; j++) {
+                curr = curr.next;
+            }
+            ListNode next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+
+        return parts;
+    }
+
     public static void main(String[] args) {
         ListNode tstNode1 = LinkedListNodeUtils.constructListNode(new int[]{1, 2, 3});
         System.out.println("[] ?= " + splitListToParts(tstNode1, 5));
