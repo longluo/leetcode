a0e6187f68b8e25fbccd971ff26db7446bcb3ffd
longluo <calmbody@163.com>
2021-09-25 09:33:53 +0800

    Offer 05

--- Java/src/com/longluo/offer/Offer05_replaceSpace.java
+++ Java/src/com/longluo/offer/Offer05_replaceSpace.java
@@ -2,6 +2,7 @@ package com.longluo.offer;
 
 /**
  * 剑指 Offer 05. 替换空格
+ * <p>
  * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
  * <p>
  * 示例 1：
@@ -10,6 +11,8 @@ package com.longluo.offer;
  * <p>
  * 限制：
  * 0 <= s的长度 <= 10000
+ * <p>
+ * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
  */
 public class Offer05_replaceSpace {
 
@@ -17,7 +20,48 @@ public class Offer05_replaceSpace {
         return s.replaceAll("\\ ", "%20");
     }
 
+    public static String replaceSpace_1(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        int n = s.length();
+        StringBuilder sb = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != ' ') {
+                sb.append(s.charAt(i));
+            } else {
+                sb.append("%20");
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String replaceSpace_2(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+
+        int n = s.length();
+        char[] array = new char[n * 3];
+        int size = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) != ' ') {
+                array[size++] = s.charAt(i);
+            } else {
+                array[size++] = '%';
+                array[size++] = '2';
+                array[size++] = '0';
+            }
+        }
+
+        return new String(array, 0, size);
+    }
+
     public static void main(String[] args) {
         System.out.println("We%20are%20happy. ?= " + replaceSpace("We are happy."));
+        System.out.println("We%20are%20happy. ?= " + replaceSpace_1("We are happy."));
+        System.out.println("We%20are%20happy. ?= " + replaceSpace_2("We are happy."));
     }
 }
