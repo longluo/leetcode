3c43d4140fe17641ef21ff773b60697fb0567240
longluo <calmbody@163.com>
2021-10-17 16:07:09 +0800

    Problem 4 BF

---
+++ Java/src/com/longluo/top_interviews/Problem4_findMedianSortedArrays.java
@@ -0,0 +1,102 @@
+package com.longluo.top_interviews;
+
+/**
+ * 4. 寻找两个正序数组的中位数
+ * <p>
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * <p>
+ * 示例 1：
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * <p>
+ * 示例 2：
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * <p>
+ * 示例 3：
+ * 输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ * <p>
+ * 示例 4：
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ * <p>
+ * 示例 5：
+ * 输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ * <p>
+ * 提示：
+ * nums1.length == m
+ * nums2.length == n
+ * 0 <= m <= 1000
+ * 0 <= n <= 1000
+ * 1 <= m + n <= 2000
+ * -10^6 <= nums1[i], nums2[i] <= 10^6
+ * <p>
+ * 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+ * <p>
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+ */
+public class Problem4_findMedianSortedArrays {
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] nums = new int[m + n];
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+        }
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("1.00000 ?= " + findMedianSortedArrays(new int[]{}, new int[]{1}));
+        System.out.println("2.00000 ?= " + findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
+        System.out.println("2.50000 ?= " + findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}));
+        System.out.println("0.00000 ?= " + findMedianSortedArrays(new int[]{0, 0}, new int[]{0, 0}));
+        System.out.println("2.00000 ?= " + findMedianSortedArrays(new int[]{2}, new int[]{}));
+        System.out.println("0.00000 ?= " + findMedianSortedArrays(new int[]{0, 0, 0, 0, 0}, new int[]{-1, 0, 0, 0, 0, 0, 1}));
+    }
+}
