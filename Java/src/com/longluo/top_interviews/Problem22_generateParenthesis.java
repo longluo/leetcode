3abd53fb9827ff3520bcba17b7c869d85fc557c7
longluo <calmbody@163.com>
2021-10-18 15:32:30 +0800

    Problem 22 BF

--- Java/src/com/longluo/top_interviews/Problem22_generateParenthesis.java
+++ Java/src/com/longluo/top_interviews/Problem22_generateParenthesis.java
@@ -53,8 +53,47 @@ public class Problem22_generateParenthesis {
         sb.deleteCharAt(sb.length() - 1);
     }
 
+    public static List<String> generateParenthesis_bf(int n) {
+        if (n < 1) {
+            return new ArrayList<>();
+        }
+
+        List<String> ans = new ArrayList<>();
+        generateAll(new char[2 * n], 0, ans);
+        return ans;
+    }
+
+    public static void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current)) {
+                result.add(new String(current));
+            }
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos + 1, result);
+            current[pos] = ')';
+            generateAll(current, pos + 1, result);
+        }
+    }
+
+    public static boolean valid(char[] current) {
+        int balance = 0;
+        for (char c : current) {
+            if (c == '(') {
+                ++balance;
+            } else {
+                --balance;
+            }
+            if (balance < 0) {
+                return false;
+            }
+        }
+        return balance == 0;
+    }
+
     public static void main(String[] args) {
         System.out.println("[()] ?= " + generateParenthesis(1));
         System.out.println("[((())), (()()), (())(), ()(()), ()()()] ?= " + generateParenthesis(3));
+        System.out.println("[((())), (()()), (())(), ()(()), ()()()] ?= " + generateParenthesis_bf(3));
     }
 }
