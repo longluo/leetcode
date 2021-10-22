9b3d157b9d27deca12cd2edce1284e2db67eb781
longluo <calmbody@163.com>
2021-10-22 10:20:45 +0800

    Problem 229 Poll

--- Java/src/com/longluo/leetcode/array/Problem229_majorityElement.java
+++ Java/src/com/longluo/leetcode/array/Problem229_majorityElement.java
@@ -1,9 +1,6 @@
 package com.longluo.leetcode.array;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 229. 求众数 II
@@ -54,9 +51,78 @@ public class Problem229_majorityElement {
         return ans;
     }
 
+    public static List<Integer> majorityElement_cnt(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return ans;
+        }
+
+        int len = nums.length;
+        int min = Arrays.stream(nums).min().getAsInt();
+        int max = Arrays.stream(nums).max().getAsInt();
+        int[] freq = new int[max - min + 1];
+        for (int x : nums) {
+            freq[x - min]++;
+        }
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] > len / 3) {
+                ans.add(i + min);
+            }
+        }
+
+        return ans;
+    }
+
+    public static List<Integer> majorityElement_vote(int[] nums) {
+        int element1 = 0;
+        int element2 = 0;
+        int vote1 = 0;
+        int vote2 = 0;
+        for (int x : nums) {
+            if (vote1 > 0 && element1 == x) {
+                vote1++;
+            } else if (vote2 > 0 && element2 == x) {
+                vote2++;
+            } else if (vote1 == 0) {
+                element1 = x;
+                vote1++;
+            } else if (vote2 == 0) {
+                element2 = x;
+                vote2++;
+            } else {
+                vote1--;
+                vote2--;
+            }
+        }
+
+        int cnt1 = 0;
+        int cnt2 = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int x : nums) {
+            if (vote1 > 0 && element1 == x) {
+                cnt1++;
+            } else if (vote2 > 0 && element2 == x) {
+                cnt2++;
+            }
+        }
+
+        if (vote1 > 0 && cnt1 > nums.length / 3) {
+            ans.add(element1);
+        }
+        if (vote2 > 0 && cnt2 > nums.length / 3) {
+            ans.add(element2);
+        }
+
+//        System.out.println(" " + element1 + " " + cnt1);
+//        System.out.println(" " + element2 + " " + cnt2);
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("[3] ?= " + majorityElement(new int[]{3, 2, 3}));
         System.out.println("[1] ?= " + majorityElement(new int[]{1}));
         System.out.println("[1, 2] ?= " + majorityElement(new int[]{1, 1, 1, 3, 3, 2, 2, 2}));
+        System.out.println("[1, 2] ?= " + majorityElement_cnt(new int[]{1, 1, 1, 3, 3, 2, 2, 2}));
+        System.out.println("[1, 2] ?= " + majorityElement_vote(new int[]{1, 1, 1, 3, 3, 2, 2, 2}));
     }
 }
