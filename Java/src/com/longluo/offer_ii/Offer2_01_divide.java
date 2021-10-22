3ccdaea8ebdf5af1e35f332c159cdb66b54b91c2
longluo <calmbody@163.com>
2021-10-22 10:30:49 +0800

    offer 2 01 divide

--- Java/src/com/longluo/offer_ii/Offer2_01_divide.java
+++ Java/src/com/longluo/offer_ii/Offer2_01_divide.java
@@ -71,8 +71,81 @@ public class Offer2_01_divide {
         return ans;
     }
 
+    public static int divide_quick(int a, int b) {
+        if (a == 0) {
+            return 0;
+        }
+
+        if (a == Integer.MIN_VALUE) {
+            if (b == -1) {
+                return Integer.MAX_VALUE;
+            }
+            if (b == Integer.MIN_VALUE) {
+                return 1;
+            }
+        }
+
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (a > 0) {
+            a = -a;
+            rev = !rev;
+        }
+        if (b > 0) {
+            b = -b;
+            rev = !rev;
+        }
+
+        int left = 1, right = Integer.MAX_VALUE, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            boolean check = quickAdd(a, mid, b);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == Integer.MAX_VALUE) {
+                    break;
+                }
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
+    // 快速乘
+    public static boolean quickAdd(int y, int z, int x) {
+        // x 和 y 是负数，z 是正数
+        // 需要判断 z * y >= x 是否成立
+        int result = 0, add = y;
+        while (z != 0) {
+            if ((z & 1) != 0) {
+                // 需要保证 result + add >= x
+                if (result < x - add) {
+                    return false;
+                }
+                result += add;
+            }
+            if (z != 1) {
+                // 需要保证 add + add >= x
+                if (add < x - add) {
+                    return false;
+                }
+                add += add;
+            }
+            // 不能使用除法
+            z >>= 1;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("7 ?= " + divide(15, 2));
+        System.out.println("7 ?= " + divide_quick(15, 2));
         System.out.println("-2 ?= " + divide(7, -3));
         System.out.println("0 ?= " + divide(0, 1));
         System.out.println("1 ?= " + divide(1, 1));
