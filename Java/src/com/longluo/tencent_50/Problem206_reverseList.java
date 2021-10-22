e3bfd2a74011bbec3ea36bf026471b00da578ae8
longluo <calmbody@163.com>
2021-10-22 15:17:50 +0800

    Problem 206 Iteration Opt

--- Java/src/com/longluo/tencent_50/Problem206_reverseList.java
+++ Java/src/com/longluo/tencent_50/Problem206_reverseList.java
@@ -55,8 +55,32 @@ public class Problem206_reverseList {
         return res;
     }
 
+    public static ListNode reverseList_opt(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static ListNode reverseList_rec(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+
+
+        return head;
+    }
+
     public static void main(String[] args) {
         ListNode tst1 = LinkedListNodeUtils.constructListNode(new int[]{1, 2, 3, 4, 5});
-        LinkedListNodeUtils.printLinkedList(reverseList(tst1));
+//        System.out.println(LinkedListNodeUtils.printLinkedList(reverseList(tst1)));
+        System.out.println(LinkedListNodeUtils.printLinkedList(reverseList_opt(tst1)));
     }
 }
