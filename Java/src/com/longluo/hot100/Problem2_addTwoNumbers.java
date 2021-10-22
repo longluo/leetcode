58ee61b7c09e73c4fe735b5392d6c58e81157d6b
longluo <calmbody@163.com>
2021-10-22 10:29:25 +0800

    tencent hot 50

---
+++ Java/src/com/longluo/hot100/Problem2_addTwoNumbers.java
@@ -0,0 +1,125 @@
+package com.longluo.hot100;
+
+import com.longluo.datastructure.LinkedListNodeUtils;
+import com.longluo.datastructure.ListNode;
+
+/**
+ * 2. 两数相加
+ * <p>
+ * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例 1：
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * <p>
+ * 示例 2：
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ * <p>
+ * 示例 3：
+ * 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * 输出：[8,9,9,9,0,0,0,1]
+ * <p>
+ * 提示：
+ * 每个链表中的节点数在范围 [1, 100] 内
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ * <p>
+ * https://leetcode-cn.com/problems/add-two-numbers/
+ */
+public class Problem2_addTwoNumbers {
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pNode1 = l1;
+        ListNode pNode2 = l2;
+        ListNode dummyNode = new ListNode(-1);
+        ListNode pNode = dummyNode;
+        int carry = 0;
+        while (pNode1 != null && pNode2 != null) {
+            int sum = pNode1.val + pNode2.val + carry;
+            carry = 0;
+            if (sum >= 10) {
+                sum -= 10;
+                carry = 1;
+            }
+            ListNode node = new ListNode(sum);
+            pNode.next = node;
+            pNode = pNode.next;
+
+            pNode1 = pNode1.next;
+            pNode2 = pNode2.next;
+        }
+
+        while (pNode1 != null) {
+            int sum = pNode1.val + carry;
+            carry = 0;
+            if (sum >= 10) {
+                sum -= 10;
+                carry = 1;
+            }
+            ListNode node = new ListNode(sum);
+            pNode.next = node;
+            pNode = pNode.next;
+
+            pNode1 = pNode1.next;
+        }
+
+        while (pNode2 != null) {
+            int sum = pNode2.val + carry;
+            carry = 0;
+            if (sum >= 10) {
+                sum -= 10;
+                carry = 1;
+            }
+            ListNode node = new ListNode(sum);
+            pNode.next = node;
+            pNode = pNode.next;
+            pNode2 = pNode2.next;
+        }
+
+        if (carry > 0) {
+            ListNode node = new ListNode(carry);
+            pNode.next = node;
+        }
+
+        return dummyNode.next;
+    }
+
+    public static ListNode addTwoNumbers_opt(ListNode l1, ListNode l2) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode pNode = dummyNode;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            pNode.next = new ListNode(sum % 10);
+            pNode = pNode.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry > 0) {
+            pNode.next = new ListNode(carry);
+        }
+
+        return dummyNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode tst1 = LinkedListNodeUtils.constructListNode(new int[]{2, 4, 3});
+        ListNode tst2 = LinkedListNodeUtils.constructListNode(new int[]{5, 6, 4});
+        System.out.println("[7, 0, 8] ?= " + LinkedListNodeUtils.printLinkedList(addTwoNumbers(tst1, tst2)));
+        System.out.println("[7, 0, 8] ?= " + LinkedListNodeUtils.printLinkedList(addTwoNumbers_opt(tst1, tst2)));
+    }
+}
--- Java/src/com/longluo/hot100/README.md
+++ Java/src/com/longluo/hot100/README.md
@@ -2,14 +2,17 @@
 
 [LeetCode 热题 HOT 100](https://leetcode-cn.com/problem-list/ex0k24j/)
 
-[腾讯精选练习 50 题](http://www.longluo.me/blog/2021/08/15/Leetcode-Tencent-Interview-Problems/)
+[LeetCode 热题 HOT 100](http://www.longluo.me/blog/2021/08/15/Leetcode-Tencent-Interview-Problems/)
 
 -------------------
 
 |   Leetcode题目   |     难度     |          解答          |   Source Code   |
 |    :-----        |    :----:    |         :----:         |      :----:     |
-| [2. 两数相加](https://leetcode-cn.com/problems/add-two-numbers/) | Easy | []()  | [Java](./Offer2_01_divide.java) |
-|
+| [2. 两数相加](https://leetcode-cn.com/problems/add-two-numbers/) | Medium | [2. 两数相加](http://www.longluo.me/blog/2019/03/25/Leetcode-add-two-numbers/)  | [Java](./Problem2_addTwoNumbers.java) |
+| [4. 寻找两个正序数组的中位数](https://leetcode-cn.com/problems/median-of-two-sorted-arrays/) | Hard | [4. 寻找两个正序数组的中位数](http://www.longluo.me/blog/2021/02/17/Leetcode-median-of-two-sorted-arrays/)  | [Java](./Problem4_findMedianSortedArrays.java) |
+| [155. 最小栈](https://leetcode-cn.com/problems/min-stack/) | Easy | [155. 最小栈](http://www.longluo.me/blog/2021/02/23/Leetcode-min-stack/)  | [Java](./Problem155_minStack.java) |
+| [230. 二叉搜索树中第K小的元素](https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/) | Medium | [230. 二叉搜索树中第K小的元素]()  | [Java](./Problem230_kthSmallestElementInABst.java) |
+
 
 欢迎光临[我的个人网站](http://www.longluo.me)
 
