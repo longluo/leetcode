b94dcc352b89302d1188b9e6c84de7f3083911a7
longluo <calmbody@163.com>
2021-10-22 15:03:09 +0800

    Problem 206 迭代法

---
+++ Java/src/com/longluo/tencent_50/Problem206_reverseList.java
@@ -0,0 +1,62 @@
+package com.longluo.tencent_50;
+
+import com.longluo.datastructure.LinkedListNodeUtils;
+import com.longluo.datastructure.ListNode;
+
+import java.util.Stack;
+
+/**
+ * 206. 反转链表
+ * <p>
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * <p>
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ * <p>
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * <p>
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ */
+public class Problem206_reverseList {
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Stack<ListNode> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head);
+            head = head.next;
+        }
+
+        ListNode pHead = stack.pop();
+        ListNode res = pHead;
+        while (!stack.empty()) {
+            ListNode node = stack.pop();
+            node.next = null;
+            pHead.next = node;
+            pHead = pHead.next;
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ListNode tst1 = LinkedListNodeUtils.constructListNode(new int[]{1, 2, 3, 4, 5});
+        LinkedListNodeUtils.printLinkedList(reverseList(tst1));
+    }
+}
