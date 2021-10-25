81ff3b066685292ee607e08d67bc9ada1f7b5155
longluo <calmbody@163.com>
2021-10-25 17:09:34 +0800

    Problem 240 BF

---
+++ Java/src/com/longluo/leetcode/array/Problem240_searchMatrix.java
@@ -0,0 +1,66 @@
+package com.longluo.leetcode.array;
+
+/**
+ * 240. 搜索二维矩阵 II
+ * <p>
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * <p>
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * <p>
+ * 示例 1：
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * <p>
+ * 示例 2：
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ * <p>
+ * 提示：
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -10^9 <= matrix[i][j] <= 10^9
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -10^9 <= target <= 10^9
+ * <p>
+ * https://leetcode-cn.com/problems/search-a-2d-matrix-ii/
+ */
+public class Problem240_searchMatrix {
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        if (matrix[0][0] > target || matrix[row - 1][col - 1] < target) {
+            return false;
+        }
+
+        for (int i = 0; i < row; i++) {
+            if (matrix[i][0] > target) {
+                break;
+            }
+
+            if (matrix[i][col - 1] < target) {
+                continue;
+            }
+
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
