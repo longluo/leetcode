53e43c85e0e40a8d7c2d327bf9fc887f5e8ab9e0
longluo <calmbody@163.com>
2021-10-29 22:37:02 +0800

    Problem 47

---
+++ Java/src/com/longluo/leetcode/backtracking/Problem47_permutations_ii.java
@@ -0,0 +1,38 @@
+package com.longluo.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 47. 全排列 II
+ * <p>
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：
+ * [[1,1,2],
+ * [1,2,1],
+ * [2,1,1]]
+ * <p>
+ * 示例 2：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 8
+ * -10 <= nums[i] <= 10
+ * <p>
+ * https://leetcode-cn.com/problems/permutations-ii/
+ */
+public class Problem47_permutations_ii {
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+
+        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
