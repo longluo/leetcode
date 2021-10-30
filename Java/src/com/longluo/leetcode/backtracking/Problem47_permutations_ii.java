dee3dae14e6c582d23b3ef10d8ecea0b79a6cc29
longluo <calmbody@163.com>
2021-10-30 15:01:13 +0800

    Problem 47 backtrack

--- Java/src/com/longluo/leetcode/backtracking/Problem47_permutations_ii.java
+++ Java/src/com/longluo/leetcode/backtracking/Problem47_permutations_ii.java
@@ -1,7 +1,6 @@
 package com.longluo.leetcode.backtracking;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 47. 全排列 II
@@ -28,11 +27,36 @@ import java.util.List;
 public class Problem47_permutations_ii {
 
     public static List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+//        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> ans = new HashSet<>();
+        boolean[] visited = new boolean[nums.length];
+        backtrack(ans, new ArrayList<>(), nums, visited, 0);
+        return new ArrayList<>(ans);
+    }
 
-        return new ArrayList<>();
+    public static void backtrack(Set<List<Integer>> res, List<Integer> path, int[] nums, boolean[] visited, int len) {
+        if (len == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                path.add(nums[i]);
+                backtrack(res, path, nums, visited, len + 1);
+                path.remove(path.size() - 1);
+                visited[i] = false;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        permuteUnique(new int[]{0});
+        permuteUnique(new int[]{1, 2});
+        permuteUnique(new int[]{1, 1, 2});
+        permuteUnique(new int[]{1, 1});
+        permuteUnique(new int[]{1, 2, 3});
     }
 }
