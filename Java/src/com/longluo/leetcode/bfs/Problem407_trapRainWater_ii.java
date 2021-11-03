fcd37aec7db9ddb8c46746cc5046dfee72b8a3d3
longluo <calmbody@163.com>
2021-11-03 20:17:21 +0800

    Problem 407

---
+++ Java/src/com/longluo/leetcode/bfs/Problem407_trapRainWater_ii.java
@@ -0,0 +1,86 @@
+package com.longluo.leetcode.bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 407. 接雨水 II
+ * <p>
+ * 给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+ * <p>
+ * 示例 1:
+ * 输入: heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]]
+ * 输出: 4
+ * 解释: 下雨后，雨水将会被上图蓝色的方块中。总的接雨水量为1+2+1=4。
+ * <p>
+ * 示例 2:
+ * 输入: heightMap = [[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]
+ * 输出: 10
+ * <p>
+ * 提示:
+ * m == heightMap.length
+ * n == heightMap[i].length
+ * 1 <= m, n <= 200
+ * 0 <= heightMap[i][j] <= 2 * 10^4
+ * <p>
+ * https://leetcode-cn.com/problems/trapping-rain-water-ii/
+ */
+public class Problem407_trapRainWater_ii {
+
+    public static int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        int maxHeight = 0;
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                maxHeight = Math.max(maxHeight, heightMap[i][j]);
+            }
+        }
+        int[][] water = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                water[i][j] = maxHeight;
+            }
+        }
+        Queue<int[]> qu = new LinkedList<>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    if (water[i][j] > heightMap[i][j]) {
+                        water[i][j] = heightMap[i][j];
+                        qu.offer(new int[]{i, j});
+                    }
+                }
+            }
+        }
+        while (!qu.isEmpty()) {
+            int[] curr = qu.poll();
+            int x = curr[0];
+            int y = curr[1];
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dirs[i], ny = y + dirs[i + 1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                    continue;
+                }
+                if (water[x][y] < water[nx][ny] && water[nx][ny] > heightMap[nx][ny]) {
+                    water[nx][ny] = Math.max(water[x][y], heightMap[nx][ny]);
+                    qu.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res += water[i][j] - heightMap[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
