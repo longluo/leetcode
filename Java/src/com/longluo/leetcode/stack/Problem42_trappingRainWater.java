7f00650f3475daca938d85bc9cbcac7e28064fbc
longluo <calmbody@163.com>
2021-11-10 11:59:53 +0800

    Problem 42 优化

--- Java/src/com/longluo/leetcode/stack/Problem42_trappingRainWater.java
+++ Java/src/com/longluo/leetcode/stack/Problem42_trappingRainWater.java
@@ -26,12 +26,14 @@ import java.util.Stack;
 public class Problem42_trappingRainWater {
 
     public static int trap(int[] height) {
+        if (height == null || height.length <= 2) {
+            return 0;
+        }
         int ans = 0;
         int len = height.length;
         for (int i = 1; i < len - 1; i++) {
-            int value = height[i];
-            int maxRight = value;
-            int maxLeft = value;
+            int maxRight = 0;
+            int maxLeft = 0;
             for (int j = i; j < len; j++) {
                 maxRight = Math.max(maxRight, height[j]);
             }
@@ -40,8 +42,53 @@ public class Problem42_trappingRainWater {
                 maxLeft = Math.max(maxLeft, height[k]);
             }
 
-            ans += (Math.min(maxLeft, maxRight) - value);
+            ans += (Math.min(maxLeft, maxRight) - height[i]);
         }
+
+        return ans;
+    }
+
+    public static int trap_dy(int[] height) {
+        if (height == null || height.length <= 2) {
+            return 0;
+        }
+
+        int ans = 0;
+        int len = height.length;
+        int[] leftMax = new int[len];
+        int[] rightMax = new int[len];
+        leftMax[0] = height[0];
+        for (int i = 1; i < len; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        rightMax[len - 1] = height[len - 1];
+        for (int i = len - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        for (int i = 1; i < len - 1; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+
+        return ans;
+    }
+
+    public static int trap_dp(int[] height) {
+        int ans = 0;
+
+
+        return ans;
+    }
+
+    public static int trap_tp(int[] height) {
+        int ans = 0;
+
+
+        return ans;
+    }
+
+    public static int trap_st(int[] height) {
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
 
         return ans;
     }
@@ -49,5 +96,6 @@ public class Problem42_trappingRainWater {
     public static void main(String[] args) {
         System.out.println("6 ?= " + trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
         System.out.println("9 ?= " + trap(new int[]{4, 2, 0, 3, 2, 5}));
+        System.out.println("9 ?= " + trap_dy(new int[]{4, 2, 0, 3, 2, 5}));
     }
 }
