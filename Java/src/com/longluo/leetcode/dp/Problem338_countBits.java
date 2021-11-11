8e69b364e7017b43e76f4c66e161bb813efb9a40
longluo <calmbody@163.com>
2021-11-11 20:57:31 +0800

    Problem 338 DP

--- Java/src/com/longluo/leetcode/dp/Problem338_countBits.java
+++ Java/src/com/longluo/leetcode/dp/Problem338_countBits.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 /**
  * 338. 比特位计数
+ * <p>
  * 给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
  * <p>
  * 示例 1:
@@ -18,6 +19,8 @@ import java.util.Arrays;
  * 给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
  * 要求算法的空间复杂度为O(n)。
  * 你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
+ * <p>
+ * https://leetcode-cn.com/problems/counting-bits
  */
 public class Problem338_countBits {
 
