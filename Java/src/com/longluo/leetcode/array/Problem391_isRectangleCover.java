f08e08c47cdfb2df0688573c6c4fac351049cb66
longluo <calmbody@163.com>
2021-11-16 19:58:31 +0800

    Problem 391 Pass

---
+++ Java/src/com/longluo/leetcode/array/Problem391_isRectangleCover.java
@@ -0,0 +1,123 @@
+package com.longluo.leetcode.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 391. 完美矩形
+ * <p>
+ * 给你一个数组 rectangles ，其中 rectangles[i] = [xi, yi, ai, bi] 表示一个坐标轴平行的矩形。这个矩形的左下顶点是 (xi, yi) ，右上顶点是 (ai, bi) 。
+ * 如果所有矩形一起精确覆盖了某个矩形区域，则返回 true ；否则，返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：rectangles = [[1,1,3,3],[3,1,4,2],[3,2,4,4],[1,3,2,4],[2,3,3,4]]
+ * 输出：true
+ * 解释：5 个矩形一起可以精确地覆盖一个矩形区域。
+ * <p>
+ * 示例 2：
+ * 输入：rectangles = [[1,1,2,3],[1,3,2,4],[3,1,4,2],[3,2,4,4]]
+ * 输出：false
+ * 解释：两个矩形之间有间隔，无法覆盖成一个矩形。
+ * <p>
+ * 示例 3：
+ * 输入：rectangles = [[1,1,3,3],[3,1,4,2],[1,3,2,4],[3,2,4,4]]
+ * 输出：false
+ * 解释：图形顶端留有空缺，无法覆盖成一个矩形。
+ * <p>
+ * 示例 4：
+ * 输入：rectangles = [[1,1,3,3],[3,1,4,2],[1,3,2,4],[2,2,4,4]]
+ * 输出：false
+ * 解释：因为中间有相交区域，虽然形成了矩形，但不是精确覆盖。
+ * <p>
+ * 提示：
+ * 1 <= rectangles.length <= 2 * 10^4
+ * rectangles[i].length == 4
+ * -10^5 <= xi, yi, ai, bi <= 10^5
+ * <p>
+ * https://leetcode-cn.com/problems/perfect-rectangle/
+ */
+public class Problem391_isRectangleCover {
+
+    public static boolean isRectangleCover(int[][] rectangles) {
+        long area = 0;
+
+        int minX = rectangles[0][0];
+        int minY = rectangles[0][1];
+        int maxX = rectangles[0][2];
+        int maxY = rectangles[0][3];
+
+        Map<Point, Integer> cnt = new HashMap<Point, Integer>();
+        for (int[] rect : rectangles) {
+            int x = rect[0], y = rect[1], a = rect[2], b = rect[3];
+            area += (long) (a - x) * (b - y);
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, a);
+            maxY = Math.max(maxY, b);
+
+            Point point1 = new Point(x, y);
+            Point point2 = new Point(x, b);
+            Point point3 = new Point(a, y);
+            Point point4 = new Point(a, b);
+
+            cnt.put(point1, cnt.getOrDefault(point1, 0) + 1);
+            cnt.put(point2, cnt.getOrDefault(point2, 0) + 1);
+            cnt.put(point3, cnt.getOrDefault(point3, 0) + 1);
+            cnt.put(point4, cnt.getOrDefault(point4, 0) + 1);
+        }
+
+        Point pointMinMin = new Point(minX, minY);
+        Point pointMinMax = new Point(minX, maxY);
+        Point pointMaxMin = new Point(maxX, minY);
+        Point pointMaxMax = new Point(maxX, maxY);
+        if (area != (long) (maxX - minX) * (maxY - minY)
+                || cnt.getOrDefault(pointMinMin, 0) != 1
+                || cnt.getOrDefault(pointMinMax, 0) != 1
+                || cnt.getOrDefault(pointMaxMin, 0) != 1
+                || cnt.getOrDefault(pointMaxMax, 0) != 1) {
+            return false;
+        }
+
+        cnt.remove(pointMinMin);
+        cnt.remove(pointMinMax);
+        cnt.remove(pointMaxMin);
+        cnt.remove(pointMaxMax);
+
+        for (Map.Entry<Point, Integer> entry : cnt.entrySet()) {
+            int value = entry.getValue();
+            if (value != 2 && value != 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int hashCode() {
+            return x + y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Point) {
+                Point point2 = (Point) obj;
+                return this.x == point2.x && this.y == point2.y;
+            }
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
