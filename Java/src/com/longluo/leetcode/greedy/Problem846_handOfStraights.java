42b93b0762036cc007da8f12040321c6c7ccbd46
longluo <calmbody@163.com>
2021-12-30 10:17:59 +0800

    Problem 846 BF

---
+++ Java/src/com/longluo/leetcode/greedy/Problem846_handOfStraights.java
@@ -0,0 +1,82 @@
+package com.longluo.leetcode.greedy;
+
+import java.util.Arrays;
+
+/**
+ * 846. 一手顺子
+ * <p>
+ * Alice 手中有一把牌，她想要重新排列这些牌，分成若干组，使每一组的牌数都是 groupSize ，
+ * 并且由 groupSize 张连续的牌组成。
+ * 给你一个整数数组 hand 其中 hand[i] 是写在第 i 张牌，和一个整数 groupSize 。如果她可能重新排列这些牌，返回 true ；
+ * 否则，返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+ * 输出：true
+ * 解释：Alice 手中的牌可以被重新排列为 [1,2,3]，[2,3,4]，[6,7,8]。
+ * <p>
+ * 示例 2：
+ * 输入：hand = [1,2,3,4,5], groupSize = 4
+ * 输出：false
+ * 解释：Alice 手中的牌无法被重新排列成几个大小为 4 的组。
+ * <p>
+ * <p>
+ * 提示：
+ * 1 <= hand.length <= 10^4
+ * 0 <= hand[i] <= 10^9
+ * 1 <= groupSize <= hand.length
+ * <p>
+ * 注意：此题目与 1296 重复：https://leetcode-cn.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+ * <p>
+ * https://leetcode-cn.com/problems/hand-of-straights/
+ */
+public class Problem846_handOfStraights {
+
+    public static boolean isNStraightHand(int[] hand, int groupSize) {
+        if (hand == null || hand.length < groupSize || hand.length % groupSize != 0) {
+            return false;
+        }
+
+        if (groupSize == 1) {
+            return true;
+        }
+
+        int len = hand.length;
+        Arrays.sort(hand);
+        int[] nums = new int[len];
+        System.arraycopy(hand, 0, nums, 0, len);
+        int seqCnt = 0;
+        for (int i = 0; i < len; i++) {
+            if (hand[i] != -1) {
+                int seqNum = hand[i];
+                int seqLen = 1;
+                seqNum++;
+                hand[i] = -1;
+                for (int j = i + 1; j < len; j++) {
+                    if (hand[j] == seqNum) {
+                        seqLen++;
+                        seqNum++;
+                        hand[j] = -1;
+                        if (seqLen == groupSize) {
+                            seqCnt++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (seqCnt == len / groupSize) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("true ?= " + isNStraightHand(new int[]{1, 2, 3}, 1));
+        System.out.println("false ?= " + isNStraightHand(new int[]{1, 1, 2, 2, 3, 3}, 2));
+        System.out.println("true ?= " + isNStraightHand(new int[]{1, 2, 3, 6, 2, 3, 4, 7, 8}, 3));
+        System.out.println("false ?= " + isNStraightHand(new int[]{1, 2, 3, 4, 5}, 4));
+    }
+}
