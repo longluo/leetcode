0d90bc4006eee32020304719365b20608d7a7fc4
longluo <calmbody@163.com>
2022-01-17 19:53:35 +0800

    Problem 1220 BF

---
+++ Java/src/com/longluo/leetcode/dp/Problem1220_countVowelPermutation.java
@@ -0,0 +1,44 @@
+package com.longluo.leetcode.dp;
+
+/**
+ * 1220. 统计元音字母序列的数目
+ * <p>
+ * 给你一个整数 n，请你帮忙统计一下我们可以按下述规则形成多少个长度为 n 的字符串：
+ * <p>
+ * 字符串中的每个字符都应当是小写元音字母（'a', 'e', 'i', 'o', 'u'）
+ * 每个元音 'a' 后面都只能跟着 'e'
+ * 每个元音 'e' 后面只能跟着 'a' 或者是 'i'
+ * 每个元音 'i' 后面 不能 再跟着另一个 'i'
+ * 每个元音 'o' 后面只能跟着 'i' 或者是 'u'
+ * 每个元音 'u' 后面只能跟着 'a'
+ * 由于答案可能会很大，所以请你返回 模 10^9 + 7 之后的结果。
+ * <p>
+ * 示例 1：
+ * 输入：n = 1
+ * 输出：5
+ * 解释：所有可能的字符串分别是："a", "e", "i" , "o" 和 "u"。
+ * <p>
+ * 示例 2：
+ * 输入：n = 2
+ * 输出：10
+ * 解释：所有可能的字符串分别是："ae", "ea", "ei", "ia", "ie", "io", "iu", "oi", "ou" 和 "ua"。
+ * <p>
+ * 示例 3：
+ * 输入：n = 5
+ * 输出：68
+ * <p>
+ * 提示：
+ * 1 <= n <= 2 * 10^4
+ * <p>
+ * https://leetcode-cn.com/problems/count-vowels-permutation/
+ */
+public class Problem1220_countVowelPermutation {
+
+    public static int countVowelPermutation(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
