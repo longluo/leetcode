2cd0194aff2bc801185d87a7ac6027806348a0b9
longluo <calmbody@163.com>
2022-01-17 22:32:41 +0800

    Problem 1220 BF

--- Java/src/com/longluo/leetcode/dp/Problem1220_countVowelPermutation.java
+++ Java/src/com/longluo/leetcode/dp/Problem1220_countVowelPermutation.java
@@ -35,10 +35,36 @@ package com.longluo.leetcode.dp;
 public class Problem1220_countVowelPermutation {
 
     public static int countVowelPermutation(int n) {
+        long mod = 1000000007;
+        long[] dp = new long[5];
+        long[] ndp = new long[5];
+        for (int i = 0; i < 5; ++i) {
+            dp[i] = 1;
+        }
+        for (int i = 2; i <= n; ++i) {
+            /* a前面可以为e,u,i */
+            ndp[0] = (dp[1] + dp[2] + dp[4]) % mod;
+            /* e前面可以为a,i */
+            ndp[1] = (dp[0] + dp[2]) % mod;
+            /* i前面可以为e,o */
+            ndp[2] = (dp[1] + dp[3]) % mod;
+            /* o前面可以为i */
+            ndp[3] = dp[2];
+            /* u前面可以为i,o */
+            ndp[4] = (dp[2] + dp[3]) % mod;
+            System.arraycopy(ndp, 0, dp, 0, 5);
+        }
+        long ans = 0;
+        for (int i = 0; i < 5; ++i) {
+            ans = (ans + dp[i]) % mod;
+        }
 
+        return (int) ans;
     }
 
     public static void main(String[] args) {
-
+        System.out.println("5 ?= " + countVowelPermutation(1));
+        System.out.println("10 ?= " + countVowelPermutation(2));
+        System.out.println("68 ?= " + countVowelPermutation(5));
     }
 }
