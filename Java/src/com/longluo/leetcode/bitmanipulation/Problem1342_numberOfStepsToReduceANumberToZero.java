cb6f1d783edc62b450e3068bcf5e163d091a23cb
longluo <calmbody@163.com>
2022-02-02 21:07:55 +0800

    Problem 1342 BF

---
+++ Java/src/com/longluo/leetcode/bitmanipulation/Problem1342_numberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,60 @@
+package com.longluo.leetcode.bitmanipulation;
+
+/**
+ * 1342. 将数字变成 0 的操作次数
+ * <p>
+ * 给你一个非负整数 num ，请你返回将它变成 0 所需要的步数。 如果当前数字是偶数，你需要把它除以 2 ；否则，减去 1 。
+ * <p>
+ * 示例 1：
+ * 输入：num = 14
+ * 输出：6
+ * 解释：
+ * 步骤 1) 14 是偶数，除以 2 得到 7 。
+ * 步骤 2） 7 是奇数，减 1 得到 6 。
+ * 步骤 3） 6 是偶数，除以 2 得到 3 。
+ * 步骤 4） 3 是奇数，减 1 得到 2 。
+ * 步骤 5） 2 是偶数，除以 2 得到 1 。
+ * 步骤 6） 1 是奇数，减 1 得到 0 。
+ * <p>
+ * 示例 2：
+ * 输入：num = 8
+ * 输出：4
+ * 解释：
+ * 步骤 1） 8 是偶数，除以 2 得到 4 。
+ * 步骤 2） 4 是偶数，除以 2 得到 2 。
+ * 步骤 3） 2 是偶数，除以 2 得到 1 。
+ * 步骤 4） 1 是奇数，减 1 得到 0 。
+ * <p>
+ * 示例 3：
+ * 输入：num = 123
+ * 输出：12
+ * <p>
+ * 提示：
+ * 0 <= num <= 10^6
+ * <p>
+ * https://leetcode-cn.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+ */
+public class Problem1342_numberOfStepsToReduceANumberToZero {
+
+    public static int numberOfSteps(int num) {
+        int ans = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            ans++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 ?= " + numberOfSteps(1));
+        System.out.println("2 ?= " + numberOfSteps(2));
+        System.out.println("4 ?= " + numberOfSteps(8));
+        System.out.println("6 ?= " + numberOfSteps(14));
+        System.out.println("12 ?= " + numberOfSteps(123));
+    }
+}
