89f5f5dde50adf98c19fcbc83e3566d062d12c56
longluo <calmbody@163.com>
2022-03-07 17:11:19 +0800

    Problem 504 BF

---
+++ Java/src/com/longluo/leetcode/math/Problem504_convertToBase7.java
@@ -0,0 +1,55 @@
+package com.longluo.leetcode.math;
+
+/**
+ * 504. 七进制数
+ * <p>
+ * 给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。
+ * <p>
+ * 示例 1:
+ * 输入: num = 100
+ * 输出: "202"
+ * <p>
+ * 示例 2:
+ * 输入: num = -7
+ * 输出: "-10"
+ * <p>
+ * 提示：
+ * -10^7 <= num <= 10^7
+ * <p>
+ * https://leetcode-cn.com/problems/base-7/
+ */
+public class Problem504_convertToBase7 {
+
+    public static String convertToBase7(int num) {
+        if (num >= -6 && num <= 6) {
+            return String.valueOf(num);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        if (num < 0) {
+            flag = true;
+            num = -num;
+        }
+
+        while (num > 0) {
+            sb.append(num % 7);
+            num = num / 7;
+        }
+
+        if (flag) {
+            sb.append("-");
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("0 ?= " + convertToBase7(0));
+        System.out.println("5 ?= " + convertToBase7(5));
+        System.out.println("10 ?= " + convertToBase7(7));
+        System.out.println("11 ?= " + convertToBase7(8));
+        System.out.println("-10 ?= " + convertToBase7(-7));
+        System.out.println("202 ?= " + convertToBase7(100));
+    }
+}
