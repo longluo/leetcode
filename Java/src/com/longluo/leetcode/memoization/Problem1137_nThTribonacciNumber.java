f079503b4be3252814e413a7d962c759c77181a2
longluo <calmbody@163.com>
2022-03-16 21:45:25 +0800

    Problem 1137

--- Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
+++ Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
@@ -23,6 +23,8 @@ package com.longluo.leetcode.memoization;
  * 答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
  * <p>
  * https://leetcode-cn.com/problems/n-th-tribonacci-number/
+ * <p>
+ * https://leetcode.com/problems/n-th-tribonacci-number/
  */
 public class Problem1137_nThTribonacciNumber {
 
@@ -44,7 +46,7 @@ public class Problem1137_nThTribonacciNumber {
         return res[n];
     }
 
-    public static int tribonacci_2(int n) {
+    public static int tribonacci_iter(int n) {
         if (n == 0) {
             return 0;
         } else if (n == 1 || n == 2) {
@@ -66,18 +68,39 @@ public class Problem1137_nThTribonacciNumber {
         return ans;
     }
 
+    public static int tribonacci_recursive(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 2) {
+            return 1;
+        }
+
+        return tribonacci_recursive(n - 1) + tribonacci_recursive(n - 2) + tribonacci_recursive(n - 3);
+    }
+
+    static int[] nums = {0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415, 223317, 410744, 755476, 1389537, 2555757, 4700770, 8646064, 15902591, 29249425, 53798080, 98950096, 181997601, 334745777, 615693474, 1132436852, 2082876103};
+    public static int tribonacci_fast(int n) {
+        return nums[n];
+    }
+
     public static void main(String[] args) {
         System.out.println("0 ?= " + tribonacci(0));
-        System.out.println("0 ?= " + tribonacci_2(0));
+        System.out.println("0 ?= " + tribonacci_iter(0));
         System.out.println("1 ?= " + tribonacci(1));
-        System.out.println("1 ?= " + tribonacci_2(1));
+        System.out.println("1 ?= " + tribonacci_iter(1));
         System.out.println("1 ?= " + tribonacci(2));
-        System.out.println("1 ?= " + tribonacci_2(2));
+        System.out.println("1 ?= " + tribonacci_iter(2));
         System.out.println("2 ?= " + tribonacci(3));
-        System.out.println("2 ?= " + tribonacci_2(3));
+        System.out.println("2 ?= " + tribonacci_iter(3));
         System.out.println("4 ?= " + tribonacci(4));
-        System.out.println("4 ?= " + tribonacci_2(4));
+        System.out.println("4 ?= " + tribonacci_iter(4));
         System.out.println("1389537 ?= " + tribonacci(25));
-        System.out.println("1389537 ?= " + tribonacci_2(25));
+        System.out.println("1389537 ?= " + tribonacci_iter(25));
+
+        for (int i = 0; i <= 37; i++) {
+            System.out.print("," + tribonacci_iter(i));
+        }
     }
 }
