4b8b02a742b1e45c97e37afb5e80837425fc79e2
longluo <calmbody@163.com>
2022-03-19 22:38:45 +0800

    Problem 204 Better

--- Java/src/com/longluo/leetcode/math/Problem204_countPrimes.java
+++ Java/src/com/longluo/leetcode/math/Problem204_countPrimes.java
@@ -2,25 +2,27 @@ package com.longluo.leetcode.math;
 
 import java.util.Arrays;
 
-/*
-204. 计数质数
-统计所有小于非负整数n的质数的数量。
-
-示例 1：
-输入：n = 10
-输出：4
-解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7。
-
-示例 2：
-输入：n = 0
-输出：0
-
-示例 3：
-输入：n = 1
-输出：0
-
-提示：
-0 <= n <= 5 * 10^6
+/**
+ * 204. 计数质数
+ * 统计所有小于非负整数n的质数的数量。
+ * <p>
+ * 示例 1：
+ * 输入：n = 10
+ * 输出：4
+ * 解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7。
+ * <p>
+ * 示例 2：
+ * 输入：n = 0
+ * 输出：0
+ * <p>
+ * 示例 3：
+ * 输入：n = 1
+ * 输出：0
+ * <p>
+ * 提示：
+ * 0 <= n <= 5 * 10^6
+ * <p>
+ * https://leetcode.com/problems/count-primes/
  */
 public class Problem204_countPrimes {
 
@@ -49,7 +51,26 @@ public class Problem204_countPrimes {
         return count;
     }
 
+    public static int countPrimes_bf(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+
+        int ans = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
     private static boolean isPrimeNumber(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
@@ -70,5 +91,12 @@ public class Problem204_countPrimes {
         System.out.println("0 ?= " + countPrimes(0));
         System.out.println("0 ?= " + countPrimes(1));
 
+        System.out.println("4 ?= " + countPrimes_bf(10));
+
+//        for (int i = 2; i < 5000000; i++) {
+//            if (isPrimeNumber(i)) {
+//                System.out.print(i + ", ");
+//            }
+//        }
     }
 }
