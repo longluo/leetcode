63a1a90a0a3b6210c577b5482b15966444fd8659
longluo <calmbody@163.com>
2022-03-20 19:11:02 +0800

    Problem 2 Recursive Solution

--- Java/src/com/longluo/hot100/Problem2_addTwoNumbers.java
+++ Java/src/com/longluo/hot100/Problem2_addTwoNumbers.java
@@ -29,6 +29,8 @@ import com.longluo.datastructure.ListNode;
  * 题目数据保证列表表示的数字不含前导零
  * <p>
  * https://leetcode-cn.com/problems/add-two-numbers/
+ * <p>
+ * https://leetcode.com/problems/add-two-numbers/
  */
 public class Problem2_addTwoNumbers {
 
@@ -116,10 +118,46 @@ public class Problem2_addTwoNumbers {
         return dummyNode.next;
     }
 
+    /**
+     * Recursive
+     */
+    public static ListNode addTwoNumbers_rec(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+
+        return addNode(l1, l2, 0);
+    }
+
+    public static ListNode addNode(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
+        }
+
+        if (l1 != null) {
+            carry += l1.val;
+            l1 = l1.next;
+        }
+
+        if (l2 != null) {
+            carry += l2.val;
+            l2 = l2.next;
+        }
+
+        ListNode addNode = new ListNode(carry % 10);
+        carry = carry / 10;
+        addNode.next = addNode(l1, l2, carry);
+        return addNode;
+    }
+
     public static void main(String[] args) {
         ListNode tst1 = LinkedListNodeUtils.constructListNode(new int[]{2, 4, 3});
         ListNode tst2 = LinkedListNodeUtils.constructListNode(new int[]{5, 6, 4});
         System.out.println("[7, 0, 8] ?= " + LinkedListNodeUtils.printLinkedList(addTwoNumbers(tst1, tst2)));
         System.out.println("[7, 0, 8] ?= " + LinkedListNodeUtils.printLinkedList(addTwoNumbers_opt(tst1, tst2)));
+
+        System.out.println("[7, 0, 8] ?= " + LinkedListNodeUtils.printLinkedList(addTwoNumbers_rec(tst1, tst2)));
     }
 }
