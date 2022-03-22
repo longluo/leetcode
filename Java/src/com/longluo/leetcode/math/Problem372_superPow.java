d44c96044632bccf6c6067f9461cd6fe71d71c83
longluo <calmbody@163.com>
2022-03-22 20:24:20 +0800

    Problem 372 Super Power

--- Java/src/com/longluo/leetcode/math/Problem372_superPow.java
+++ Java/src/com/longluo/leetcode/math/Problem372_superPow.java
@@ -28,10 +28,12 @@ package com.longluo.leetcode.math;
  * b 不含前导 0
  * <p>
  * https://leetcode-cn.com/problems/super-pow/
+ * <p>
+ * https://leetcode.com/problems/super-pow/
  */
 public class Problem372_superPow {
 
-    public static int superPow(int a, int[] b) {
+    public static int superPow_bf(int a, int[] b) {
         if (a == 1) {
             return 1;
         }
@@ -49,10 +51,46 @@ public class Problem372_superPow {
         return ans;
     }
 
+    public static int superPow_fast(int a, int[] b) {
+        if (a == 1) {
+            return 1;
+        }
+
+        int ans = 1;
+        int len = b.length;
+        for (int i = len - 1; i >= 0; i--) {
+            ans = (int) ((long) ans * binaryPower(a, b[i]) % 1337);
+            a = binaryPower(a, 10);
+        }
+
+        return ans;
+    }
+
+    public static int binaryPower(int base, int exp) {
+        int res = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                res = (int) ((long) res * base % 1337);
+            }
+
+            base = (int) ((long) base * base % 1337);
+            exp = exp >> 1;
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println("8 ?= " + superPow(2, new int[]{3}));
-        System.out.println("1024 ?= " + superPow(2, new int[]{1, 0}));
-        System.out.println("1 ?= " + superPow(1, new int[]{4, 3, 3, 8, 5, 2}));
-        System.out.println("1198 ?= " + superPow(2147483647, new int[]{2, 0, 0}));
+        System.out.println("8 ?= " + superPow_bf(2, new int[]{3}));
+        System.out.println("1024 ?= " + superPow_bf(2, new int[]{1, 0}));
+        System.out.println("1 ?= " + superPow_bf(1, new int[]{4, 3, 3, 8, 5, 2}));
+        System.out.println("1198 ?= " + superPow_bf(2147483647, new int[]{2, 0, 0}));
+
+        System.out.println("128 ?= " + binaryPower(2, 7));
+
+        System.out.println("8 ?= " + superPow_fast(2, new int[]{3}));
+        System.out.println("1024 ?= " + superPow_fast(2, new int[]{1, 0}));
+        System.out.println("1 ?= " + superPow_fast(1, new int[]{4, 3, 3, 8, 5, 2}));
+        System.out.println("1198 ?= " + superPow_fast(2147483647, new int[]{2, 0, 0}));
     }
 }
