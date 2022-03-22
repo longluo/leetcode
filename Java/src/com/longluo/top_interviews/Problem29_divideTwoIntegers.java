5024732dadc903089310302ee60ed9bd3d5e0043
longluo <calmbody@163.com>
2022-03-22 22:26:27 +0800

    Problem 29 Quick Div

--- Java/src/com/longluo/top_interviews/Problem29_divideTwoIntegers.java
+++ Java/src/com/longluo/top_interviews/Problem29_divideTwoIntegers.java
@@ -1,5 +1,8 @@
 package com.longluo.top_interviews;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 29. 两数相除
  * <p>
@@ -196,9 +199,61 @@ public class Problem29_divideTwoIntegers {
         return true;
     }
 
+    public static int divide_quick(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用类二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        List<Integer> candidates = new ArrayList<Integer>();
+        candidates.add(divisor);
+        int index = 0;
+        // 注意溢出
+        while (candidates.get(index) >= dividend - candidates.get(index)) {
+            candidates.add(candidates.get(index) + candidates.get(index));
+            ++index;
+        }
+        int ans = 0;
+        for (int i = candidates.size() - 1; i >= 0; --i) {
+            if (candidates.get(i) >= dividend) {
+                ans += 1 << i;
+                dividend -= candidates.get(i);
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
     public static void main(String[] args) {
         int val = Math.abs(-2147483648);
         System.out.println("val = " + val);
+
+        System.out.println("15 ?= " + divide_quick(31, 2));
 
         System.out.println("3 ?= " + divide_fast(10, 3));
         System.out.println("3 ?= " + divide_fast(2147483647, 2));
