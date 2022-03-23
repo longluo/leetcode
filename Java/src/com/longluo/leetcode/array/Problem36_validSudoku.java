cbd2a786ea04704778c8b2325581ffc8d001c453
longluo <calmbody@163.com>
2022-03-23 11:43:12 +0800

    Problem 36 Array

--- Java/src/com/longluo/leetcode/array/Problem36_validSudoku.java
+++ Java/src/com/longluo/leetcode/array/Problem36_validSudoku.java
@@ -140,6 +140,29 @@ public class Problem36_validSudoku {
         return true;
     }
 
+    public static boolean isValidSudoku_array(char[][] board) {
+        int[][] row = new int[9][9];
+        int[][] col = new int[9][9];
+        int[][][] subBox = new int[3][3][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char ch = board[i][j];
+                if (ch != '.') {
+                    int num = ch - '0' - 1;
+                    row[i][num]++;
+                    col[j][num]++;
+                    subBox[i / 3][j / 3][num]++;
+                    if (row[i][num] > 1 || col[j][num] > 1 || subBox[i / 3][j / 3][num] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println("true ?= " + isValidSudoku(new char[][]
                 {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
