685895dfa8c125f5031837dc656a3c6ba7298b64
longluo <calmbody@163.com>
2022-03-23 18:40:39 +0800

    Problem 440 Over Memory

---
+++ Java/src/com/longluo/leetcode/trie/Problem440_findKthNumberInLexicographicalOrder.java
@@ -0,0 +1,51 @@
+package com.longluo.leetcode.trie;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 440. 字典序的第K小数字
+ * <p>
+ * 给定整数 n 和 k，返回  [1, n] 中字典序第 k 小的数字。
+ * <p>
+ * 示例 1:
+ * 输入: n = 13, k = 2
+ * 输出: 10
+ * 解释: 字典序的排列是 [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9]，所以第二小的数字是 10。
+ * <p>
+ * 示例 2:
+ * 输入: n = 1, k = 1
+ * 输出: 1
+ * <p>
+ * 提示:
+ * 1 <= k <= n <= 10^9
+ * <p>
+ * https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
+ */
+public class Problem440_findKthNumberInLexicographicalOrder {
+
+    public static int findKthNumber(int n, int k) {
+        if (n == 1) {
+            return 1;
+        }
+
+        String[] numStrs = new String[n + 1];
+        for (int i = 0; i <= n; i++) {
+            numStrs[i] = "" + i;
+        }
+
+        Arrays.sort(numStrs, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+
+        return Integer.parseInt(numStrs[k]);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 ?= " + findKthNumber(1, 1));
+        System.out.println("10 ?= " + findKthNumber(13, 2));
+    }
+}
