1cdb4ba0e29bb3091e1d24b240ba18b4e9635ecf
longluo <calmbody@163.com>
2022-03-24 11:47:55 +0800

    Problem 268 Sort

---
+++ Java/src/com/longluo/leetcode/array/Problem268_missingNumber.java
@@ -0,0 +1,54 @@
+package com.longluo.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 268. Missing Number
+ * <p>
+ * Easy
+ * <p>
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ * *
+ * Example 1:
+ * Input: nums = [3,0,1]
+ * Output: 2
+ * Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+ * <p>
+ * Example 2:
+ * Input: nums = [0,1]
+ * Output: 2
+ * Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+ * <p>
+ * Example 3:
+ * Input: nums = [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ * <p>
+ * Constraints:
+ * n == nums.length
+ * 1 <= n <= 10^4
+ * 0 <= nums[i] <= n
+ * All the numbers of nums are unique.
+ * <p>
+ * Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+ * <p>
+ * https://leetcode.com/problems/missing-number/
+ */
+public class Problem268_missingNumber {
+
+    public static int missingNumber_sort(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return len;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
