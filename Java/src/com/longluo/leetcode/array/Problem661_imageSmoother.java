ffe1ae3d7dec3dc16eeca382c49e19aeb12f3781
longluo <calmbody@163.com>
2022-03-24 11:42:38 +0800

    Problem 661 PrefixSum

--- Java/src/com/longluo/leetcode/array/Problem661_imageSmoother.java
+++ Java/src/com/longluo/leetcode/array/Problem661_imageSmoother.java
@@ -3,7 +3,6 @@ package com.longluo.leetcode.array;
 import com.longluo.datastructure.ArrayUtils;
 
 /**
- *
  * https://leetcode-cn.com/problems/image-smoother/
  */
 public class Problem661_imageSmoother {
@@ -34,6 +33,28 @@ public class Problem661_imageSmoother {
         }
 
         return res;
+    }
+
+    public static int[][] imageSmoother_prefixSum(int[][] img) {
+        int m = img.length, n = img[0].length;
+        int[][] sum = new int[m + 10][n + 10];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + img[i - 1][j - 1];
+            }
+        }
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int a = Math.max(0, i - 1), b = Math.max(0, j - 1);
+                int c = Math.min(m - 1, i + 1), d = Math.min(n - 1, j + 1);
+                int cnt = (c - a + 1) * (d - b + 1);
+                int tot = sum[c + 1][d + 1] - sum[a][d + 1] - sum[c + 1][b] + sum[a][b];
+                ans[i][j] = tot / cnt;
+            }
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
