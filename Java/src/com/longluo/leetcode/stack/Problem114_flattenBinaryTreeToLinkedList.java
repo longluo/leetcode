f2194427f39d8c84663ce461dc747fcfc6a206d0
longluo <calmbody@163.com>
2022-03-26 15:41:07 +0800

    Problem 114 Stack + ArrayList

--- Java/src/com/longluo/leetcode/stack/Problem114_flattenBinaryTreeToLinkedList.java
+++ Java/src/com/longluo/leetcode/stack/Problem114_flattenBinaryTreeToLinkedList.java
@@ -1,9 +1,11 @@
 package com.longluo.leetcode.stack;
 
 import com.longluo.datastructure.TreeNode;
+import com.longluo.datastructure.TreeUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 114. 二叉树展开为链表
@@ -62,7 +64,35 @@ public class Problem114_flattenBinaryTreeToLinkedList {
         preOrder(root.right, nodeList);
     }
 
-    public static void main(String[] args) {
+    public static void flatten_stack(TreeNode root) {
+        if (root == null) {
+            return;
+        }
 
+        List<TreeNode> nodeList = new ArrayList<>();
+        Stack<TreeNode> stk = new Stack<>();
+        while (root != null || !stk.empty()) {
+            while (root != null) {
+                nodeList.add(root);
+                stk.push(root);
+                root = root.left;
+            }
+
+            root = stk.pop();
+            root = root.right;
+        }
+
+        int n = nodeList.size();
+        for (int i = 1; i < n; i++) {
+            TreeNode preNode = nodeList.get(i - 1);
+            TreeNode currNode = nodeList.get(i);
+            preNode.left = null;
+            preNode.right = currNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode tst1 = TreeUtils.constructTree(new Integer[]{1, 2, 5, 3, 4, null, 6});
+        flatten_stack(tst1);
     }
 }
