1dbc3b026363cd46d6ea7deb9b2eba8d5fd4a97b
longluo <calmbody@163.com>
2022-03-28 22:20:22 +0800

    Problem 1137 Matrix

--- Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
+++ Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
@@ -81,8 +81,44 @@ public class Problem1137_nThTribonacciNumber {
     }
 
     static int[] nums = {0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415, 223317, 410744, 755476, 1389537, 2555757, 4700770, 8646064, 15902591, 29249425, 53798080, 98950096, 181997601, 334745777, 615693474, 1132436852, 2082876103};
-    public static int tribonacci_fast(int n) {
+
+    public static int tribonacci_table(int n) {
         return nums[n];
+    }
+
+    public static int tribonacci_matrix(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return 1;
+        }
+        int[][] q = {{1, 1, 1}, {1, 0, 0}, {0, 1, 0}};
+        int[][] res = pow(q, n);
+        return res[0][2];
+    }
+
+    public static int[][] pow(int[][] a, int n) {
+        int[][] ret = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                ret = multiply(ret, a);
+            }
+            n >>= 1;
+            a = multiply(a, a);
+        }
+        return ret;
+    }
+
+    public static int[][] multiply(int[][] a, int[][] b) {
+        int[][] c = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j] + a[i][2] * b[2][j];
+            }
+        }
+
+        return c;
     }
 
     public static void main(String[] args) {
