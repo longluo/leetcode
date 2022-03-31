3452c8d7c1da93960bca27af5c02fb27b8a15d58
longluo <calmbody@163.com>
2022-03-31 21:37:31 +0800

    56 Sort

---
+++ Java/src/com/longluo/leetcode/array/Problem56_mergeIntervals.java
@@ -0,0 +1,70 @@
+package com.longluo.leetcode.array;
+
+import java.util.*;
+
+/**
+ * 56. 合并区间
+ * <p>
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，
+ * 并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+ * <p>
+ * 示例 1：
+ * 输入：intervals = [[1,3},{2,6},{8,10},{15,18]]
+ * 输出：[[1,6},{8,10},{15,18]]
+ * 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+ * <p>
+ * 示例 2：
+ * 输入：intervals = [[1,4},{4,5]]
+ * 输出：[[1,5]]
+ * 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ * <p>
+ * 提示：
+ * 1 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^4
+ * <p>
+ * https://leetcode-cn.com/problems/merge-intervals/
+ */
+public class Problem56_mergeIntervals {
+
+    // Sort
+    public static int[][] merge_sort(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> ans = new ArrayList<>();
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] > right) {
+                ans.add(new int[]{left, right});
+                left = interval[0];
+                right = interval[1];
+            } else {
+                right = Math.max(right, interval[1]);
+            }
+        }
+
+        ans.add(new int[]{left, right});
+
+        int[][] res = new int[ans.size()][2];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        merge_sort(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
+        merge_sort(new int[][]{{1, 4}, {2, 3}});
+    }
+}
