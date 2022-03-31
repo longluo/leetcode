ab6f180a60867c727c29a94ad6b1628e1c1c5c99
longluo <calmbody@163.com>
2022-03-31 16:24:54 +0800

    557 Two Pointers & Regex

--- Java/src/com/longluo/leetcode/twopointers/Problem557_reverseWordsInAString_iii.java
+++ Java/src/com/longluo/leetcode/twopointers/Problem557_reverseWordsInAString_iii.java
@@ -15,8 +15,36 @@ package com.longluo.leetcode.twopointers;
  * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
  */
 public class Problem557_reverseWordsInAString_iii {
+    // BF O(n) O(n)
+    public static String reverseWords_bf(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
 
-    public static String reverseWords(String s) {
+        int len = s.length();
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        while (idx < len) {
+            int start = idx;
+            while (idx < len && s.charAt(idx) != ' ') {
+                idx++;
+            }
+
+            for (int j = idx - 1; j >= start; j--) {
+                sb.append(s.charAt(j));
+            }
+
+            while (idx < len && s.charAt(idx) == ' ') {
+                sb.append(" ");
+                idx++;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Two Pointers O(n) O(n)
+    public static String reverseWords_tp(String s) {
         if (s == null || s.length() <= 1) {
             return s;
         }
@@ -38,6 +66,7 @@ public class Problem557_reverseWordsInAString_iii {
                 start++;
                 end--;
             }
+
             while (idx < n && arr[idx] == ' ') {
                 idx++;
             }
@@ -46,7 +75,8 @@ public class Problem557_reverseWordsInAString_iii {
         return new String(arr);
     }
 
-    public static String reverseWords_2(String s) {
+    // Regex Reverse Word O(n) O(n)
+    public static String reverseWords_regex(String s) {
         if (s == null || s.length() <= 1) {
             return s;
         }
@@ -75,14 +105,35 @@ public class Problem557_reverseWordsInAString_iii {
         return sb.toString();
     }
 
+    // Reverse Reverse Word Opt O(n) O(n)
+    public static String reverseWords_regex_opt(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+
+        String[] words = s.split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder sb = new StringBuilder(words[i]);
+            words[i] = sb.reverse().toString();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]).append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("a ?= " + reverseWords("a"));
-        System.out.println("a b ?= " + reverseWords("a b"));
-        System.out.println("ab ?= " + reverseWords("ab"));
-        System.out.println("ab  ?= " + reverseWords("ab "));
-        System.out.println("ba ab ?= " + reverseWords("ab ba"));
-        System.out.println("cba ?= " + reverseWords("abc"));
-        System.out.println("s'teL ekat edoCteeL tsetnoc ?= " + reverseWords("Let's take LeetCode contest"));
-        System.out.println("s'teL ekat edoCteeL tsetnoc ?= " + reverseWords_2("Let's take LeetCode contest"));
+        System.out.println("a ?= " + reverseWords_tp("a"));
+        System.out.println("a b ?= " + reverseWords_tp("a b"));
+        System.out.println("ab ?= " + reverseWords_tp("ab"));
+        System.out.println("ab  ?= " + reverseWords_tp("ab "));
+        System.out.println("ba ab ?= " + reverseWords_tp("ab ba"));
+        System.out.println("cba ?= " + reverseWords_tp("abc"));
+        System.out.println("s'teL ekat edoCteeL tsetnoc ?= " + reverseWords_tp("Let's take LeetCode contest"));
+        System.out.println("s'teL ekat edoCteeL tsetnoc ?= " + reverseWords_regex("Let's take LeetCode contest"));
     }
 }
