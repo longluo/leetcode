04206f27d8b41cbf1c73b94691a9688a1bb2e994
longluo <calmbody@163.com>
2022-04-01 14:18:21 +0800

    1790 HashMap time: O(n) space: O(n)

---
+++ Java/src/com/longluo/leetcode/hash/Problem1790_areAlmostEqual.java
@@ -0,0 +1,75 @@
+package com.longluo.leetcode.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1790. 仅执行一次字符串交换能否使两个字符串相等
+ * <p>
+ * 给你长度相等的两个字符串 s1 和 s2 。一次 字符串交换 操作的步骤如下：选出某个字符串中的两个下标（不必不同），
+ * 并交换这两个下标所对应的字符。
+ * 如果对 其中一个字符串 执行 最多一次字符串交换 就可以使两个字符串相等，返回 true ；否则，返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：s1 = "bank", s2 = "kanb"
+ * 输出：true
+ * 解释：例如，交换 s2 中的第一个和最后一个字符可以得到 "bank"
+ * <p>
+ * 示例 2：
+ * 输入：s1 = "attack", s2 = "defend"
+ * 输出：false
+ * 解释：一次字符串交换无法使两个字符串相等
+ * <p>
+ * 示例 3：
+ * 输入：s1 = "kelb", s2 = "kelb"
+ * 输出：true
+ * 解释：两个字符串已经相等，所以不需要进行字符串交换
+ * <p>
+ * 示例 4：
+ * 输入：s1 = "abcd", s2 = "dcba"
+ * 输出：false
+ * <p>
+ * 提示：
+ * 1 <= s1.length, s2.length <= 100
+ * s1.length == s2.length
+ * s1 和 s2 仅由小写英文字母组成
+ * <p>
+ * https://leetcode-cn.com/problems/check-if-one-string-swap-can-make-strings-equal/
+ */
+public class Problem1790_areAlmostEqual {
+
+    // HashMap time: O(n) space: O(n)
+    public static boolean areAlmostEqual_hash(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            map1.put(s1.charAt(i), map1.getOrDefault(s1.charAt(i), 0) + 1);
+            map2.put(s2.charAt(i), map2.getOrDefault(s2.charAt(i), 0) + 1);
+        }
+
+        int cnt = 2;
+        for (int i = 0; i < s1.length(); i++) {
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
+            if (!map1.getOrDefault(ch1, 0).equals(map2.getOrDefault(ch1, 0))
+                    || !map1.getOrDefault(ch2, 0).equals(map2.getOrDefault(ch2, 0))) {
+                return false;
+            }
+            if (ch1 != ch2) {
+                cnt--;
+            }
+        }
+
+        return cnt >= 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("true ?= " + areAlmostEqual_hash("bank", "kanb"));
+        System.out.println("false ?= " + areAlmostEqual_hash("aa", "ac"));
+        System.out.println("false ?= " + areAlmostEqual_hash("abca", "abcc"));
+    }
+}
