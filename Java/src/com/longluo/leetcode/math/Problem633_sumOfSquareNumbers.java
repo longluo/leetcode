13a57e015e1108ebd1964d68da0d2b70ca4b18a5
longluo <calmbody@163.com>
2022-04-02 18:55:11 +0800

    633 Binary Search

--- Java/src/com/longluo/leetcode/math/Problem633_sumOfSquareNumbers.java
+++ Java/src/com/longluo/leetcode/math/Problem633_sumOfSquareNumbers.java
@@ -2,6 +2,7 @@ package com.longluo.leetcode.math;
 
 /**
  * 633. 平方数之和
+ * <p>
  * 给定一个非负整数c，你要判断是否存在两个整数 a 和 b，使得 a^2 + b^2 = c 。
  * <p>
  * 示例 1：
@@ -32,7 +33,8 @@ package com.longluo.leetcode.math;
  */
 public class Problem633_sumOfSquareNumbers {
 
-    public static boolean judgeSquareSum(int c) {
+    // Math.sqrt time: O(sqrt(n)) space: O(1)
+    public static boolean judgeSquareSum_sqrt(int c) {
         if (c < 0) {
             return false;
         } else if (c <= 2) {
@@ -49,12 +51,49 @@ public class Problem633_sumOfSquareNumbers {
         return false;
     }
 
-    public static boolean judgeSquareSum_tp(int c) {
-        int left = 0;
-        int right = (int) Math.ceil(Math.sqrt(c));
+    // Math.sqrt time: O(sqrt(n)) space: O(1)
+    public static boolean judgeSquareSum_sqrt_opt(int c) {
+        if (c < 0) {
+            return false;
+        } else if (c <= 2) {
+            return true;
+        }
 
+        for (long i = 0; i * i <= c; i++) {
+            int remain = (int) Math.sqrt(c - i * i);
+            if (remain * remain == c - i * i) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // BF Sqrt
+    public static boolean judgeSquareSum_bf_sqrt_opt(int c) {
+        if (c < 0) {
+            return false;
+        } else if (c <= 2) {
+            return true;
+        }
+
+        int rightMargin = (int) (Math.sqrt(0.5) * Math.sqrt(c));
+        for (long i = 0; i <= rightMargin; i++) {
+            double remain = Math.sqrt(c - i * i);
+            if ((int) remain == remain) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Two Pointers time: O(sqrt(n)) space: O(1)
+    public static boolean judgeSquareSum_tp(int c) {
+        long left = 0;
+        long right = (int) Math.ceil(Math.sqrt(c));
         while (left <= right) {
-            int sum = left * left + right * right;
+            long sum = left * left + right * right;
             if (sum > c) {
                 right--;
             } else if (sum < c) {
@@ -67,24 +106,49 @@ public class Problem633_sumOfSquareNumbers {
         return false;
     }
 
+    // BS time: O(sqrt(n)) space: O(1)
+    public static boolean judgeSquareSum_bs(int c) {
+        long left = 0;
+        long right = (long) Math.sqrt(c);
+        long mid = left + (right - left) / 2;
+        while (left <= right) {
+            long sum = left * left + right * right;
+            if (sum == c) {
+                return true;
+            } else if (sum > c) {
+                right--;
+                if (left * left + mid * mid > c) {
+                    right = mid - 1;
+                }
+            } else {
+                left++;
+            }
+
+            mid = left + (right - left) / 2;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("true ?= " + judgeSquareSum(0));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(0));
+        System.out.println("true ?= " + judgeSquareSum_sqrt_opt(8));
         System.out.println("true ?= " + judgeSquareSum_tp(0));
-        System.out.println("true ?= " + judgeSquareSum(1));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(1));
         System.out.println("true ?= " + judgeSquareSum_tp(1));
-        System.out.println("true ?= " + judgeSquareSum(2));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(2));
         System.out.println("true ?= " + judgeSquareSum_tp(2));
-        System.out.println("false ?= " + judgeSquareSum(3));
+        System.out.println("false ?= " + judgeSquareSum_sqrt(3));
         System.out.println("false ?= " + judgeSquareSum_tp(3));
-        System.out.println("true ?= " + judgeSquareSum(4));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(4));
         System.out.println("true ?= " + judgeSquareSum_tp(4));
-        System.out.println("true ?= " + judgeSquareSum(5));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(5));
         System.out.println("true ?= " + judgeSquareSum_tp(5));
-        System.out.println("true ?= " + judgeSquareSum(8));
+        System.out.println("true ?= " + judgeSquareSum_sqrt(8));
         System.out.println("true ?= " + judgeSquareSum_tp(8));
-        System.out.println("false ?= " + judgeSquareSum(999999999));
+        System.out.println("false ?= " + judgeSquareSum_sqrt(999999999));
         System.out.println("false ?= " + judgeSquareSum_tp(999999999));
-        System.out.println("false ?= " + judgeSquareSum(2147482647));
+        System.out.println("false ?= " + judgeSquareSum_sqrt(2147482647));
         System.out.println("false ?= " + judgeSquareSum_tp(2147482647));
     }
 }
