edcf17400d57ee4359829dd2df71c7ce543ad3e5
longluo <calmbody@163.com>
2022-04-06 21:11:13 +0800

    64 DP

---
+++ Java/src/com/longluo/leetcode/dp/Problem64_minimumPathSum.java
@@ -0,0 +1,55 @@
+package com.longluo.leetcode.dp;
+
+/**
+ * 64. 最小路径和
+ * <p>
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ * <p>
+ * 示例 1：
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ * <p>
+ * 示例 2：
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ * <p>
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 100
+ * <p>
+ * https://leetcode-cn.com/problems/minimum-path-sum/
+ */
+public class Problem64_minimumPathSum {
+
+    // DP time: O(n) space: O(n)
+    public static int minPathSum_dp(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j] + grid[i][j], dp[i][j - 1] + grid[i][j]);
+            }
+        }
+
+        return dp[row - 1][col - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("7 ?= " + minPathSum_dp(new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}));
+    }
+}
