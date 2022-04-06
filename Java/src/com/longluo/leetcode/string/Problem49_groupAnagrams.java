3edf84ef249ddbca492181a82895fddb54c0649e
longluo <calmbody@163.com>
2022-04-06 23:04:13 +0800

    49 Hash

--- Java/src/com/longluo/leetcode/string/Problem49_groupAnagrams.java
+++ Java/src/com/longluo/leetcode/string/Problem49_groupAnagrams.java
@@ -4,46 +4,85 @@ import java.util.*;
 
 /**
  * 49. 字母异位词分组
- * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
  * <p>
- * 示例:
- * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
- * 输出:
- * [
- * ["ate","eat","tea"],
- * ["nat","tan"],
- * ["bat"]
- * ]
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
  * <p>
- * 说明：
- * 所有输入均为小写字母。
- * 不考虑答案输出的顺序。
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * <p>
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ * <p>
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ * <p>
+ * 提示：
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ * <p>
+ * https://leetcode-cn.com/problems/group-anagrams/
  */
 public class Problem49_groupAnagrams {
 
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String str : strs) {
-            int[] counts = new int[26];
-            char[] array = str.toCharArray();
-            for (int i = 0; i < array.length; i++) {
-                counts[array[i] - 'a']++;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < counts.length; i++) {
-                sb.append((char)('a' + i));
-                sb.append(counts[i]);
-            }
-            
+    // Hash time: O(n^2) space: O(n)
+    public static List<List<String>> groupAnagrams_hash(String[] strs) {
+        List<List<String>> ans = new ArrayList<>();
+        if (strs == null || strs.length == 0) {
+            return ans;
         }
 
-        return new ArrayList<>();
+        int len = strs.length;
+        boolean[] visited = new boolean[len];
+        for (int i = 0; i < len; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            List<String> list = new ArrayList<>();
+            String word = strs[i];
+            list.add(word);
+            visited[i] = true;
+            for (int j = i + 1; j < len; j++) {
+                if (checkAnagram(word, strs[j]) && !visited[j]) {
+                    list.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.add(list);
+        }
+
+        return ans;
+    }
+
+    public static boolean checkAnagram(String strA, String strB) {
+        if (strA.length() != strB.length()) {
+            return false;
+        }
+
+        int[] cntA = new int[26];
+        int[] cntB = new int[26];
+        for (int i = 0; i < strA.length(); i++) {
+            cntA[strA.charAt(i) - 'a']++;
+            cntB[strB.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (cntA[i] != cntB[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(" ?= " + groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
-
+        System.out.println(" ?= " + groupAnagrams_hash(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(" ?= " + groupAnagrams_hash(new String[]{"", ""}));
     }
 }
