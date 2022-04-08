d211d537601056dbfefadfc9beb5e09a8b1dafca
longluo <calmbody@163.com>
2022-04-08 21:05:09 +0800

    1603 Simulate

--- Java/src/com/longluo/studyplan/programming_skills/Problem1603_designParkingSystem.java
+++ Java/src/com/longluo/studyplan/programming_skills/Problem1603_designParkingSystem.java
@@ -1,12 +1,13 @@
-package com.longluo.leetcode.design;
+package com.longluo.studyplan.programming_skills;
 
 /**
  * 1603. 设计停车系统
+ * <p>
  * 请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+ * <p>
  * 请你实现 ParkingSystem 类：
  * ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
- * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。
- * 一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+ * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
  * <p>
  * 示例 1：
  * 输入：
@@ -26,9 +27,12 @@ package com.longluo.leetcode.design;
  * 0 <= big, medium, small <= 1000
  * carType 取值为 1， 2 或 3
  * 最多会调用 addCar 函数 1000 次
+ * <p>
+ * https://leetcode-cn.com/problems/design-parking-system/
  */
 public class Problem1603_designParkingSystem {
 
+    // Simulate time: O(1) space: O(1)
     class ParkingSystem {
         int[] carParkingLot = new int[3];
 
@@ -41,5 +45,9 @@ public class Problem1603_designParkingSystem {
         public boolean addCar(int carType) {
             return carParkingLot[carType - 1]-- > 0;
         }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
