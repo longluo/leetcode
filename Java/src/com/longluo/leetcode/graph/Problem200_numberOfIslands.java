86f00be36b0ecf6123a5747d4b4af5e6ce5f9b2c
longluo <calmbody@163.com>
2022-04-10 15:04:40 +0800

    200 BFS

--- Java/src/com/longluo/leetcode/graph/Problem200_numberOfIslands.java
+++ Java/src/com/longluo/leetcode/graph/Problem200_numberOfIslands.java
@@ -1,5 +1,8 @@
 package com.longluo.leetcode.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 200. 岛屿数量
  * <p>
@@ -37,12 +40,48 @@ package com.longluo.leetcode.graph;
  */
 public class Problem200_numberOfIslands {
 
-    public static int numIslands(char[][] grid) {
+    // BFS  time: O(m*n) space: O(m*n)
+    public static int numIslands_bfs(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
 
-        return 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int ans = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    bfs(grid, visited, i, j);
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void bfs(char[][] grid, boolean[][] visited, int x, int y) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[x][y] = true;
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
+            for (int i = 0; i < dirs.length; i++) {
+                int nextX = pos[0] + dirs[i][0];
+                int nextY = pos[1] + dirs[i][1];
+                if (nextX >= 0 && nextX < grid.length && nextY >= 0 && nextY < grid[0].length
+                        && !visited[nextX][nextY] && grid[nextX][nextY] == '1') {
+                    queue.offer(new int[]{nextX, nextY});
+                    visited[nextX][nextY] = true;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        System.out.println("1 ?= " + numIslands_bfs(new char[][]{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}}));
     }
 }
