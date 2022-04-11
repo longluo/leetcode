880e5c7c511cf1238a937ed3cdcf396b955eb124
longluo <calmbody@163.com>
2022-04-11 22:51:59 +0800

    700 Recursion

---
+++ Cpp/leetcode/editor/cn/700_search-in-a-binary-search-tree.cpp
@@ -0,0 +1,75 @@
+//给定二叉搜索树（BST）的根节点 root 和一个整数值 val。 
+//
+// 你需要在 BST 中找到节点值等于 val 的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 null 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：root = [4,2,7,1,3], val = 2
+//输出：[2,1,3]
+// 
+//
+// Example 2: 
+//
+// 
+//输入：root = [4,2,7,1,3], val = 5
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 数中节点数在 [1, 5000] 范围内 
+// 1 <= Node.val <= 10⁷ 
+// root 是二叉搜索树 
+// 1 <= val <= 10⁷ 
+// 
+// Related Topics 树 二叉搜索树 二叉树 👍 264 👎 0
+
+
+// 2022-04-11 22:36:48
+// By Long Luo
+
+#include <bits/stdc++.h>
+#include <TreeNode.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Recursion
+    TreeNode *searchBST(TreeNode *root, int val) {
+        if (root == NULL || root->val == val) {
+            return root;
+        } else if (root->val > val) {
+            return searchBST(root->left, val);
+        } else {
+            return searchBST(root->right, val);
+        }
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+
+}
