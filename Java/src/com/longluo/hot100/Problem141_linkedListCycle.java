007ecf7c8786fd5a576ff49f9ce507dbbc309cc0
longluo <calmbody@163.com>
2022-04-11 21:35:59 +0800

    141 Another Two Pointers

--- Java/src/com/longluo/hot100/Problem141_linkedListCycle.java
+++ Java/src/com/longluo/hot100/Problem141_linkedListCycle.java
@@ -78,6 +78,28 @@ public class Problem141_linkedListCycle {
         return true;
     }
 
+    // Two Pointers time: O(n) space: O(1)
+    // AC
+    public static boolean hasCycle_fast_other(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+
+
     public static void main(String[] args) {
 
     }
