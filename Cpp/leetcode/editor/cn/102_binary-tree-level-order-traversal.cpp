6c928a8fb830fe0072d2cd7f020cae880015afab
longluo <calmbody@163.com>
2022-04-12 07:31:50 +0800

    102 BFS

---
+++ Cpp/leetcode/editor/cn/102_binary-tree-level-order-traversal.cpp
@@ -0,0 +1,95 @@
+//给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[9,20],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 1276 👎 0
+
+
+// 2022-04-12 07:19:46
+// By Long Luo
+
+#include <bits/stdc++.h>
+#include "TreeNode.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // BFS
+    vector<vector<int>> levelOrder(TreeNode *root) {
+        if (root == NULL) {
+            return vector<vector<int>>();
+        }
+
+        queue<TreeNode*> queue;
+        queue.push(root);
+        vector<vector<int>> ans;
+        while (!queue.empty()) {
+            int size = queue.size();
+            vector<int> level;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = queue.front();
+                queue.pop();
+                level.push_back(node->val);
+                if (node->left != NULL) {
+                    queue.push(node->left);
+                }
+                if (node->right != NULL) {
+                    queue.push(node->right);
+                }
+            }
+
+            ans.push_back(level);
+        }
+
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+}
