e6b8d01b5542818e97f993608927580495464381
longluo <calmbody@163.com>
2022-04-13 22:30:21 +0800

    110 Recursion time: O(n^2) space: O(n)

---
+++ Cpp/leetcode/editor/cn/110_balanced-binary-tree.cpp
@@ -0,0 +1,89 @@
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在范围 [0, 5000] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+// Related Topics 树 深度优先搜索 二叉树 👍 970 👎 0
+
+
+// 2022-04-13 22:23:33
+// By Long Luo
+
+#include <bits/stdc++.h>
+#include <TreeNode.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // Recursion time: O(n^2) space: O(n)
+    bool isBalanced(TreeNode *root) {
+        if (root == NULL) {
+            return true;
+        } else {
+            return isBalanced(root->left) && isBalanced(root->right)
+                   && abs(maxHeight(root->left) - maxHeight(root->right)) <= 1;
+        }
+    }
+
+    int maxHeight(TreeNode *root) {
+        if (root == NULL) {
+            return 0;
+        }
+
+        return max(maxHeight(root->left), maxHeight(root->right)) + 1;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+}
