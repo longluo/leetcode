4f8f43bb052fb1208b12d4e9ca4a4f4f2012401d
longluo <calmbody@163.com>
2022-04-13 11:23:24 +0800

    136 XOR

---
+++ Cpp/leetcode/editor/cn/136_single-number.cpp
@@ -0,0 +1,47 @@
+//给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。 
+//
+// 说明： 
+//
+// 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 示例 1: 
+//
+// 输入: [2,2,1]
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: [4,1,2,1,2]
+//输出: 4 
+// Related Topics 位运算 数组 👍 2364 👎 0
+
+
+// 2022-04-13 11:18:08
+// By Long Luo
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    // XOR time: O(n) space: O(1)
+    int singleNumber(vector<int> &nums) {
+        int ans = 0;
+        for (auto x : nums) {
+            ans = ans ^ x;
+        }
+
+        return ans;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> data{2, 2, 1};
+    cout << "2 ?= " << s.singleNumber(data) << endl;
+}
