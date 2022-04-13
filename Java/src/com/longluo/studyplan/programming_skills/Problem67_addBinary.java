3915d5537d5606ff9e3299b0391ce09ababbf774
longluo <calmbody@163.com>
2022-04-13 18:36:34 +0800

    67 AddBinary Simulate

--- Java/src/com/longluo/studyplan/programming_skills/Problem67_addBinary.java
+++ Java/src/com/longluo/studyplan/programming_skills/Problem67_addBinary.java
@@ -1,5 +1,26 @@
 package com.longluo.studyplan.programming_skills;
 
+/**
+ * 67. 二进制求和
+ * <p>
+ * 给你两个二进制字符串，返回它们的和（用二进制表示）。
+ * 输入为 非空 字符串且只包含数字 1 和 0。
+ * <p>
+ * 示例 1:
+ * 输入: a = "11", b = "1"
+ * 输出: "100"
+ * <p>
+ * 示例 2:
+ * 输入: a = "1010", b = "1011"
+ * 输出: "10101"
+ * <p>
+ * 提示：
+ * 每个字符串仅由字符 '0' 或 '1' 组成。
+ * 1 <= a.length, b.length <= 10^4
+ * 字符串如果不是 "0" ，就都不含前导零。
+ * <p>
+ * https://leetcode-cn.com/problems/add-binary/
+ */
 public class Problem67_addBinary {
 
     // BF API time: O(n) space: O(1)
@@ -11,19 +32,17 @@ public class Problem67_addBinary {
         return Integer.toBinaryString(ans);
     }
 
-    // BF time: O(n) space: O(1)
-    public static String addBinary_bf_bit(String a, String b) {
-        int lenA = a.length();
-        int lenB = b.length();
-        int p = lenA - 1;
-        int q = lenB - 1;
+    // BF Simulate time: O(n) space: O(1)
+    public static String addBinary_simu(String a, String b) {
+        int p = a.length() - 1;
+        int q = b.length() - 1;
         int carry = 0;
         StringBuilder sb = new StringBuilder();
         while (p >= 0 || q >= 0) {
             int numA = p >= 0 ? a.charAt(p) - '0' : 0;
             int numB = q >= 0 ? b.charAt(q) - '0' : 0;
             int sum = numA + numB + carry;
-            carry = sum > 1 ? 1 : 0;
+            carry = sum / 2;
             sb.append(sum % 2);
             p--;
             q--;
@@ -40,7 +59,7 @@ public class Problem67_addBinary {
         System.out.println("100 ?= " + addBinary_api("11", "1"));
         System.out.println("10101 ?= " + addBinary_api("1010", "1011"));
 
-        System.out.println("100 ?= " + addBinary_bf_bit("11", "1"));
-        System.out.println("10101 ?= " + addBinary_bf_bit("1010", "1011"));
+        System.out.println("100 ?= " + addBinary_simu("11", "1"));
+        System.out.println("10101 ?= " + addBinary_simu("1010", "1011"));
     }
 }
