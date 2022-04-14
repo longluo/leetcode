dacf40e85bd87a3089a6652bf5a52ed13390bf16
longluo <calmbody@163.com>
2022-04-14 11:29:45 +0800

    14 Substring + Sort

--- Java/src/com/longluo/top_interviews/Problem14_longestCommonPrefix.java
+++ Java/src/com/longluo/top_interviews/Problem14_longestCommonPrefix.java
@@ -27,33 +27,58 @@ import java.util.Comparator;
  */
 public class Problem14_longestCommonPrefix {
 
+    // BF Substring time: O(n^2) space: O(1)
+    public static String longestCommonPrefix_bf(String[] strs) {
+        int len = strs.length;
+        String str = strs[0];
+        String ans = "";
+        int maxLen = 0;
+        for (int i = 1; i <= str.length(); i++) {
+            String prefix = str.substring(0, i);
+            boolean match = true;
+            for (int j = 1; j < len; j++) {
+                if (!strs[j].startsWith(prefix)) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match && prefix.length() > maxLen) {
+                maxLen = prefix.length();
+                ans = prefix;
+            }
+        }
+
+        return ans;
+    }
+
+    // Sort time: O(n^2) space: O(1)
     public static String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
         }
 
         String ans = "";
-        Arrays.sort(strs, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if (o1.length() == o2.length()) {
-                    return o1.compareTo(o2);
-                }
-                return o1.length() - o2.length();
+        // Length as the first
+        Arrays.sort(strs, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
             }
+            return o1.length() - o2.length();
         });
+
         int len = strs.length;
         for (int i = strs[0].length(); i >= 1; i--) {
             String prefix = strs[0].substring(0, i);
-            boolean flag = true;
+            boolean match = true;
             for (int j = 1; j < len; j++) {
                 if (!strs[j].startsWith(prefix)) {
-                    flag = false;
+                    match = false;
                     break;
                 }
             }
 
-            if (flag) {
+            if (match) {
                 ans = prefix;
                 return ans;
             }
@@ -63,6 +88,9 @@ public class Problem14_longestCommonPrefix {
     }
 
     public static void main(String[] args) {
+        System.out.println("a ?= " + longestCommonPrefix_bf(new String[]{"a"}));
+        System.out.println("a ?= " + longestCommonPrefix_bf(new String[]{"a", "ab"}));
+        System.out.println("fl ?= " + longestCommonPrefix_bf(new String[]{"flower", "flow", "flight"}));
         System.out.println("fl ?= " + longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
         System.out.println(" ?= " + longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
     }
