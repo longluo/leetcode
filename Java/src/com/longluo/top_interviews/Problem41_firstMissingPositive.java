f85b99e9b6ecee51b31bdde92eaef9a4cb03cfd2
longluo <calmbody@163.com>
2022-04-17 10:42:53 +0800

    41 BF timeout solution

---
+++ Java/src/com/longluo/top_interviews/Problem41_firstMissingPositive.java
@@ -0,0 +1,56 @@
+package com.longluo.top_interviews;
+
+/**
+ * 41. 缺失的第一个正数
+ * <p>
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ * <p>
+ * 请你实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案。
+ * <p>
+ * 示例 1：
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * <p>
+ * 示例 2：
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * <p>
+ * 示例 3：
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ * <p>
+ * 提示：
+ * 1 <= nums.length <= 5 * 10^5
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * <p>
+ * https://leetcode-cn.com/problems/first-missing-positive/
+ */
+public class Problem41_firstMissingPositive {
+
+    // BF time: O(n^2) space: O(1)
+    // TimeOut
+    public static int firstMissingPositive_bf(int[] nums) {
+        int ans = 1;
+        int len = nums.length;
+        for (int i = 1; i > 0; i++) {
+            boolean flag = false;
+            for (int j = 0; j < len; j++) {
+                if (nums[j] == i) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag) {
+                ans = i;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 ?= " + firstMissingPositive_bf(new int[]{3, 4, -1, 1}));
+    }
+}
