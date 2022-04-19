eea0585fbf1f57c4b7e68e31182de39f87bec831
longluo <calmbody@163.com>
2022-04-19 13:25:00 +0800

    611 BinarySearch

--- Java/src/com/longluo/studyplan/binary_search/Problem611_validTriangleNumber.java
+++ Java/src/com/longluo/studyplan/binary_search/Problem611_validTriangleNumber.java
@@ -58,7 +58,7 @@ public class Problem611_validTriangleNumber {
         return ans;
     }
 
-    // Sort + BF time: O(n^3) space: O(logn)
+    // Sort + Greedy time: O(n^3) space: O(logn)
     // AC
     public static int triangleNumber_sort(int[] nums) {
         if (nums == null || nums.length <= 2) {
@@ -89,10 +89,60 @@ public class Problem611_validTriangleNumber {
         return ans;
     }
 
+    // Sort + BinarySearch time: O(n^2 * logn) space: O(logn)
+    public static int triangleNumber_bs(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        int ans = 0;
+        int len = nums.length;
+        for (int i = 0; i < len - 2; i++) {
+            if (nums[i] <= 0) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 1; j++) {
+                if (nums[j] <= 0) {
+                    continue;
+                }
+
+                int sum = nums[i] + nums[j];
+                int right = binarySearch(nums, j + 1, sum);
+                if (right > j) {
+                    ans += right - j;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static int binarySearch(int[] nums, int start, int target) {
+        int len = nums.length;
+        if (start > len - 1) {
+            return -1;
+        }
+
+        int left = start;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return nums[left] < target ? left : -1;
+    }
+
     public static void main(String[] args) {
         System.out.println("0 ?= " + triangleNumber_bf(new int[]{7, 0, 0, 0}));
         System.out.println("10 ?= " + triangleNumber_bf(new int[]{24, 3, 82, 22, 35, 84, 19}));
         System.out.println("3 ?= " + triangleNumber_bf(new int[]{2, 2, 3, 4}));
         System.out.println("3 ?= " + triangleNumber_sort(new int[]{2, 2, 3, 4}));
+        System.out.println("3 ?= " + triangleNumber_bs(new int[]{2, 2, 3, 4}));
     }
 }
