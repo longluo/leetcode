efa5654e4a241058bcdd9293db90e0355d764c01
longluo <calmbody@163.com>
2022-04-20 10:58:33 +0800

    107 BFS + LinkedList

--- Java/src/com/longluo/leetcode/tree/Problem107_binaryTreeLevelOrderTraversal_ii.java
+++ Java/src/com/longluo/leetcode/tree/Problem107_binaryTreeLevelOrderTraversal_ii.java
@@ -60,6 +60,35 @@ public class Problem107_binaryTreeLevelOrderTraversal_ii {
         return ans;
     }
 
+    // BFS + LinkedList time: O(n) space: O(n)
+    public static List<List<Integer>> levelOrderBottom_link(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> levelList = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            ans.add(0, levelList);
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
 
     }
