c2768639462f89b14afbc537de4129653ae0d6c4
longluo <calmbody@163.com>
2022-04-28 16:10:34 +0800

    1705 Heap PriorityQueue

--- Java/src/com/longluo/leetcode/greedy/Problem1705_eatenApples.java
+++ Java/src/com/longluo/leetcode/greedy/Problem1705_eatenApples.java
@@ -1,5 +1,9 @@
 package com.longluo.leetcode.greedy;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 1705. 吃苹果的最大数目
  * <p>
@@ -37,38 +41,90 @@ package com.longluo.leetcode.greedy;
  */
 public class Problem1705_eatenApples {
 
+    // Failed Not AC
+    public static int eatenApples_bf(int[] apples, int[] days) {
+        int len = apples.length;
+        int ans = 0;
+        int[][] sorted = new int[len][2];
+        for (int i = 0; i < len; i++) {
+            sorted[i][0] = apples[i];
+            sorted[i][1] = i + days[i];
+        }
+
+        Arrays.sort(sorted, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] > 0 && o2[0] > 0) {
+                    return o1[1] - o2[1];
+                }
+
+                return o1[0] - o2[0];
+            }
+        });
+
+        int time = 0;
+        int idx = 0;
+        while (idx < len) {
+            while (idx < len && (sorted[idx][0] <= 0 || sorted[idx][1] <= time)) {
+                idx++;
+            }
+
+            if (idx < len && sorted[idx][1] > time) {
+                sorted[idx][0]--;
+                ans++;
+            }
+
+            time++;
+        }
+
+        return ans;
+    }
+
+    // Greedy + PriorityQueue time: O(nlogn) space: O(n)
     public static int eatenApples(int[] apples, int[] days) {
         if (apples == null || apples.length == 0 || days == null || days.length == 0) {
             return 0;
         }
 
-        int ans = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
         int len = apples.length;
-        int day = 1;
-        int idx = 0;
-        while (day < 50000) {
-            while (idx < len) {
-                if (apples[idx] > 0) {
-                    if (idx + days[idx] > day) {
-                        int eat = Math.min(idx + days[idx] - day, apples[idx]);
-                        ans += eat;
-                        day += eat;
-                        idx++;
-                    }
-                } else {
-                    idx++;
+        int ans = 0;
+        int time = 0;
+        while (time < len || !pq.isEmpty()) {
+            if (time < len && apples[time] > 0) {
+                pq.offer(new int[]{apples[time], days[time] + time});
+            }
+
+            while (!pq.isEmpty() && pq.peek()[1] <= time) {
+                pq.poll();
+            }
+
+            if (!pq.isEmpty()) {
+                int[] curr = pq.poll();
+                curr[0]--;
+                ans++;
+                if (curr[0] > 0 && curr[1] > time) {
+                    pq.offer(curr);
                 }
             }
-            if (idx == len) {
-                break;
-            }
+
+            time++;
         }
 
         return ans;
     }
 
     public static void main(String[] args) {
+        System.out.println("7 ?= " + eatenApples_bf(new int[]{1, 2, 3, 5, 2}, new int[]{3, 2, 1, 4, 2}));
+        System.out.println("8 ?= " + eatenApples_bf(new int[]{2, 1, 1, 4, 5}, new int[]{10, 10, 6, 4, 2}));
         System.out.println("7 ?= " + eatenApples(new int[]{1, 2, 3, 5, 2}, new int[]{3, 2, 1, 4, 2}));
+        System.out.println("5 ?= " + eatenApples_bf(new int[]{3, 0, 0, 0, 0, 2}, new int[]{3, 0, 0, 0, 0, 2}));
         System.out.println("5 ?= " + eatenApples(new int[]{3, 0, 0, 0, 0, 2}, new int[]{3, 0, 0, 0, 0, 2}));
     }
 }
