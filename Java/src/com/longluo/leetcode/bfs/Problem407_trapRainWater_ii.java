6ad43ecdad45fb3e08b4b470b87a835eb2b6570c
longluo <calmbody@163.com>
2022-04-29 20:48:49 +0800

    407 PriorityQueue

--- Java/src/com/longluo/leetcode/bfs/Problem407_trapRainWater_ii.java
+++ Java/src/com/longluo/leetcode/bfs/Problem407_trapRainWater_ii.java
@@ -1,6 +1,8 @@
 package com.longluo.leetcode.bfs;
 
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -27,60 +29,79 @@ import java.util.Queue;
  */
 public class Problem407_trapRainWater_ii {
 
-    public static int trapRainWater(int[][] heightMap) {
-        int m = heightMap.length;
-        int n = heightMap[0].length;
-        int[] dirs = {-1, 0, 1, 0, -1};
+    //
+    public static int trapRainWater_bf(int[][] heightMap) {
+        int row = heightMap.length;
+        int col = heightMap[0].length;
         int maxHeight = 0;
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        int ans = 0;
+        int sum = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 maxHeight = Math.max(maxHeight, heightMap[i][j]);
+                sum += heightMap[i][j];
             }
         }
-        int[][] water = new int[m][n];
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                water[i][j] = maxHeight;
+
+        int fullWater = maxHeight * row * col;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
             }
         }
-        Queue<int[]> qu = new LinkedList<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
-                    if (water[i][j] > heightMap[i][j]) {
-                        water[i][j] = heightMap[i][j];
-                        qu.offer(new int[]{i, j});
-                    }
+
+        return ans;
+    }
+
+    // PriorityQueue time: O(m * n) space: O(m * n)
+    public static int trapRainWater_pq(int[][] heightMap) {
+        if (heightMap == null || heightMap.length <= 1 || heightMap[0].length <= 1) {
+            return 0;
+        }
+
+        int ans = 0;
+        int row = heightMap.length;
+        int col = heightMap[0].length;
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
+
+        boolean[][] visited = new boolean[row][col];
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1) {
+                    pq.offer(new int[]{i, j, heightMap[i][j]});
+                    visited[i][j] = true;
                 }
             }
         }
-        while (!qu.isEmpty()) {
-            int[] curr = qu.poll();
-            int x = curr[0];
-            int y = curr[1];
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + dirs[i], ny = y + dirs[i + 1];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+
+            for (int[] dir : dirs) {
+                int nextX = curr[0] + dir[0];
+                int nextY = curr[1] + dir[1];
+
+                if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col || visited[nextX][nextY]) {
                     continue;
                 }
-                if (water[x][y] < water[nx][ny] && water[nx][ny] > heightMap[nx][ny]) {
-                    water[nx][ny] = Math.max(water[x][y], heightMap[nx][ny]);
-                    qu.offer(new int[]{nx, ny});
+
+                if (heightMap[nextX][nextY] < curr[2]) {
+                    ans += curr[2] - heightMap[nextX][nextY];
                 }
+
+                visited[nextX][nextY] = true;
+                pq.offer(new int[]{nextX, nextY, Math.max(heightMap[nextX][nextY], curr[2])});
             }
         }
 
-        int res = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                res += water[i][j] - heightMap[i][j];
-            }
-        }
-        return res;
+        return ans;
     }
 
     public static void main(String[] args) {
-
+        System.out.println("4 ?= " + trapRainWater_pq(new int[][]{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}));
+        System.out.println("10 ?= " + trapRainWater_pq(new int[][]{{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}}));
     }
 }
