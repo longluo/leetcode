c019dfed0be3cc701659470022a3dd763525f64b
longluo <calmbody@163.com>
2022-04-29 22:21:35 +0800

    407 BFS

--- Java/src/com/longluo/leetcode/bfs/Problem407_trapRainWater_ii.java
+++ Java/src/com/longluo/leetcode/bfs/Problem407_trapRainWater_ii.java
@@ -29,31 +29,7 @@ import java.util.Queue;
  */
 public class Problem407_trapRainWater_ii {
 
-    //
-    public static int trapRainWater_bf(int[][] heightMap) {
-        int row = heightMap.length;
-        int col = heightMap[0].length;
-        int maxHeight = 0;
-        int ans = 0;
-        int sum = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                maxHeight = Math.max(maxHeight, heightMap[i][j]);
-                sum += heightMap[i][j];
-            }
-        }
-
-        int fullWater = maxHeight * row * col;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-
-            }
-        }
-
-        return ans;
-    }
-
-    // PriorityQueue time: O(m * n) space: O(m * n)
+    // PriorityQueue time: O(MNlog(M+N)) space: O(MN)
     public static int trapRainWater_pq(int[][] heightMap) {
         if (heightMap == null || heightMap.length <= 1 || heightMap[0].length <= 1) {
             return 0;
@@ -100,8 +76,76 @@ public class Problem407_trapRainWater_ii {
         return ans;
     }
 
+    // BFS time: O(M^2 * N^2) space: O(MN)
+    public static int trapRainWater_bfs(int[][] heightMap) {
+        if (heightMap == null || heightMap.length <= 1 || heightMap[0].length <= 1) {
+            return 0;
+        }
+
+        int ans = 0;
+        int row = heightMap.length;
+        int col = heightMap[0].length;
+        int maxHeight = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                maxHeight = Math.max(maxHeight, heightMap[i][j]);
+            }
+        }
+        int[][] water = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                water[i][j] = maxHeight;
+            }
+        }
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1) {
+                    if (water[i][j] > heightMap[i][j]) {
+                        water[i][j] = heightMap[i][j];
+                        queue.offer(new int[]{i, j});
+                    }
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+
+            for (int[] dir : dirs) {
+                int nextX = curr[0] + dir[0];
+                int nextY = curr[1] + dir[1];
+
+                if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col) {
+                    continue;
+                }
+
+                // 只有比当前水位高才会流出水，water[nextX][nextY] > heightMap[nextX][nextY]是为了保证不会重复遍历。
+                if (water[curr[0]][curr[1]] < water[nextX][nextY] && water[nextX][nextY] > heightMap[nextX][nextY]) {
+                    water[nextX][nextY] = Math.max(water[curr[0]][curr[1]], heightMap[nextX][nextY]);
+                    queue.offer(new int[]{nextX, nextY});
+                }
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                ans += water[i][j] - heightMap[i][j];
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("4 ?= " + trapRainWater_pq(new int[][]{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}));
-        System.out.println("10 ?= " + trapRainWater_pq(new int[][]{{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}}));
+        System.out.println("4 ?= " + trapRainWater_bfs(new int[][]{{1, 4, 3, 1, 3, 2}, {3, 2, 1, 3, 2, 4}, {2, 3, 3, 2, 3, 1}}));
+       System.out.println("10 ?= " + trapRainWater_pq(new int[][]{{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}}));
+        System.out.println("10 ?= " + trapRainWater_bfs(new int[][]{{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}}));
+        System.out.println("3 ?= " + trapRainWater_bfs(new int[][]{{5, 5, 5, 1}, {5, 1, 1, 5}, {5, 1, 5, 5}, {5, 2, 5, 8}}));
+        // System.out.println("3 ?= " + trapRainWater_bfs_marked(new int[][]{{5, 5, 5, 1}, {5, 1, 1, 5}, {5, 1, 5, 5}, {5, 2, 5, 8}}));
     }
 }
