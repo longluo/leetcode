122868c7f95d1416d8ec5103f52820d301dd8f8d
longluo <calmbody@163.com>
2022-05-02 08:50:45 +0800

    128 Sort + Hash

--- Java/src/com/longluo/top100/Problem128_longestConsecutiveSequence.java
+++ Java/src/com/longluo/top100/Problem128_longestConsecutiveSequence.java
@@ -1,6 +1,6 @@
 package com.longluo.top100;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * 128. 最长连续序列
@@ -50,11 +50,39 @@ public class Problem128_longestConsecutiveSequence {
         return ans;
     }
 
+    // Hash time: O(nlogn) space: O(n)
+    public static int longestConsecutive_hash(int[] nums) {
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
+        }
+        int ans = 1;
+        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x : nums) {
+            if (map.containsKey(x + 1) || map.containsKey(x - 1)) {
+                int tempLen = Math.max(map.getOrDefault(x + 1, 1) + 1, map.getOrDefault(x - 1, 1) + 1);
+                map.put(x, tempLen);
+                ans = Math.max(ans, tempLen);
+            } else {
+                map.put(x, 1);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + longestConsecutive_bf(new int[]{0}));
         System.out.println("1 ?= " + longestConsecutive_bf(new int[]{0, 0}));
         System.out.println("3 ?= " + longestConsecutive_bf(new int[]{1, 2, 0, 1}));
         System.out.println("4 ?= " + longestConsecutive_bf(new int[]{100, 4, 200, 1, 3, 2}));
         System.out.println("9 ?= " + longestConsecutive_bf(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+
+        System.out.println("1 ?= " + longestConsecutive_hash(new int[]{0}));
+        System.out.println("1 ?= " + longestConsecutive_hash(new int[]{0, 0}));
+        System.out.println("3 ?= " + longestConsecutive_hash(new int[]{1, 2, 0, 1}));
+        System.out.println("4 ?= " + longestConsecutive_hash(new int[]{100, 4, 200, 1, 3, 2}));
+        System.out.println("9 ?= " + longestConsecutive_hash(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
     }
 }
