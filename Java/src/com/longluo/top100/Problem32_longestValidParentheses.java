55a3f7b4e415ff7eca663be9e75d108fd7cdfd1a
longluo <calmbody@163.com>
2022-05-03 08:59:16 +0800

    32 BF Ok

--- Java/src/com/longluo/top100/Problem32_longestValidParentheses.java
+++ Java/src/com/longluo/top100/Problem32_longestValidParentheses.java
@@ -1,5 +1,8 @@
 package com.longluo.top100;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 32. 最长有效括号
  * <p>
@@ -27,12 +30,56 @@ package com.longluo.top100;
  */
 public class Problem32_longestValidParentheses {
 
-    public int longestValidParentheses(String s) {
+    // BF time: O(n^3) space: O(n)
+    // TimeOut
+    public static int longestValidParentheses_bf(String s) {
+        if (s == null || s.length() <= 1) {
+            return 0;
+        }
 
-        return 0;
+        int len = s.length();
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == ')') {
+                continue;
+            }
+
+            for (int j = len; j >= i + 2; j--) {
+                if ((j - i) % 2 == 1) {
+                    continue;
+                }
+                String subStr = s.substring(i, j);
+                if (checkValid(subStr)) {
+                    ans = Math.max(ans, j - i);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static boolean checkValid(String s) {
+        if (s == null || s.length() <= 1 || s.length() % 2 == 1) {
+            return false;
+        }
+
+        Deque<Character> stk = new LinkedList<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                stk.push(ch);
+            } else if (ch == ')' && stk.isEmpty()) {
+                return false;
+            } else if (ch == ')') {
+                stk.pop();
+            }
+        }
+
+        return stk.isEmpty();
     }
 
     public static void main(String[] args) {
-
+        System.out.println("0 ?= " + longestValidParentheses_bf(""));
+        System.out.println("2 ?= " + longestValidParentheses_bf("(()"));
+        System.out.println("4 ?= " + longestValidParentheses_bf(")()())"));
     }
 }
