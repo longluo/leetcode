8868cf2db3c6b17a4fca4b0f66e39265b6069768
longluo <calmbody@163.com>
2022-05-05 15:09:20 +0800

    225 MyStack Opt

--- Java/src/com/longluo/leetcode/stack/Problem225_implementStackUsingQueues.java
+++ Java/src/com/longluo/leetcode/stack/Problem225_implementStackUsingQueues.java
@@ -83,6 +83,41 @@ public class Problem225_implementStackUsingQueues {
         }
     }
 
+    // MyStack Opt
+    static class MyStack_Opt {
+        Queue<Integer> queue1;
+        Queue<Integer> queue2;
+
+        public MyStack_Opt() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            queue2.offer(x);
+
+            while (!queue1.isEmpty()) {
+                queue2.add(queue1.poll());
+            }
+
+            Queue<Integer> temp = queue2;
+            queue2 = queue1;
+            queue1 = temp;
+        }
+
+        public int pop() {
+            return queue1.poll();
+        }
+
+        public int top() {
+            return queue1.peek();
+        }
+
+        public boolean empty() {
+            return queue1.isEmpty();
+        }
+    }
+
     /**
      * Your MyStack object will be instantiated and called as such:
      * MyStack obj = new MyStack();
