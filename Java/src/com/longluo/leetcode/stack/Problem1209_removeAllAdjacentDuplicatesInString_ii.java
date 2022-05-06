547584c04fcc6009c51a31da7de14e513f0bdbde
longluo <calmbody@163.com>
2022-05-06 22:05:28 +0800

    1209 Finish

--- Java/src/com/longluo/leetcode/stack/Problem1209_removeAllAdjacentDuplicatesInString_ii.java
+++ Java/src/com/longluo/leetcode/stack/Problem1209_removeAllAdjacentDuplicatesInString_ii.java
@@ -134,11 +134,35 @@ public class Problem1209_removeAllAdjacentDuplicatesInString_ii {
         return sb.toString();
     }
 
+    public static String removeDuplicates_stack_opt(String s, int k) {
+        if (s.length() < k) {
+            return s;
+        }
+
+        StringBuilder sb = new StringBuilder(s);
+        int count[] = new int[sb.length()];
+        for (int i = 0; i < sb.length(); i++) {
+            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+                count[i] = 1;
+            } else {
+                count[i] = count[i - 1] + 1;
+                if (count[i] == k) {
+                    sb.delete(i - k + 1, i + 1);
+                    i = i - k;
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("abcd ?= " + removeDuplicates("abcd", 2));
         System.out.println("aa ?= " + removeDuplicates("deeedbbcccbdaa", 3));
 
         System.out.println("abcd ?= " + removeDuplicates_stack("abcd", 2));
         System.out.println("aa ?= " + removeDuplicates_stack("deeedbbcccbdaa", 3));
+
+        System.out.println("aa ?= " + removeDuplicates_stack_opt("deeedbbcccbdaa", 3));
     }
 }
