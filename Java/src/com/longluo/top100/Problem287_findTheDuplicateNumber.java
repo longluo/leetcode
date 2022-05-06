1e5e7c18452ce803d47ac5276a7c2e69430d4654
longluo <calmbody@163.com>
2022-05-06 16:56:29 +0800

    287 9 Solutions

--- Java/src/com/longluo/top100/Problem287_findTheDuplicateNumber.java
+++ Java/src/com/longluo/top100/Problem287_findTheDuplicateNumber.java
@@ -1,42 +1,39 @@
-package com.longluo.leetcode.array;
+package com.longluo.top100;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 287. Find the Duplicate Number
+ * 287. 寻找重复数
  * <p>
- * Medium
+ * 给定一个包含 n + 1 个整数的数组 nums ，其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数。
+ * 假设 nums 只有 一个重复的整数 ，返回 这个重复的数 。
+ * 你设计的解决方案必须 不修改 数组 nums 且只用常量级 O(1) 的额外空间。
  * <p>
- * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
- * There is only one repeated number in nums, return this repeated number.
- * You must solve the problem without modifying the array nums and uses only constant extra space.
+ * 示例 1：
+ * 输入：nums = [1,3,4,2,2]
+ * 输出：2
  * <p>
- * Example 1:
- * Input: nums = [1,3,4,2,2]
- * Output: 2
+ * 示例 2：
+ * 输入：nums = [3,1,3,4,2]
+ * 输出：3
  * <p>
- * Example 2:
- * Input: nums = [3,1,3,4,2]
- * Output: 3
- * <p>
- * <p>
- * Constraints:
+ * 提示：
  * 1 <= n <= 10^5
  * nums.length == n + 1
  * 1 <= nums[i] <= n
- * All the integers in nums appear only once except for precisely one integer which appears two or more times.
+ * nums 中 只有一个整数 出现 两次或多次 ，其余整数均只出现 一次
  * <p>
- * Follow up:
- * How can we prove that at least one duplicate number must exist in nums?
- * Can you solve the problem in linear runtime complexity?
+ * 进阶：
+ * 如何证明 nums 中至少存在一个重复的数字?
+ * 你可以设计一个线性级时间复杂度 O(n) 的解决方案吗？
  * <p>
- * https://leetcode.com/problems/find-the-duplicate-number/
+ * https://leetcode-cn.com/problems/find-the-duplicate-number/
  */
 public class Problem287_findTheDuplicateNumber {
 
-    // 2 Loops
+    // 2 Loops time: O(n^2) space: O(1)
     public static int findDuplicate_2loops(int[] nums) {
         int len = nums.length;
         for (int i = 0; i < len; i++) {
@@ -50,7 +47,7 @@ public class Problem287_findTheDuplicateNumber {
         return len;
     }
 
-    // Count
+    // Count time: O(n) space: O(n)
     public static int findDuplicate_count(int[] nums) {
         int len = nums.length;
         int[] cnt = new int[len + 1];
@@ -64,7 +61,7 @@ public class Problem287_findTheDuplicateNumber {
         return len;
     }
 
-    // Set
+    // Set time: O(n) space: O(n)
     public static int findDuplicate_set(int[] nums) {
         Set<Integer> set = new HashSet<>();
         int len = nums.length;
@@ -77,7 +74,7 @@ public class Problem287_findTheDuplicateNumber {
         return len;
     }
 
-    // Visited
+    // Visited time: O(n) space: O(1)
     public static int findDuplicate_mark(int[] nums) {
         int len = nums.length;
         for (int num : nums) {
@@ -91,7 +88,7 @@ public class Problem287_findTheDuplicateNumber {
         return len;
     }
 
-    // Sort
+    // Sort time: O(nlogn) space: O(logn)
     public static int findDuplicate_sort(int[] nums) {
         Arrays.sort(nums);
         int len = nums.length;
@@ -104,7 +101,7 @@ public class Problem287_findTheDuplicateNumber {
         return len;
     }
 
-    // Index Sort
+    // Index Sort time: O(n) space: O(1)
     // n + 1 numbers in n.
     public static int findDuplicate_index_sort(int[] nums) {
         int len = nums.length;
@@ -123,7 +120,7 @@ public class Problem287_findTheDuplicateNumber {
         return 0;
     }
 
-    // Binary Search
+    // Binary Search time: O(nlogn) space: O(1)
     public static int findDuplicate_bs(int[] nums) {
         int len = nums.length;
         int low = 1;
@@ -147,11 +144,12 @@ public class Problem287_findTheDuplicateNumber {
         return low;
     }
 
-    // Bit
+    // Bit time: O(nlogn) space: O(1)
     public static int findDuplicate_bit(int[] nums) {
         int n = nums.length;
         int ans = 0;
         int bit_max = 31;
+
         while (((n - 1) >> bit_max) == 0) {
             bit_max -= 1;
         }
@@ -174,7 +172,7 @@ public class Problem287_findTheDuplicateNumber {
         return ans;
     }
 
-    // Slow Fast Pointers
+    // Slow Fast Pointers time: O(n) space: O(1)
     public static int findDuplicate_fastSlow(int[] nums) {
         int slow = 0;
         int fast = 0;
