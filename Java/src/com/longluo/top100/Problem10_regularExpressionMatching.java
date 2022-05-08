30211be3035671be7cf3d968525da8a5563c9d7e
longluo <calmbody@163.com>
2022-05-08 21:19:27 +0800

    Problem 10 Regex

--- Java/src/com/longluo/top100/Problem10_regularExpressionMatching.java
+++ Java/src/com/longluo/top100/Problem10_regularExpressionMatching.java
@@ -1,49 +1,88 @@
 package com.longluo.top100;
 
 /**
- * 10. Regular Expression Matching
+ * 10. 正则表达式匹配
  * <p>
- * Hard
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
  * <p>
- * Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
  * <p>
- * '.' Matches any single character.​​​​
- * '*' Matches zero or more of the preceding element.
- * The matching should cover the entire input string (not partial).
+ * 示例 1：
+ * 输入：s = "aa", p = "a"
+ * 输出：false
+ * 解释："a" 无法匹配 "aa" 整个字符串。
  * <p>
- * Example 1:
- * Input: s = "aa", p = "a"
- * Output: false
- * Explanation: "a" does not match the entire string "aa".
+ * 示例 2:
+ * 输入：s = "aa", p = "a*"
+ * 输出：true
+ * 解释：因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
  * <p>
- * Example 2:
- * Input: s = "aa", p = "a*"
- * Output: true
- * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * 示例 3：
+ * 输入：s = "ab", p = ".*"
+ * 输出：true
+ * 解释：".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
  * <p>
- * Example 3:
- * Input: s = "ab", p = ".*"
- * Output: true
- * Explanation: ".*" means "zero or more (*) of any character (.)".
- * <p>
- * Constraints:
+ * 提示：
  * 1 <= s.length <= 20
  * 1 <= p.length <= 30
- * s contains only lowercase English letters.
- * p contains only lowercase English letters, '.', and '*'.
- * It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+ * s 只包含从 a-z 的小写字母。
+ * p 只包含从 a-z 的小写字母，以及字符 . 和 *。
+ * 保证每次出现字符 * 时，前面都匹配到有效的字符
  * <p>
  * https://leetcode.com/problems/regular-expression-matching/
  */
 public class Problem10_regularExpressionMatching {
 
-    public static boolean isMatch(String s, String p) {
+    // DP time: O(m*n) space: O(m*n)
+    public static boolean isMatch_dp(String s, String p) {
+        if (s.equals(p)) {
+            return true;
+        }
 
-        return true;
+        int sLen = s.length();
+        int pLen = p.length();
+
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+
+        // if both are empty
+        dp[0][0] = true;
+
+        for (int j = 2; j <= pLen; j += 2) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= sLen; i++) {
+            for (int j = 1; j <= pLen; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+
+        return dp[sLen][pLen];
+    }
+
+    // Regex API
+    public static boolean isMatch_api(String s, String p) {
+        return s.matches(p);
     }
 
     public static void main(String[] args) {
-        System.out.println("false ?= " + isMatch("aa", "a"));
-        System.out.println("true ?= " + isMatch("aa", "a*"));
+        System.out.println("true ?= " + isMatch_dp("", "a*a*"));
+        System.out.println("false ?= " + isMatch_dp("aa", "a"));
+        System.out.println("true ?= " + isMatch_dp("aa", "a*"));
+
+        System.out.println("true ?= " + isMatch_api("aa", "a*"));
+        System.out.println("true ?= " + isMatch_api("", "a*a*"));
     }
 }
