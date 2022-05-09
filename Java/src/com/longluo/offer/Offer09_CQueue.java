7c1e93c3eb4873bff29d5ca8979e6f5a6d8a9051
longluo <calmbody@163.com>
2022-05-09 19:36:03 +0800

    Offer 09 Stack

--- Java/src/com/longluo/offer/Offer09_CQueue.java
+++ Java/src/com/longluo/offer/Offer09_CQueue.java
@@ -1,10 +1,13 @@
 package com.longluo.offer;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 剑指 Offer 09. 用两个栈实现队列
+ * <p>
  * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
  * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
- * <p>
  * <p>
  * 示例 1：
  * 输入：
@@ -22,26 +25,53 @@ package com.longluo.offer;
  * 1 <= values <= 10000
  * 最多会对 appendTail、deleteHead 进行 10000 次调用
  * <p>
- * <p>
  * Your CQueue object will be instantiated and called as such:
  * CQueue obj = new CQueue();
  * obj.appendTail(value);
  * int param_2 = obj.deleteHead();
+ * <p>
+ * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
  */
 public class Offer09_CQueue {
 
-    class CQueue {
+    static class CQueue {
+        Deque<Integer> inStack;
+        Deque<Integer> outStack;
 
         public CQueue() {
-
+            inStack = new ArrayDeque<>();
+            outStack = new ArrayDeque<>();
         }
 
         public void appendTail(int value) {
-
+            inStack.push(value);
         }
 
         public int deleteHead() {
-            return 0;
+            if (empty()) {
+                return -1;
+            }
+
+            if (!outStack.isEmpty()) {
+                return outStack.pop();
+            }
+
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+
+            return outStack.pop();
         }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        CQueue tst1 = new CQueue();
+        tst1.appendTail(3);
+        System.out.println("3 ?= " + tst1.deleteHead());
+        System.out.println("-1 ?=" + tst1.deleteHead());
     }
 }
