22f6cc19e96f8281553d70ddd6ae24088f42e5d2
longluo <calmbody@163.com>
2022-05-09 15:35:26 +0800

    102 BFS

--- Java/src/com/longluo/top100/Problem102_binaryTreeLevelOrderTraversal.java
+++ Java/src/com/longluo/top100/Problem102_binaryTreeLevelOrderTraversal.java
@@ -1,4 +1,4 @@
-package com.longluo.leetcode.tree;
+package com.longluo.top100;
 
 import com.longluo.datastructure.TreeNode;
 import com.longluo.datastructure.TreeUtils;
@@ -9,31 +9,31 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * 102. Binary Tree Level Order Traversal
- * Medium
- * Given the root of a binary tree, return the level order traversal of its nodes' values.
- * (i.e., from left to right, level by level).
+ * 102. 二叉树的层序遍历
  * <p>
- * Example 1:
- * Input: root = [3,9,20,null,null,15,7]
- * Output: [[3],[9,20],[15,7]]
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
  * <p>
- * Example 2:
- * Input: root = [1]
- * Output: [[1]]
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
  * <p>
- * Example 3:
- * Input: root = []
- * Output: []
+ * 示例 2：
+ * 输入：root = [1]
+ * 输出：[[1]]
  * <p>
- * Constraints:
- * The number of nodes in the tree is in the range [0, 2000].
+ * 示例 3：
+ * 输入：root = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * 树中节点数目在范围 [0, 2000] 内
  * -1000 <= Node.val <= 1000
  * <p>
  * https://leetcode.com/problems/binary-tree-level-order-traversal/
  */
 public class Problem102_binaryTreeLevelOrderTraversal {
 
+    // BFS time: O(n) space: O(n)
     public static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         if (root == null) {
@@ -65,6 +65,8 @@ public class Problem102_binaryTreeLevelOrderTraversal {
         return ans;
     }
 
+    // TODO: 2022/5/9 DFS 
+    
     public static void main(String[] args) {
         TreeNode treeNode1 = TreeUtils.constructTree(new Integer[]{3, 9, 20, null, null, 15, 7});
         System.out.println(" ?= " + levelOrder(treeNode1));
