6802275612459716962ed5b07a8588331b1c7633
longluo <calmbody@163.com>
2022-05-11 22:39:48 +0800

    128 UnionFind

--- Java/src/com/longluo/top100/Problem128_longestConsecutiveSequence.java
+++ Java/src/com/longluo/top100/Problem128_longestConsecutiveSequence.java
@@ -50,8 +50,8 @@ public class Problem128_longestConsecutiveSequence {
         return ans;
     }
 
-    // Hash time: O(nlogn) space: O(n)
-    public static int longestConsecutive_hash(int[] nums) {
+    // HashMap time: O(nlogn) space: O(n)
+    public static int longestConsecutive_hashmap(int[] nums) {
         int len = nums.length;
         if (len <= 1) {
             return len;
@@ -72,6 +72,115 @@ public class Problem128_longestConsecutiveSequence {
         return ans;
     }
 
+    // Hash time: O(n) space: O(n)
+    public static int longestConsecutive_hash(int[] nums) {
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
+        }
+        int ans = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int x : nums) {
+            set.add(x);
+        }
+
+        for (int x : set) {
+            if (!set.contains(x - 1)) {
+                int curr = x;
+                int currLen = 1;
+                while (set.contains(curr + 1)) {
+                    curr++;
+                    currLen++;
+                }
+
+                ans = Math.max(ans, currLen);
+            }
+        }
+
+        return ans;
+    }
+
+    // UnionFind time: O(n) space: O(n)
+    public static int longestConsecutive_uf(int[] nums) {
+        int len = nums.length;
+        if (len <= 1) {
+            return len;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        UnionFind uf = new UnionFind(len);
+
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(nums[i])) {
+                continue;
+            }
+
+            if (map.containsKey(nums[i] - 1)) {
+                uf.union(i, map.get(nums[i] - 1));
+            }
+
+            if (map.containsKey(nums[i] + 1)) {
+                uf.union(i, map.get(nums[i] + 1));
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return uf.getMaxConnectSize();
+    }
+
+    // Union Find
+    static class UnionFind {
+        int count;
+        int[] parent;
+        int[] size;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (size[rootX] < size[rootY]) {
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            } else {
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+            }
+        }
+
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+
+            return x;
+        }
+
+        public int getMaxConnectSize() {
+            int maxSize = 0;
+            for (int i = 0; i < parent.length; i++) {
+                if (i == parent[i]) {
+                    maxSize = Math.max(maxSize, size[i]);
+                }
+            }
+
+            return maxSize;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + longestConsecutive_bf(new int[]{0}));
         System.out.println("1 ?= " + longestConsecutive_bf(new int[]{0, 0}));
@@ -79,10 +188,14 @@ public class Problem128_longestConsecutiveSequence {
         System.out.println("4 ?= " + longestConsecutive_bf(new int[]{100, 4, 200, 1, 3, 2}));
         System.out.println("9 ?= " + longestConsecutive_bf(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
 
-        System.out.println("1 ?= " + longestConsecutive_hash(new int[]{0}));
-        System.out.println("1 ?= " + longestConsecutive_hash(new int[]{0, 0}));
-        System.out.println("3 ?= " + longestConsecutive_hash(new int[]{1, 2, 0, 1}));
+        System.out.println("1 ?= " + longestConsecutive_hashmap(new int[]{0}));
+        System.out.println("1 ?= " + longestConsecutive_hashmap(new int[]{0, 0}));
+        System.out.println("3 ?= " + longestConsecutive_hashmap(new int[]{1, 2, 0, 1}));
+
         System.out.println("4 ?= " + longestConsecutive_hash(new int[]{100, 4, 200, 1, 3, 2}));
         System.out.println("9 ?= " + longestConsecutive_hash(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+        System.out.println("9 ?= " + longestConsecutive_hash(new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}));
+
+        System.out.println("4 ?= " + longestConsecutive_uf(new int[]{100, 4, 200, 1, 3, 2}));
     }
 }
