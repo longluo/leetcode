6f107afcb648929779bb483d048f6c34f54c8e82
longluo <calmbody@163.com>
2022-05-14 18:39:01 +0800

    406 Greedy Opt

--- Java/src/com/longluo/top100/Problem406_queueReconstructionByHeight.java
+++ Java/src/com/longluo/top100/Problem406_queueReconstructionByHeight.java
@@ -36,8 +36,8 @@ import java.util.*;
  */
 public class Problem406_queueReconstructionByHeight {
 
-    // Greedy
-    public static int[][] reconstructQueue(int[][] people) {
+    // Greedy time: O(n^2) space: O(logn)
+    public static int[][] reconstructQueue_greedy(int[][] people) {
         if (people == null || people.length <= 1) {
             return people;
         }
@@ -58,8 +58,27 @@ public class Problem406_queueReconstructionByHeight {
         return ans;
     }
 
+    // Greedy opt time: O(n^2) space: O(logn)
+    public static int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
+            }
+        });
+
+        LinkedList<int[]> ans = new LinkedList<>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+
     public static void main(String[] args) {
+        System.out.println("[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] ?= " + Arrays.deepToString(reconstructQueue_greedy(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}})));
         System.out.println("[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] ?= " + Arrays.deepToString(reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}})));
+        System.out.println("[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]] ?= " + Arrays.deepToString(reconstructQueue_greedy(new int[][]{{6, 0}, {5, 0}, {4, 0}, {3, 2}, {2, 2}, {1, 4}})));
         System.out.println("[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]] ?= " + Arrays.deepToString(reconstructQueue(new int[][]{{6, 0}, {5, 0}, {4, 0}, {3, 2}, {2, 2}, {1, 4}})));
     }
 }
