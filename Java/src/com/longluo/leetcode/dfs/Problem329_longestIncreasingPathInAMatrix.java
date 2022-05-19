8756e40e637558d41262fb66f04e02a0e8d1c16c
longluo <calmbody@163.com>
2022-05-19 09:36:55 +0800

    329 Memory + DFS time: O(mn) space: O(mn)

--- Java/src/com/longluo/leetcode/dfs/Problem329_longestIncreasingPathInAMatrix.java
+++ Java/src/com/longluo/leetcode/dfs/Problem329_longestIncreasingPathInAMatrix.java
@@ -96,11 +96,67 @@ public class Problem329_longestIncreasingPathInAMatrix {
         return ans;
     }
 
+    // Memory + DFS time: O(mn) space: O(mn)
+    public static int longestIncreasingPath_memo(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        if (row == 1 && col == 1) {
+            return 1;
+        }
+
+        int max = 0;
+        int[][] memo = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (memo[i][j] == 0) {
+                    max = Math.max(max, dfs(matrix, memo, i, j));
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static int dfs(int[][] matrix, int[][] memo, int x, int y) {
+        if (memo[x][y] > 0) {
+            return memo[x][y];
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int ans = 1;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int[] dir : dirs) {
+            int nextX = x + dir[0];
+            int nextY = y + dir[1];
+
+            if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col) {
+                continue;
+            }
+
+            if (matrix[nextX][nextY] <= matrix[x][y]) {
+                continue;
+            }
+
+            ans = Math.max(ans, dfs(matrix, memo, nextX, nextY) + 1);
+        }
+
+        memo[x][y] = ans;
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + longestIncreasingPath_bfs(new int[][]{{1}}));
         System.out.println("4 ?= " + longestIncreasingPath_bfs(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
         System.out.println("4 ?= " + longestIncreasingPath_bfs(new int[][]{{3, 4, 5}, {3, 2, 6}, {2, 2, 1}}));
         System.out.println("6 ?= " + longestIncreasingPath_bfs(new int[][]{{7, 8, 9}, {9, 7, 6}, {7, 2, 3}}));
+
+        System.out.println("4 ?= " + longestIncreasingPath_memo(new int[][]{{9, 9, 4}, {6, 6, 8}, {2, 1, 1}}));
+        System.out.println("6 ?= " + longestIncreasingPath_memo(new int[][]{{7, 8, 9}, {9, 7, 6}, {7, 2, 3}}));
     }
 }
 
