3019f3446fb5b96bbf36829f423c764a72f0f3fa
longluo <calmbody@163.com>
2022-05-22 09:58:36 +0800

    647 DP

--- Java/src/com/longluo/top100/Problem647_palindromicSubstrings.java
+++ Java/src/com/longluo/top100/Problem647_palindromicSubstrings.java
@@ -26,7 +26,7 @@ package com.longluo.top100;
  */
 public class Problem647_palindromicSubstrings {
 
-    // BF time: O(n^2) space: O(1)
+    // BF time: O(n^3) space: O(1)
     public static int countSubstrings_bf(String s) {
         int len = s.length();
         int ans = 0;
@@ -58,7 +58,31 @@ public class Problem647_palindromicSubstrings {
         return true;
     }
 
+    // DP time: O(n^2) space: O(n^2)
+    public static int countSubstrings_dp(String s) {
+        int len = s.length();
+        int ans = 0;
+        boolean[][] dp = new boolean[len][len];
+
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (i + 1 == j) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
+                } else if (i < len - 1 && j >= 1) {
+                    dp[i][j] = dp[i + 1][j - 1] && s.charAt(j) == s.charAt(i);
+                }
+
+                ans += dp[i][j] ? 1 : 0;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("3 ?= " + countSubstrings_bf("abc"));
+        System.out.println("3 ?= " + countSubstrings_dp("abc"));
     }
 }
