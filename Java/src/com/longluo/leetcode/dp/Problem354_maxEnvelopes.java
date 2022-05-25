e92e4a38e8cd2c807ba63fccf5488a20abf27b32
longluo <calmbody@163.com>
2022-05-25 17:23:50 +0800

    354 BF Failed Not consider some cases.

--- Java/src/com/longluo/leetcode/dp/Problem354_maxEnvelopes.java
+++ Java/src/com/longluo/leetcode/dp/Problem354_maxEnvelopes.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * 354. 俄罗斯套娃信封问题
+ * <p>
  * 给定一些标记了宽度和高度的信封，宽度和高度以整数对形式 (w, h) 出现。当另一个信封的宽度和高度都比这个信封大的时候，
  * 这个信封就可以放进另一个信封里，如同俄罗斯套娃一样。
  * 请计算最多能有多少个信封能组成一组“俄罗斯套娃”信封（即可以把一个信封放到另一个信封里面）。
@@ -15,8 +16,47 @@ import java.util.*;
  * 输入: envelopes = [[5,4],[6,4],[6,7],[2,3]]
  * 输出: 3
  * 解释: 最多信封的个数为 3, 组合为: [2,3] => [5,4] => [6,7]。
+ * <p>
+ * 示例 2：
+ * 输入：envelopes = [[1,1],[1,1],[1,1]]
+ * 输出：1
+ * <p>
+ * 提示：
+ * 1 <= envelopes.length <= 10^5
+ * envelopes[i].length == 2
+ * 1 <= wi, hi <= 10^5
+ * <p>
+ * https://leetcode.cn/problems/russian-doll-envelopes/
  */
 public class Problem354_maxEnvelopes {
+
+    // BF time: O(n^2 + nlogn) space: O(logn)
+    public static int maxEnvelopes_bf(int[][] envelopes) {
+        int len = envelopes.length;
+        if (len <= 1) {
+            return len;
+        }
+
+        Arrays.sort(envelopes, (o1, o2) -> o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]);
+
+        int ans = 1;
+        for (int i = 0; i < len; i++) {
+            int w = envelopes[i][0];
+            int h = envelopes[i][1];
+            int cnt = 1;
+            for (int j = i + 1; j < len; j++) {
+                if (envelopes[j][0] > w && envelopes[j][1] > h) {
+                    w = envelopes[j][0];
+                    h = envelopes[j][1];
+                    cnt++;
+                }
+            }
+
+            ans = Math.max(ans, cnt);
+        }
+
+        return ans;
+    }
 
     public static int maxEnvelopes(int[][] envelopes) {
         if (envelopes == null || envelopes.length == 0 || envelopes[0].length == 0) {
@@ -75,6 +115,9 @@ public class Problem354_maxEnvelopes {
     }
 
     public static void main(String[] args) {
+        System.out.println("3 ?= " + maxEnvelopes_bf(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}));
+        System.out.println("5 ?= " + maxEnvelopes_bf(new int[][]{{2, 100}, {3, 200}, {4, 300}, {5, 500}, {5, 400}, {5, 250}, {6, 370}, {6, 360}, {7, 380}}));
+
         System.out.println("3 ?= " + maxEnvelopes(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}));
         System.out.println("5 ?= " + maxEnvelopes(new int[][]{{2, 100}, {3, 200}, {4, 300}, {5, 500}, {5, 400}, {5, 250}, {6, 370}, {6, 360}, {7, 380}}));
         System.out.println("3 ?= " + maxEnvelopes(new int[][]{{46, 89}, {50, 53}, {52, 68}, {72, 45}, {77, 81}}));
