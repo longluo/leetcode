0913c550bb93666d74d9cb853cea4d970e20876e
longluo <calmbody@163.com>
2022-05-30 10:59:45 +0800

    29 Recursion

--- Java/src/com/longluo/leetcode/bitmanipulation/Problem29_divideTwoIntegers.java
+++ Java/src/com/longluo/leetcode/bitmanipulation/Problem29_divideTwoIntegers.java
@@ -258,6 +258,59 @@ public class Problem29_divideTwoIntegers {
         return sign ? ans : -ans;
     }
 
+    // Recursion time: O(logx * logy) space: O(1)
+    public static int divide_rec(int dividend, int divisor) {
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+
+        if (dividend == 0) {
+            return 0;
+        }
+
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            } else if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        long x = dividend;
+        long y = divisor;
+
+        boolean sign = false;
+        if ((x > 0 && y < 0) || (x < 0 && y > 0)) {
+            sign = true;
+        }
+
+        x = x > 0 ? x : -x;
+        y = y > 0 ? y : -y;
+
+        long ans = div(x, y);
+        if (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return (int) (sign ? -ans : ans);
+    }
+
+    public static long div(long dividend, long divisor) {
+        if (dividend < divisor) {
+            return 0;
+        }
+
+        long ans = 1;
+        long temp = divisor;
+
+        while ((temp + temp) <= dividend) {
+            ans = ans << 1;
+            temp = temp << 1;
+        }
+
+        return ans + div(dividend - temp, divisor);
+    }
+
     public static void main(String[] args) {
         int val = -2147483648;
         System.out.println("val = " + val);
@@ -281,5 +334,7 @@ public class Problem29_divideTwoIntegers {
         System.out.println("-1073741824 ?= " + divide_bs(-2147483648, 2));
 
         System.out.println("-1073741824 ?= " + divide_bs_32(-2147483648, 2));
+
+        System.out.println("-1073741824 ?= " + divide_rec(-2147483648, 2));
     }
 }
