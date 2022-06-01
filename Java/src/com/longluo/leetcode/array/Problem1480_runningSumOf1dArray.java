7650a9b2b8faee7ca06455b42c697921cb30d3c7
longluo <calmbody@163.com>
2022-06-01 10:42:02 +0800

    1480 Prefix Sums

--- Java/src/com/longluo/leetcode/array/Problem1480_runningSumOf1dArray.java
+++ Java/src/com/longluo/leetcode/array/Problem1480_runningSumOf1dArray.java
@@ -30,6 +30,7 @@ import java.util.Arrays;
  */
 public class Problem1480_runningSumOf1dArray {
 
+    // BF time: O(n^2) space: O(n)
     public static int[] runningSum(int[] nums) {
         int[] sums = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
@@ -41,7 +42,20 @@ public class Problem1480_runningSumOf1dArray {
         return sums;
     }
 
-    public static int[] runningSum2(int[] nums) {
+    // BF Opt time: O(n^2) space: O(n)
+    public static int[] runningSum_bf_opt(int[] nums) {
+        int len = nums.length;
+        int[] sums = new int[len];
+        sums[0] = nums[0];
+        for (int i = 1; i < len; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+        }
+
+        return sums;
+    }
+
+    // Opt time: O(n) space: O(1)
+    public static int[] runningSum_opt(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             nums[i] += nums[i - 1];
         }
@@ -53,5 +67,9 @@ public class Problem1480_runningSumOf1dArray {
         System.out.println("[1, 3, 6, 10] ?= " + Arrays.toString(runningSum(new int[]{1, 2, 3, 4})));
         System.out.println("[1, 2, 3, 4, 5] ?= " + Arrays.toString(runningSum(new int[]{1, 1, 1, 1, 1})));
         System.out.println("[3, 4, 6, 16, 17] ?= " + Arrays.toString(runningSum(new int[]{3, 1, 2, 10, 1})));
+
+        System.out.println("[3, 4, 6, 16, 17] ?= " + Arrays.toString(runningSum_bf_opt(new int[]{3, 1, 2, 10, 1})));
+
+        System.out.println("[3, 4, 6, 16, 17] ?= " + Arrays.toString(runningSum_opt(new int[]{3, 1, 2, 10, 1})));
     }
 }
