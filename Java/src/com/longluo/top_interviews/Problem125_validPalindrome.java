dee5939058f32434a314f0b8dff4f27434ce0831
longluo <calmbody@163.com>
2022-06-03 10:14:47 +0800

    125 Two Pointers

--- Java/src/com/longluo/top_interviews/Problem125_validPalindrome.java
+++ Java/src/com/longluo/top_interviews/Problem125_validPalindrome.java
@@ -1,34 +1,45 @@
-package com.longluo.leetcode.string;
+package com.longluo.top_interviews;
 
 /**
  * 125. 验证回文串
+ * <p>
  * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * <p>
  * 说明：本题中，我们将空字符串定义为有效的回文串。
  * <p>
  * 示例 1:
  * 输入: "A man, a plan, a canal: Panama"
  * 输出: true
+ * 解释："amanaplanacanalpanama" 是回文串
  * <p>
  * 示例 2:
  * 输入: "race a car"
  * 输出: false
+ * 解释："raceacar" 不是回文串
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 2 * 10^5
+ * 字符串 s 由 ASCII 字符组成
+ * <p>
+ * https://leetcode.cn/problems/valid-palindrome/
  */
-public class Problem125_isPalindrome {
+public class Problem125_validPalindrome {
 
+    // Two Pointers time: O(n) space: O(1)
     public static boolean isPalindrome(String s) {
         if (s == null || s.length() <= 1) {
             return true;
         }
 
-        int n = s.length();
+        int len = s.length();
         int left = 0;
-        int right = n - 1;
+        int right = len - 1;
         while (left < right) {
-            while (left < n && left < right && !(Character.isDigit(s.charAt(left)) || Character.isAlphabetic(s.charAt(left)))) {
+            while (left < right && !(Character.isDigit(s.charAt(left)) || Character.isAlphabetic(s.charAt(left)))) {
                 left++;
             }
 
-            while (right > 0 && left < right && !(Character.isDigit(s.charAt(right)) || Character.isAlphabetic(s.charAt(right)))) {
+            while (right > left && !(Character.isDigit(s.charAt(right)) || Character.isAlphabetic(s.charAt(right)))) {
                 right--;
             }
 
