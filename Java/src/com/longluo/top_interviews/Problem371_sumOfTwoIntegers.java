87cc83aa79e8c9c00a0bbb8d922f741e7b92ffe3
longluo <calmbody@163.com>
2022-06-06 22:59:15 +0800

    371 Compute

--- Java/src/com/longluo/top_interviews/Problem371_sumOfTwoIntegers.java
+++ Java/src/com/longluo/top_interviews/Problem371_sumOfTwoIntegers.java
@@ -1,4 +1,4 @@
-package com.longluo.leetcode.bitmanipulation;
+package com.longluo.top_interviews;
 
 /**
  * 371. 两整数之和
@@ -16,7 +16,7 @@ package com.longluo.leetcode.bitmanipulation;
  * 提示：
  * -1000 <= a, b <= 1000
  * <p>
- * https://leetcode-cn.com/problems/sum-of-two-integers/
+ * https://leetcode.com/problems/sum-of-two-integers/
  */
 public class Problem371_sumOfTwoIntegers {
 
@@ -36,7 +36,35 @@ public class Problem371_sumOfTwoIntegers {
         return a;
     }
 
+    // Compute time: O(log(max_int)) space: O(1)
+    public static int getSum_compute(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+
+        if (b == 0) {
+            return a;
+        }
+
+        int lower = 0;
+        int carry = 0;
+        while (true) {
+            lower = a ^ b;
+            carry = a & b;
+            if (carry == 0) {
+                break;
+            }
+
+            a = lower;
+            b = carry << 1;
+        }
+
+        return lower;
+    }
+
     public static void main(String[] args) {
+        System.out.println("3 ?= " + getSum_bf(2, 1));
         System.out.println("3 ?= " + getSum_bit(2, 1));
+        System.out.println("3 ?= " + getSum_compute(2, 1));
     }
 }
