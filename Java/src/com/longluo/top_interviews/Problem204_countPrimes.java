d0cc386f467711aed6a43eab701eb0053942547c
longluo <calmbody@163.com>
2022-06-14 10:08:03 +0800

    204 BF TLE

--- Java/src/com/longluo/top_interviews/Problem204_countPrimes.java
+++ Java/src/com/longluo/top_interviews/Problem204_countPrimes.java
@@ -47,17 +47,13 @@ public class Problem204_countPrimes {
     }
 
     private static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
             }
         }
 
-        return true;
+        return n >= 2;
     }
 
     // TODO: 2022/6/7
