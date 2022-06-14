456e478147c8b8ed8714baf47f7d9047af28d9ab
longluo <calmbody@163.com>
2022-06-14 20:55:22 +0800

    63 DP Opt

--- Java/src/com/longluo/leetcode/dp/Problem63_uniquePaths_ii.java
+++ Java/src/com/longluo/leetcode/dp/Problem63_uniquePaths_ii.java
@@ -70,9 +70,40 @@ public class Problem63_uniquePaths_ii {
         return dp[row - 1][col - 1];
     }
 
+    // DP Opt time: O(n^2) space: O(n)
+    public static int uniquePathsWithObstacles_opt(int[][] obstacleGrid) {
+        int row = obstacleGrid.length;
+        int col = obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[row - 1][col - 1] == 1) {
+            return 0;
+        }
+
+        int[] dp = new int[col];
+        dp[0] = 1;
+
+        for (int i = 1; i < col; i++) {
+            dp[i] = obstacleGrid[0][i] == 0 ? dp[i - 1] : 0;
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[j] = j == 0 ? dp[j] : dp[j - 1] + dp[j];
+                } else {
+                    dp[j] = 0;
+                }
+            }
+        }
+
+        return dp[col - 1];
+    }
+
     public static void main(String[] args) {
         System.out.println("1  ?= " + uniquePathsWithObstacles(new int[][]{{0, 1}, {0, 0}}));
         System.out.println("0  ?= " + uniquePathsWithObstacles(new int[][]{{0, 0}, {1, 1}, {0, 0}}));
         System.out.println("2  ?= " + uniquePathsWithObstacles(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
+
+        System.out.println("2  ?= " + uniquePathsWithObstacles_opt(new int[][]{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}));
     }
 }
