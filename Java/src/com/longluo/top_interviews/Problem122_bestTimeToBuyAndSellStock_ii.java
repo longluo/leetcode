993699506d39de273c2a939ab63e3c8cdef21eb1
longluo <calmbody@163.com>
2022-06-23 07:55:53 +0800

    122 DP

--- Java/src/com/longluo/top_interviews/Problem122_bestTimeToBuyAndSellStock_ii.java
+++ Java/src/com/longluo/top_interviews/Problem122_bestTimeToBuyAndSellStock_ii.java
@@ -35,12 +35,23 @@ package com.longluo.top_interviews;
  */
 public class Problem122_bestTimeToBuyAndSellStock_ii {
 
+    // DP time: O(n) space: O(n)
     public static int maxProfit(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][2];
 
-        return 0;
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[len - 1][0];
     }
 
     public static void main(String[] args) {
-
+        System.out.println("7 ?= " + maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
 }
