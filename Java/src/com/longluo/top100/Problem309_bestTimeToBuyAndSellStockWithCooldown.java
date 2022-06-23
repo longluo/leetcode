7728c9f472da63408742381f0898e27f943fe087
longluo <calmbody@163.com>
2022-06-23 17:32:58 +0800

    309 DP

--- Java/src/com/longluo/top100/Problem309_bestTimeToBuyAndSellStockWithCooldown.java
+++ Java/src/com/longluo/top100/Problem309_bestTimeToBuyAndSellStockWithCooldown.java
@@ -22,26 +22,28 @@ package com.longluo.top100;
  * 1 <= prices.length <= 5000
  * 0 <= prices[i] <= 1000
  * <p>
- * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
  */
 public class Problem309_bestTimeToBuyAndSellStockWithCooldown {
 
-    //
-    public static int maxProfit_dp(int[] prices) {
+    // DP time: O(n) space: O(n)
+    public static int maxProfit(int[] prices) {
         int len = prices.length;
-        if (len <= 1) {
-            return 0;
+
+        int[][] dp = new int[len][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], i >= 2 ? dp[i - 2][0] - prices[i] : -prices[i]);
         }
 
-        int maxans = 0;
-        for (int i = 0; i < len; i++) {
-
-        }
-
-        return maxans;
+        return dp[len - 1][0];
     }
 
     public static void main(String[] args) {
-
+        System.out.println("3 ?= " + maxProfit(new int[]{1, 2, 3, 0, 2}));
     }
 }
