e7e857bd3c3040ce5952ffdf725ff850c399c241
longluo <calmbody@163.com>
2022-06-23 17:35:51 +0800

    123 DP Opt

--- Java/src/com/longluo/studyplan/stock/Problem123_bestTimeToBuyAndSellStock_iii.java
+++ Java/src/com/longluo/studyplan/stock/Problem123_bestTimeToBuyAndSellStock_iii.java
@@ -62,9 +62,30 @@ public class Problem123_bestTimeToBuyAndSellStock_iii {
         return dp[len - 1][2][0];
     }
 
+    // DP Opt time: O(n) space: O(1)
+    public static int maxProfit_opt(int[] prices) {
+        int len = prices.length;
+        int buy1 = -prices[0];
+        int sell1 = 0;
+        int buy2 = -prices[0];
+        int sell2 = 0;
+
+        for (int i = 1; i < len; i++) {
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy1 = Math.max(buy1, -prices[i]);
+
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+        }
+
+        return sell2;
+    }
+
     public static void main(String[] args) {
         System.out.println("4 ?= " + maxProfit(new int[]{1, 2, 3, 4, 5}));
         System.out.println("0 ?= " + maxProfit(new int[]{7, 6, 4, 3, 1}));
         System.out.println("7 ?= " + maxProfit(new int[]{6, 1, 3, 2, 4, 7}));
+
+        System.out.println("7 ?= " + maxProfit_opt(new int[]{6, 1, 3, 2, 4, 7}));
     }
 }
