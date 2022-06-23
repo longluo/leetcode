dfc4357b5213e6fc7eab49c8ca5df0f1bef344ee
longluo <calmbody@163.com>
2022-06-23 15:04:04 +0800

    714 DP

--- Java/src/com/longluo/studyplan/stock/Problem714_bestTimeToBuyAndSellStockWithTransactionFee.java
+++ Java/src/com/longluo/studyplan/stock/Problem714_bestTimeToBuyAndSellStockWithTransactionFee.java
@@ -1,4 +1,4 @@
-package com.longluo.leetcode.dp;
+package com.longluo.studyplan.stock;
 
 /**
  * 714. 买卖股票的最佳时机含手续费
@@ -20,23 +20,33 @@ package com.longluo.leetcode.dp;
  * 在此处买入 prices[4] = 4
  * 在此处卖出 prices[5] = 9
  * 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
- * 注意:
  * <p>
+ * 注意:
  * 0 < prices.length <= 50000.
  * 0 < prices[i] < 50000.
  * 0 <= fee < 50000.
+ * <p>
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
  */
 public class Problem714_bestTimeToBuyAndSellStockWithTransactionFee {
 
+    // DP time: O(n) space: O(n)
     public static int maxProfit(int[] prices, int fee) {
-        int ans = 0;
+        int len = prices.length;
+        int[][] dp = new int[len][2];
 
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
 
-        
-        return ans;
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[len - 1][0];
     }
 
     public static void main(String[] args) {
-
+        System.out.println("8 ?= " + maxProfit(new int[]{1, 3, 2, 8, 4, 9}, 2));
     }
 }
