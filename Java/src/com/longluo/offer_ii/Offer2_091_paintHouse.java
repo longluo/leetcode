d77be17609f2ca3f2c73f6d64e6124c198da3e60
longluo <calmbody@163.com>
2022-06-25 10:13:36 +0800

    Offer 2 091 DP

--- Java/src/com/longluo/offer_ii/Offer2_091_paintHouse.java
+++ Java/src/com/longluo/offer_ii/Offer2_091_paintHouse.java
@@ -36,8 +36,15 @@ public class Offer2_091_paintHouse {
         int len = costs.length;
         int[][] dp = new int[len][3];
 
+        for (int i = 0; i < 3; i++) {
+            dp[0][i] = costs[0][i];
+        }
 
-
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = Math.min(costs[i][j] + dp[i - 1][(j + 1) % 3], costs[i][j] + dp[i - 1][(j + 2) % 3]);
+            }
+        }
 
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < 3; i++) {
