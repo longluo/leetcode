b01e5b6d31fd9cfbbf8a1a9f793e71a17192c87f
longluo <calmbody@163.com>
2022-06-29 21:47:50 +0800

    105 Recursion

--- Java/src/com/longluo/top100/Problem105_constructBinaryTreeFromPreorderAndInorderTraversal.java
+++ Java/src/com/longluo/top100/Problem105_constructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,6 +1,7 @@
 package com.longluo.top100;
 
 import com.longluo.datastructure.TreeNode;
+import com.longluo.datastructure.TreeUtils;
 
 /**
  * 105. 从前序与中序遍历序列构造二叉树
@@ -29,13 +30,41 @@ import com.longluo.datastructure.TreeNode;
  */
 public class Problem105_constructBinaryTreeFromPreorderAndInorderTraversal {
 
-    // TODO: 2022/6/7
+    // Recursion time: O(n) space: O(n)
     public static TreeNode buildTree(int[] preorder, int[] inorder) {
-
-        return null;
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
     }
 
-    public static void main(String[] args) {
+    public static TreeNode buildTreeHelper(int[] preOrder, int pStart, int pEnd, int[] inOrder, int iStart, int iEnd) {
+        if (pStart == pEnd) {
+            return null;
+        }
 
+        int rootVal = preOrder[pStart];
+        TreeNode root = new TreeNode(rootVal);
+
+        int i_root_idx = 0;
+        for (int i = iStart; i < iEnd; i++) {
+            if (inOrder[i] == rootVal) {
+                i_root_idx = i;
+                break;
+            }
+        }
+
+        int left = i_root_idx - iStart;
+
+        root.left = buildTreeHelper(preOrder, pStart + 1, pStart + left + 1, inOrder, iStart, i_root_idx);
+        root.right = buildTreeHelper(preOrder, pStart + left + 1, pEnd, inOrder, i_root_idx + 1, iEnd);
+
+        return root;
+    }
+
+    // TODO: 2022/6/29  
+
+    public static void main(String[] args) {
+        // [-1]
+        TreeUtils.printTree(buildTree(new int[]{-1}, new int[]{-1}));
+        // [3,9,20,null,null,15,7]
+        TreeUtils.printTree(buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7}));
     }
 }
