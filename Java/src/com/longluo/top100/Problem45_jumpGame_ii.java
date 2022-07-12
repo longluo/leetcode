5865554d1ee9b558d6c26751339c81311d6a5475
longluo <calmbody@163.com>
2022-07-12 14:20:04 +0800

    45 Greedy

--- Java/src/com/longluo/top100/Problem45_jumpGame_ii.java
+++ Java/src/com/longluo/top100/Problem45_jumpGame_ii.java
@@ -50,7 +50,7 @@ public class Problem45_jumpGame_ii {
         int maxPosition = 0;
         int end = 0;
         int steps = 0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             maxPosition = Math.max(maxPosition, i + nums[i]);
             if (i == end) {
                 end = maxPosition;
