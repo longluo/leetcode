550c8dfd62e21b7a1c4e253edba9534b67f831a5
longluo <calmbody@163.com>
2022-07-12 14:25:04 +0800

    45 DP

--- Java/src/com/longluo/top100/Problem45_jumpGame_ii.java
+++ Java/src/com/longluo/top100/Problem45_jumpGame_ii.java
@@ -28,6 +28,31 @@ import java.util.Arrays;
  */
 public class Problem45_jumpGame_ii {
 
+    // DP time: O(n^2) space: O(n)
+    public static int jump_dp(int[] nums) {
+        int len = nums.length;
+        if (len <= 1) {
+            return 0;
+        }
+
+        int[] dp = new int[len];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int minStep = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            int steps = nums[i];
+            for (int j = 1; j <= steps && i + j < len; j++) {
+                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+                if (i + j >= len - 1) {
+                    minStep = Math.min(minStep, dp[i] + 1);
+                    return minStep;
+                }
+            }
+        }
+
+        return minStep;
+    }
+
     // Greedy Reverse time: O(n^2) space: O(n）
     public static int jump_greedy_reverse(int[] nums) {
         int ans = 0;
@@ -59,31 +84,6 @@ public class Problem45_jumpGame_ii {
         }
 
         return steps;
-    }
-
-    // DP time: O(n^2) space: O(n)
-    public static int jump_dp(int[] nums) {
-        int len = nums.length;
-        if (len <= 1) {
-            return 0;
-        }
-
-        int[] dp = new int[len];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
-        int minStep = Integer.MAX_VALUE;
-        for (int i = 0; i < len; i++) {
-            int steps = nums[i];
-            for (int j = 1; j <= steps && i + j < len; j++) {
-                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
-                if (i + j >= len - 1) {
-                    minStep = Math.min(minStep, dp[i] + 1);
-                    return minStep;
-                }
-            }
-        }
-
-        return minStep;
     }
 
     public static void main(String[] args) {
