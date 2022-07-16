f5cda0d0bc9a964cfd76486929bcc84d4a3cb83d
longluo <calmbody@163.com>
2022-07-16 08:40:40 +0800

    346 BF list

---
+++ Java/src/com/longluo/leetcode/design/Problem346_movingAverageFromDataStream.java
@@ -0,0 +1,82 @@
+package com.longluo.leetcode.design;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 346. 滑动窗口的平均值
+ * <p>
+ * 给定一个整数数据流和一个窗口大小，根据该滑动窗口的大小，计算滑动窗口里所有数字的平均值。
+ * <p>
+ * 实现 MovingAverage 类：
+ * MovingAverage(int size) 用窗口大小 size 初始化对象。
+ * double next(int val) 成员函数 next 每次调用的时候都会往滑动窗口增加一个整数，
+ * 请计算并返回数据流中最后 size 个值的移动平均值，即滑动窗口里所有数字的平均值。
+ * <p>
+ * 示例：
+ * 输入：
+ * inputs = ["MovingAverage", "next", "next", "next", "next"]
+ * inputs = [[3], [1], [10], [3], [5]]
+ * 输出：
+ * [null, 1.0, 5.5, 4.66667, 6.0]
+ * <p>
+ * 解释：
+ * MovingAverage movingAverage = new MovingAverage(3);
+ * movingAverage.next(1); // 返回 1.0 = 1 / 1
+ * movingAverage.next(10); // 返回 5.5 = (1 + 10) / 2
+ * movingAverage.next(3); // 返回 4.66667 = (1 + 10 + 3) / 3
+ * movingAverage.next(5); // 返回 6.0 = (10 + 3 + 5) / 3
+ * <p>
+ * 提示：
+ * 1 <= size <= 1000
+ * -10^5 <= val <= 10^5
+ * 最多调用 next 方法 10^4 次
+ * <p>
+ * https://leetcode.cn/problems/qIsx9U/
+ */
+public class Problem346_movingAverageFromDataStream {
+
+    // List time: O(n) space: O(n)
+    static class MovingAverage_list {
+        List<Integer> numsList;
+        int size = 0;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MovingAverage_list(int size) {
+            numsList = new ArrayList<>();
+            this.size = size;
+        }
+
+        public double next(int val) {
+            numsList.add(val);
+            if (numsList.size() < size) {
+                return sum(0) / numsList.size();
+            } else {
+                return sum(numsList.size() - size) / size;
+            }
+        }
+
+        private double sum(int start) {
+            double sum = 0;
+            for (int i = start; i < numsList.size(); i++) {
+                sum += numsList.get(i);
+            }
+
+            return sum;
+        }
+    }
+
+    /**
+     * Your MovingAverage object will be instantiated and called as such:
+     * MovingAverage obj = new MovingAverage(size);
+     * double param_1 = obj.next(val);
+     */
+    public static void main(String[] args) {
+        MovingAverage_list ma1 = new MovingAverage_list(3);
+        System.out.println(ma1.next(12009));
+        System.out.println(ma1.next(1965));
+        System.out.println(ma1.next(-940));
+    }
+}
