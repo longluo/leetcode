e100677d35b129a6699ded1fa8a04a838e2c6bf4
longluo <calmbody@163.com>
2022-07-17 08:12:37 +0800

    629 DP

--- Java/src/com/longluo/leetcode/dp/Problem629_kInversePairs.java
+++ Java/src/com/longluo/leetcode/dp/Problem629_kInversePairs.java
@@ -4,8 +4,9 @@ package com.longluo.leetcode.dp;
  * 629. K个逆序对数组
  * <p>
  * 给出两个整数 n 和 k，找出所有包含从 1 到 n 的数字，且恰好拥有 k 个逆序对的不同的数组的个数。
+ * <p>
  * 逆序对的定义如下：对于数组的第i个和第 j个元素，如果满i < j且 a[i] > a[j]，则其为一个逆序对；否则不是。
- * 由于答案可能很大，只需要返回 答案 mod 109 + 7 的值。
+ * 由于答案可能很大，只需要返回 答案 mod 10^9 + 7 的值。
  * <p>
  * 示例 1:
  * 输入: n = 3, k = 0
@@ -20,12 +21,14 @@ package com.longluo.leetcode.dp;
  * 数组 [1,3,2] 和 [2,1,3] 都有 1 个逆序对。
  * <p>
  * 说明:
- * n 的范围是 [1, 1000] 并且 k 的范围是 [0, 1000]。
+ * 1 <= n <= 1000
+ * 0 <= k <= 1000
  * <p>
- * https://leetcode-cn.com/problems/k-inverse-pairs-array/
+ * https://leetcode.cn/problems/k-inverse-pairs-array/
  */
 public class Problem629_kInversePairs {
 
+    // DP
     public static int kInversePairs(int n, int k) {
         final int MOD = 1000000007;
         int[][] f = new int[2][k + 1];
