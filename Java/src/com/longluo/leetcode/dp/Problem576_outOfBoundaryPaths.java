b2e8eaebef8ef47d2d98c3b2f6127ed2a3d8330c
longluo <calmbody@163.com>
2022-07-20 18:35:26 +0800

    576 DP

--- Java/src/com/longluo/leetcode/dp/Problem576_outOfBoundaryPaths.java
+++ Java/src/com/longluo/leetcode/dp/Problem576_outOfBoundaryPaths.java
@@ -28,12 +28,39 @@ package com.longluo.leetcode.dp;
  */
 public class Problem576_outOfBoundaryPaths {
 
-    //
+    // DP
     public static int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int MOD = 1_000_000_007;
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int outCounts = 0;
+
         int[][] dp = new int[m][n];
 
+        dp[startRow][startColumn] = 1;
 
-        return 0;
+        for (int i = 0; i < maxMove; i++) {
+            int[][] dpNew = new int[m][n];
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    int count = dp[j][k];
+                    if (count > 0) {
+                        for (int[] direction : directions) {
+                            int j1 = j + direction[0], k1 = k + direction[1];
+                            if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n) {
+                                dpNew[j1][k1] = (dpNew[j1][k1] + count) % MOD;
+                            } else {
+                                outCounts = (outCounts + count) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+            dp = dpNew;
+        }
+
+        return outCounts;
     }
 
     public static void main(String[] args) {
