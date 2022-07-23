24f41e9027586a05b4be0d32e49d3d58eac37c8c
longluo <calmbody@163.com>
2022-07-23 23:15:54 +0800

    biweekly 83

---
+++ Java/src/com/longluo/contest/biweekly_contest_83/Problem1.java
@@ -0,0 +1,13 @@
+package com.longluo.contest.biweekly_contest_83;
+
+public class Problem1 {
+
+    public static String bestHand(int[] ranks, char[] suits) {
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
