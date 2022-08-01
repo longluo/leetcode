97268283b9cb3f1bd709b16213ef474c2b3cf571
longluo <calmbody@163.com>
2022-08-01 22:47:54 +0800

    1631 BinarySearch

--- Java/src/com/longluo/leetcode/graph/Problem1631_pathWithMinimumEffort.java
+++ Java/src/com/longluo/leetcode/graph/Problem1631_pathWithMinimumEffort.java
@@ -1,5 +1,6 @@
 package com.longluo.leetcode.graph;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -32,39 +33,109 @@ import java.util.Queue;
  * 1 <= rows, columns <= 100
  * 1 <= heights[i][j] <= 10^6
  * <p>
- * https://leetcode-cn.com/problems/path-with-minimum-effort/
+ * https://leetcode.com/problems/path-with-minimum-effort/
  */
 public class Problem1631_pathWithMinimumEffort {
 
+    // BFS + DP time: O() space: O()
     public static int minimumEffortPath_bfs(int[][] heights) {
         if (heights == null || heights.length == 0 || heights[0].length == 0) {
             return 0;
         }
+
         int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int ans = Integer.MAX_VALUE;
+
         int row = heights.length;
         int col = heights[0].length;
-        boolean[][] vis = new boolean[row][col];
+
+        int[][] dp = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+
+        dp[0][0] = 0;
+
         Queue<int[]> queue = new LinkedList<>();
-        Queue<Integer> cost = new LinkedList<>();
-        queue.offer(new int[]{0, 0});
+        queue.add(new int[]{0, 0});
+
+        boolean[][] vis = new boolean[row][col];
         vis[0][0] = true;
+
         while (!queue.isEmpty()) {
             int size = queue.size();
+
             for (int i = 0; i < size; i++) {
                 int[] curr = queue.poll();
-                int curHeight = heights[curr[0]][curr[1]];
+
+                int x = curr[0];
+                int y = curr[1];
+
                 for (int[] dir : dirs) {
-                    int nextX = curr[0] + dir[0];
-                    int nextY = curr[1] + dir[1];
+                    int nextX = x + dir[0];
+                    int nextY = y + dir[1];
+
+                    if (nextX >= 0 && nextX < row && nextY >= 0 && nextY < col && !vis[nextX][nextY]) {
+                        vis[nextX][nextY] = true;
+                        int cost = Math.abs(heights[nextX][nextY] - heights[x][y]);
+                        dp[nextX][nextY] = Math.min(dp[nextX][nextY], cost);
+                        queue.offer(new int[]{nextX, nextY});
+                    }
                 }
             }
         }
 
-        return ans;
+        return dp[row - 1][col - 1];
+    }
+
+    // BinarySearch time: O(mnlogC) space: O(mn)
+    public static int minimumEffortPath_bs(int[][] heights) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int rows = heights.length;
+        int cols = heights[0].length;
+
+        int left = 0;
+        int right = 1_000_000 - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            boolean[][] visited = new boolean[rows][cols];
+
+            Queue<int[]> queue = new LinkedList<>();
+            queue.offer(new int[]{0, 0});
+
+            visited[0][0] = true;
+
+            while (!queue.isEmpty()) {
+                int[] curPos = queue.poll();
+                for (int[] dir : dirs) {
+                    int nextX = curPos[0] + dir[0];
+                    int nextY = curPos[1] + dir[1];
+
+                    if (nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols && !visited[nextX][nextY]
+                            && Math.abs(heights[nextX][nextY] - heights[curPos[0]][curPos[1]]) <= mid) {
+                        visited[nextX][nextY] = true;
+                        queue.offer(new int[]{nextX, nextY});
+                    }
+                }
+            }
+
+            if (visited[rows - 1][cols - 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 
     public static void main(String[] args) {
+        System.out.println("1 ?= " + minimumEffortPath_bfs(new int[][]{{1, 2, 3}, {3, 8, 4}, {5, 3, 5}}));
+        System.out.println("2 ?= " + minimumEffortPath_bfs(new int[][]{{1, 2, 2}, {3, 8, 2}, {5, 3, 5}}));
 
+        System.out.println("2 ?= " + minimumEffortPath_bs(new int[][]{{1, 2, 2}, {3, 8, 2}, {5, 3, 5}}));
     }
 }
