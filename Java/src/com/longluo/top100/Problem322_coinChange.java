29734622c01b5bdcf67593a1f2016f73160a6474
longluo <calmbody@163.com>
2022-08-05 22:03:14 +0800

    322 BFS AC

--- Java/src/com/longluo/top100/Problem322_coinChange.java
+++ Java/src/com/longluo/top100/Problem322_coinChange.java
@@ -292,6 +292,46 @@ public class Problem322_coinChange {
         return minAns == Integer.MAX_VALUE ? -1 : minAns;
     }
 
+    public static int coinChange_bfs_opt_ac(int[] coins, int amount) {
+        if (amount <= 0) {
+            return 0;
+        }
+
+        int len = coins.length;
+        Arrays.sort(coins);
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(amount);
+
+        boolean[] visited = new boolean[amount + 1];
+        visited[amount] = true;
+
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Integer cur = queue.poll();
+                for (int x : coins) {
+                    int target = cur - x;
+                    if (target == 0) {
+                        return step;
+                    }
+                    if (target < 0) {
+                        break;
+                    }
+                    if (!visited[target]) {
+                        visited[target] = true;
+                        queue.offer(target);
+                    }
+                }
+            }
+
+            step++;
+        }
+
+        return -1;
+    }
+
     // Backtrack time: O() space: O()
     // TLE
     public static int coinChange_bt(int[] coins, int amount) {
@@ -353,5 +393,7 @@ public class Problem322_coinChange {
         System.out.println("3 ?= " + coinChange_bfs(new int[]{1, 2, 5}, 11));
 
         System.out.println("3 ?= " + coinChange_bfs_opt(new int[]{1, 2, 5}, 11));
+
+        System.out.println("3 ?= " + coinChange_bfs_opt_ac(new int[]{1, 2, 5}, 11));
     }
 }
