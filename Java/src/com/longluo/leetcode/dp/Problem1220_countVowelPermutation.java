50fa0fa21ed529aeb31ae527efb1674f1dbe603d
longluo <calmbody@163.com>
2022-08-07 08:53:24 +0800

    1220 DP

--- Java/src/com/longluo/leetcode/dp/Problem1220_countVowelPermutation.java
+++ Java/src/com/longluo/leetcode/dp/Problem1220_countVowelPermutation.java
@@ -1,5 +1,7 @@
 package com.longluo.leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * 1220. 统计元音字母序列的数目
  * <p>
@@ -30,17 +32,21 @@ package com.longluo.leetcode.dp;
  * 提示：
  * 1 <= n <= 2 * 10^4
  * <p>
- * https://leetcode-cn.com/problems/count-vowels-permutation/
+ * https://leetcode.com/problems/count-vowels-permutation/
  */
 public class Problem1220_countVowelPermutation {
 
+    // DP
     public static int countVowelPermutation(int n) {
-        long mod = 1000000007;
+        long mod = 1_000_000_007;
+
         long[] dp = new long[5];
         long[] ndp = new long[5];
+
         for (int i = 0; i < 5; ++i) {
             dp[i] = 1;
         }
+
         for (int i = 2; i <= n; ++i) {
             /* a前面可以为e,u,i */
             ndp[0] = (dp[1] + dp[2] + dp[4]) % mod;
@@ -54,6 +60,7 @@ public class Problem1220_countVowelPermutation {
             ndp[4] = (dp[2] + dp[3]) % mod;
             System.arraycopy(ndp, 0, dp, 0, 5);
         }
+
         long ans = 0;
         for (int i = 0; i < 5; ++i) {
             ans = (ans + dp[i]) % mod;
@@ -62,9 +69,47 @@ public class Problem1220_countVowelPermutation {
         return (int) ans;
     }
 
+    // DP time: O(n) space: O(n)
+    public static int countVowelPermutation_dp(int n) {
+        long mod = 1_000_000_007;
+
+        int[][] dp = new int[n][5];
+        Arrays.fill(dp[0], 1);
+
+        for (int i = 0; i < n - 1; i++) {
+            dp[i + 1][1] += dp[i][0];
+
+            dp[i + 1][0] += dp[i][1];
+            dp[i + 1][2] += dp[i][1];
+
+            dp[i + 1][0] += dp[i][2];
+            dp[i + 1][1] += dp[i][2];
+            dp[i + 1][3] += dp[i][2];
+            dp[i + 1][4] += dp[i][2];
+
+            dp[i + 1][2] += dp[i][3];
+            dp[i + 1][4] += dp[i][3];
+
+            dp[i + 1][0] += dp[i][4];
+
+            for (int j = 0; j < 5; j++) {
+                dp[i + 1][j] %= mod;
+            }
+        }
+
+        long ans = 0;
+        for (int i = 0; i < 5; i++) {
+            ans += dp[n - 1][i];
+        }
+
+        return (int) (ans % mod);
+    }
+
     public static void main(String[] args) {
         System.out.println("5 ?= " + countVowelPermutation(1));
         System.out.println("10 ?= " + countVowelPermutation(2));
         System.out.println("68 ?= " + countVowelPermutation(5));
+
+        System.out.println("68 ?= " + countVowelPermutation_dp(5));
     }
 }
