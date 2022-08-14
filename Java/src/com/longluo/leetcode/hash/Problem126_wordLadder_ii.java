e6ee4911c05eaeb97a73242499c4d0072e58821a
longluo <calmbody@163.com>
2022-08-14 10:30:16 +0800

    126

---
+++ Java/src/com/longluo/leetcode/hash/Problem126_wordLadder_ii.java
@@ -0,0 +1,56 @@
+package com.longluo.leetcode.hash;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 126. 单词接龙 II
+ * <p>
+ * 按字典 wordList 完成从单词 beginWord 到单词 endWord 转化，一个表示此过程的 转换序列 是形式上像 beginWord -> s1 -> s2 -> ... -> sk 这样的单词序列，并满足：
+ * <p>
+ * 每对相邻的单词之间仅有单个字母不同。
+ * 转换过程中的每个单词 si（1 <= i <= k）必须是字典 wordList 中的单词。注意，beginWord 不必是字典 wordList 中的单词。
+ * sk == endWord
+ * 给你两个单词 beginWord 和 endWord ，以及一个字典 wordList 。请你找出并返回所有从 beginWord 到 endWord 的 最短转换序列 ，如果不存在这样的转换序列，返回一个空列表。每个序列都应该以单词列表 [beginWord, s1, s2, ..., sk] 的形式返回。
+ * <p>
+ * 示例 1：
+ * 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+ * 输出：[["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+ * 解释：存在 2 种最短的转换序列：
+ * "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+ * "hit" -> "hot" -> "lot" -> "log" -> "cog"
+ * <p>
+ * 示例 2：
+ * 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+ * 输出：[]
+ * 解释：endWord "cog" 不在字典 wordList 中，所以不存在符合要求的转换序列。
+ * <p>
+ * 提示：
+ * 1 <= beginWord.length <= 5
+ * endWord.length == beginWord.length
+ * 1 <= wordList.length <= 500
+ * wordList[i].length == beginWord.length
+ * beginWord、endWord 和 wordList[i] 由小写英文字母组成
+ * beginWord != endWord
+ * wordList 中的所有单词 互不相同
+ * <p>
+ * https://leetcode.cn/problems/word-ladder-ii/
+ */
+public class Problem126_wordLadder_ii {
+
+    // TODO: 2022/8/14  
+    public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"hot", "dot", "dog", "lot", "log", "cog"};
+        List<String> tst1 = new ArrayList<>();
+        for (String word : words) {
+            tst1.add(word);
+        }
+
+        System.out.println("hit hot dot dog cog ?= " + findLadders("hit", "cog", tst1));
+    }
+}
