6d6e2a08e01b75e5eb9fe7c645b7b7bd97093b55
longluo <calmbody@163.com>
2022-08-15 17:05:31 +0800

    724 BF time: O(n) space: O(1)

--- Java/src/com/longluo/leetcode/prefixsum/Problem724_pivotIndex.java
+++ Java/src/com/longluo/leetcode/prefixsum/Problem724_pivotIndex.java
@@ -1,9 +1,12 @@
-package com.longluo.leetcode.array;
+package com.longluo.leetcode.prefixsum;
 
 /**
  * 724. 寻找数组的中心索引
+ * <p>
  * 给定一个整数类型的数组 nums，请编写一个能够返回数组 “中心索引” 的方法。
+ * <p>
  * 我们是这样定义数组 中心索引 的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和。
+ * <p>
  * 如果数组不存在中心索引，那么我们应该返回 -1。如果数组有多个中心索引，那么我们应该返回最靠近左边的那一个。
  * <p>
  * 示例 1：
@@ -24,28 +27,32 @@ package com.longluo.leetcode.array;
  * 说明：
  * nums 的长度范围为 [0, 10000]。
  * 任何一个 nums[i] 将会是一个范围在 [-1000, 1000]的整数。
+ * <p>
+ * https://leetcode.com/problems/find-pivot-index/
  */
 public class Problem724_pivotIndex {
 
-    public static int pivotIndex(int[] nums) {
+    // BF time: O(n) space: O(1)
+    public static int pivotIndex_bf(int[] nums) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
 
-        int length = nums.length;
-        int totalSum = 0;
-        for (int i = 0; i < length; i++) {
-            totalSum += nums[i];
+        int len = nums.length;
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
         }
 
         int leftSum = 0;
-        int rightSum = totalSum - leftSum;
+        int rightSum = sum;
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < len; i++) {
             if (i > 0) {
                 leftSum += nums[i - 1];
             }
-            rightSum = totalSum - nums[i] - leftSum;
+
+            rightSum = sum - nums[i] - leftSum;
             if (leftSum == rightSum) {
                 return i;
             }
@@ -55,9 +62,11 @@ public class Problem724_pivotIndex {
     }
 
     public static void main(String[] args) {
-        System.out.println("3 ?= " + pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
-        System.out.println("-1 ?= " + pivotIndex(new int[]{1, 2, 3}));
-        System.out.println("-1 ?= " + pivotIndex(new int[]{}));
-        System.out.println("0 ?= " + pivotIndex(new int[]{-1, -1, -1, 0, 1, 1}));
+        System.out.println("3 ?= " + pivotIndex_bf(new int[]{1, 7, 3, 6, 5, 6}));
+
+        System.out.println("3 ?= " + pivotIndex_bf(new int[]{1, 7, 3, 6, 5, 6}));
+        System.out.println("-1 ?= " + pivotIndex_bf(new int[]{1, 2, 3}));
+        System.out.println("-1 ?= " + pivotIndex_bf(new int[]{}));
+        System.out.println("0 ?= " + pivotIndex_bf(new int[]{-1, -1, -1, 0, 1, 1}));
     }
 }
