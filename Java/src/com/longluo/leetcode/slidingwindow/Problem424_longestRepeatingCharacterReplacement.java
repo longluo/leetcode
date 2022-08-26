b23ca712bc47dece1cd1fa997099ac68f70794ff
longluo <calmbody@163.com>
2022-08-26 10:04:52 +0800

    424 HashMap

--- Java/src/com/longluo/leetcode/slidingwindow/Problem424_longestRepeatingCharacterReplacement.java
+++ Java/src/com/longluo/leetcode/slidingwindow/Problem424_longestRepeatingCharacterReplacement.java
@@ -1,5 +1,8 @@
 package com.longluo.leetcode.slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 424. 替换后的最长重复字符
  * <p>
@@ -21,7 +24,12 @@ package com.longluo.leetcode.slidingwindow;
  * 将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
  * 子串 "BBBB" 有最长重复字母, 答案为 4。
  * <p>
- * https://leetcode-cn.com/problems/longest-repeating-character-replacement/
+ * 提示：
+ * 1 <= s.length <= 10^5
+ * s 仅由大写英文字母组成
+ * 0 <= k <= s.length
+ * <p>
+ * https://leetcode.cn/problems/longest-repeating-character-replacement/
  */
 public class Problem424_longestRepeatingCharacterReplacement {
 
@@ -33,7 +41,8 @@ public class Problem424_longestRepeatingCharacterReplacement {
         int[] num = new int[26];
         int n = s.length();
         int maxn = 0;
-        int left = 0, right = 0;
+        int left = 0;
+        int right = 0;
         while (right < n) {
             num[s.charAt(right) - 'A']++;
             maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
@@ -47,8 +56,31 @@ public class Problem424_longestRepeatingCharacterReplacement {
         return right - left;
     }
 
+    // HashMap time: O(n) space: O(26)
+    public static int characterReplacement_hashmap(String s, int k) {
+        int len = s.length();
+        int max = 1;
+        Map<Character, Integer> cntMap = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        while (right < len) {
+            char ch = s.charAt(right);
+            cntMap.put(ch, cntMap.getOrDefault(ch, 0) + 1);
+            max = Math.max(max, cntMap.get(ch));
+            if (right - left + 1 - max > k) {
+                cntMap.put(s.charAt(left), cntMap.get(s.charAt(left)) - 1);
+                left++;
+            }
+
+            right++;
+        }
+
+        return right - left;
+    }
+
     public static void main(String[] args) {
         System.out.println("4 ?= " + characterReplacement("ABAB", 2));
         System.out.println("4 ?= " + characterReplacement("AABABBA", 1));
+        System.out.println("4 ?= " + characterReplacement_hashmap("AABABBA", 1));
     }
 }
