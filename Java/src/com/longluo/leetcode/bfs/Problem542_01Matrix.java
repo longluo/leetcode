43ea107911b7827b214e4b410bc1b116d4aae672
longluo <calmbody@163.com>
2022-08-30 07:31:16 +0800

    542 BFS AC

--- Java/src/com/longluo/leetcode/bfs/Problem542_01Matrix.java
+++ Java/src/com/longluo/leetcode/bfs/Problem542_01Matrix.java
@@ -92,7 +92,48 @@ public class Problem542_01Matrix {
         return level;
     }
 
+    // BFS time: O(mn) space: O(mn)
+    public static int[][] updateMatrix_bfs(int[][] mat) {
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        boolean[][] visited = new boolean[rows][cols];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    queue.offer(new int[]{i, j});
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        int level = 0;
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.poll();
+                for (int[] dir : dirs) {
+                    int nextX = cur[0] + dir[0];
+                    int nextY = cur[1] + dir[1];
+                    if (nextX >= 0 && nextX < rows && nextY >= 0 && nextY < cols && !visited[nextX][nextY]) {
+                        mat[nextX][nextY] = level;
+                        visited[nextX][nextY] = true;
+                        queue.offer(new int[]{nextX, nextY});
+                    }
+                }
+            }
+        }
+
+        return mat;
+    }
+
     public static void main(String[] args) {
         System.out.println("[[0,0,0],[0,1,0],[1,2,1]] ?= " + Arrays.deepToString(updateMatrix(new int[][]{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}})));
+        System.out.println("[[0,0,0],[0,1,0],[1,2,1]] ?= " + Arrays.deepToString(updateMatrix_bfs(new int[][]{{0, 0, 0}, {0, 1, 0}, {1, 1, 1}})));
     }
 }
