0051cf94d14e38f8b574f00bfba92f7e2332d8f4
longluo <calmbody@163.com>
2022-09-02 09:29:05 +0800

    637 DFS

---
+++ Cpp/leetcode/editor/cn/687_longest-univalue-path.cpp
@@ -0,0 +1,73 @@
+//给定一个二叉树的 root ，返回 最长的路径的长度 ，这个路径中的 每个节点具有相同值 。 这条路径可以经过也可以不经过根节点。 
+//
+// 两个节点之间的路径长度 由它们之间的边数表示。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：root = [5,4,5,1,1,5]
+//输出：2
+// 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入：root = [1,4,5,4,4,5]
+//输出：2
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 树的节点数的范围是 [0, 10⁴] 
+// -1000 <= Node.val <= 1000 
+// 树的深度将不超过 1000 
+// 
+// Related Topics 树 深度优先搜索 二叉树 👍 621 👎 0
+
+
+// 2022-09-02 09:17:29
+// By Long Luo
+
+#include <bits/stdc++.h>
+#include <TreeNode.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int longestUnivaluePath(TreeNode *root) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main() {
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    //vector<int> ans = s.twoSum(data,11);
+    //cout << ans[0]<<ans[1]<<endl;
+    cout << "Hello LeetCode" << endl;
+}
+
---
+++ Cpp/leetcode/editor/cn/doc/content/637_average-of-levels-in-binary-tree.md
@@ -0,0 +1,35 @@
+<p>给定一个非空二叉树的根节点<meta charset="UTF-8" />&nbsp;<code>root</code>&nbsp;, 以数组的形式返回每一层节点的平均值。与实际答案相差&nbsp;<code>10<sup>-5</sup></code> 以内的答案可以被接受。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p>
+
+<p><img alt="" src="https://assets.leetcode.com/uploads/2021/03/09/avg1-tree.jpg" /></p>
+
+<pre>
+<strong>输入：</strong>root = [3,9,20,null,null,15,7]
+<strong>输出：</strong>[3.00000,14.50000,11.00000]
+<strong>解释：</strong>第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+因此返回 [3, 14.5, 11] 。
+</pre>
+
+<p><strong>示例 2:</strong></p>
+
+<p><img alt="" src="https://assets.leetcode.com/uploads/2021/03/09/avg2-tree.jpg" /></p>
+
+<pre>
+<strong>输入：</strong>root = [3,9,20,15,7]
+<strong>输出：</strong>[3.00000,14.50000,11.00000]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<p><meta charset="UTF-8" /></p>
+
+<ul>
+	<li>树中节点数量在&nbsp;<code>[1, 10<sup>4</sup>]</code> 范围内</li>
+	<li><code>-2<sup>31</sup>&nbsp;&lt;= Node.val &lt;= 2<sup>31</sup>&nbsp;- 1</code></li>
+</ul>
+<div><div>Related Topics</div><div><li>树</li><li>深度优先搜索</li><li>广度优先搜索</li><li>二叉树</li></div></div><br><div><li>👍 367</li><li>👎 0</li></div>
