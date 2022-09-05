b0a1015579338e18d2d8b698cad413d03ec44b3f
longluo <calmbody@163.com>
2022-09-05 15:01:25 +0800

    451 todo

--- Java/src/com/longluo/leetcode/hash/Problem451_sortCharactersByFrequency.java
+++ Java/src/com/longluo/leetcode/hash/Problem451_sortCharactersByFrequency.java
@@ -76,9 +76,34 @@ public class Problem451_sortCharactersByFrequency {
         return sb.toString();
     }
 
+    // PriorityQueue time: O(nlogn) space: O(n)
+    // TODO: 2022/9/5  
+    public static String frequencySort_pq(String s) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (char ch : s.toCharArray()) {
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         System.out.println("eert ?= " + frequencySort("tree"));
         System.out.println("cccaaa ?= " + frequencySort("cccaaa"));
         System.out.println("bbAa ?= " + frequencySort("Aabb"));
+
+        System.out.println("cccaaa ?= " + frequencySort_pq("cccaaa"));
+        System.out.println("bbAa ?= " + frequencySort_pq("Aabb"));
     }
 }
