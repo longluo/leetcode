cd34dc831696231e840e9227a250526b2d14a0a4
longluo <calmbody@163.com>
2022-09-07 10:09:52 +0800

    606 Recursion

--- Java/src/com/longluo/leetcode/tree/Problem606_constructStringFromBinaryTree.java
+++ Java/src/com/longluo/leetcode/tree/Problem606_constructStringFromBinaryTree.java
@@ -9,6 +9,7 @@ import java.util.Stack;
 
 /**
  * 606. 根据二叉树创建字符串
+ * <p>
  * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
  * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
  * <p>
@@ -36,23 +37,26 @@ import java.util.Stack;
  * <p>
  * 解释: 和第一个示例相似，
  * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ * <p>
+ * https://leetcode.cn/problems/construct-string-from-binary-tree/
  */
 public class Problem606_constructStringFromBinaryTree {
 
-    public static String tree2str(TreeNode t) {
-        if (t == null) {
+    // Recursion time: O(n) space: O(n)
+    public static String tree2str(TreeNode root) {
+        if (root == null) {
             return "";
         }
 
-        if (t != null && t.left == null && t.right == null) {
-            return t.val + "";
+        if (root.left == null && root.right == null) {
+            return root.val + "";
         }
 
-        if (t.right == null) {
-            return t.val + "(" + tree2str(t.left) + ")";
+        if (root.right == null) {
+            return root.val + "(" + tree2str(root.left) + ")";
         }
 
-        return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
+        return root.val + "(" + tree2str(root.left) + ")" + "(" + tree2str(root.right) + ")";
     }
 
     public static String tree2str2(TreeNode t) {
