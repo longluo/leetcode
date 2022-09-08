1f7b01ba93391821783ac3a9fa19f0bf8050adc3
longluo <calmbody@163.com>
2022-09-08 14:31:19 +0800

    814 Recursion time: O(n) space: O(n)

--- Java/src/com/longluo/leetcode/dfs/Problem814_binaryTreePruning.java
+++ Java/src/com/longluo/leetcode/dfs/Problem814_binaryTreePruning.java
@@ -31,12 +31,12 @@ import com.longluo.datastructure.TreeUtils;
  * The number of nodes in the tree is in the range [1, 200].
  * Node.val is either 0 or 1.
  * <p>
- * https://leetcode.com/problems/binary-tree-pruning/
+ * https://leetcode.cn/problems/binary-tree-pruning/
  */
 public class Problem814_binaryTreePruning {
 
     // DFS time: O(n) space: O(n)
-    public static TreeNode pruneTree(TreeNode root) {
+    public static TreeNode pruneTree_dfs(TreeNode root) {
         return containsOne(root) ? root : null;
     }
 
@@ -59,14 +59,31 @@ public class Problem814_binaryTreePruning {
         return root.val == 1 || left || right;
     }
 
+    // Recursion time: O(n) space: O(n)
+    public static TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.val == 0 && root.left == null && root.right == null) {
+            return null;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode tst1 = TreeUtils.constructTree(new Integer[]{1, null, 0, 0, 1});
         TreeUtils.printTree(pruneTree(tst1));
+        TreeUtils.printTree(pruneTree_dfs(tst1));
 
         TreeNode tst2 = TreeUtils.constructTree(new Integer[]{1, 0, 1, 0, 0, 0, 1});
-        TreeUtils.printTree(pruneTree(tst2));
+        TreeUtils.printTree(pruneTree_dfs(tst2));
 
         TreeNode tst3 = TreeUtils.constructTree(new Integer[]{0, null, 0, 0, 0});
-        TreeUtils.printTree(pruneTree(tst3));
+        TreeUtils.printTree(pruneTree_dfs(tst3));
     }
 }
