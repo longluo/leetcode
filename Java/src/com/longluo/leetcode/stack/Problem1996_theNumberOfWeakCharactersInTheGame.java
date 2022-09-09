d6d960a96eb6396cafe2ea68d3acf9ab8a765ac6
longluo <calmbody@163.com>
2022-09-09 20:22:00 +0800

    1996 Greedy time: O(nlogn) space: O(logn)

--- Java/src/com/longluo/leetcode/stack/Problem1996_theNumberOfWeakCharactersInTheGame.java
+++ Java/src/com/longluo/leetcode/stack/Problem1996_theNumberOfWeakCharactersInTheGame.java
@@ -1,5 +1,9 @@
 package com.longluo.leetcode.stack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 1996. 游戏中弱角色的数量
  * <p>
@@ -52,7 +56,25 @@ public class Problem1996_theNumberOfWeakCharactersInTheGame {
         return ans;
     }
 
+    // Greedy time: O(nlogn) space: O(logn)
+    public static int numberOfWeakCharacters_sort(int[][] properties) {
+        Arrays.sort(properties, (p1, p2) -> p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0]);
+
+        int ans = 0;
+        int maxDef = 0;
+        for (int[] p : properties) {
+            if (p[1] < maxDef) {
+                ans++;
+            } else {
+                maxDef = p[1];
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("0 ?= " + numberOfWeakCharacters_bf(new int[][]{{5, 5}, {6, 3}, {3, 6}}));
+        System.out.println("1 ?= " + numberOfWeakCharacters_sort(new int[][]{{2, 2}, {3, 3}}));
     }
 }
