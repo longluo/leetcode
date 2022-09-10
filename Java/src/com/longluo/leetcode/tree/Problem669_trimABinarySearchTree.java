5242d0b812c250d37f5980ea47f0c5c66a81b76f
longluo <calmbody@163.com>
2022-09-10 21:50:03 +0800

    669 DFS

--- Java/src/com/longluo/leetcode/tree/Problem669_trimABinarySearchTree.java
+++ Java/src/com/longluo/leetcode/tree/Problem669_trimABinarySearchTree.java
@@ -7,7 +7,9 @@ import com.longluo.datastructure.TreeUtils;
  * 669. 修剪二叉搜索树
  * <p>
  * 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。
- * 修剪树 不应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。 可以证明，存在 唯一的答案 。
+ * 修剪树 不应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。
+ * <p>
+ * 可以证明，存在 唯一的答案 。
  * 所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
  * <p>
  * 示例 1：
@@ -25,10 +27,11 @@ import com.longluo.datastructure.TreeUtils;
  * 题目数据保证输入是一棵有效的二叉搜索树
  * 0 <= low <= high <= 10^4
  * <p>
- * https://leetcode-cn.com/problems/trim-a-binary-search-tree/
+ * https://leetcode.cn/problems/trim-a-binary-search-tree/
  */
 public class Problem669_trimABinarySearchTree {
 
+    // DFS time: O(n) space: O(n)
     public static TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) {
             return root;
