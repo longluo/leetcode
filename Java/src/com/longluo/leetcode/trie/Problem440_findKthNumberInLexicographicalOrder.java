ab8276315b6aaee4b8eeb420d38ad1e63a2c1223
longluo <calmbody@163.com>
2022-09-14 21:31:40 +0800

    440 DFS AC

--- Java/src/com/longluo/leetcode/trie/Problem440_findKthNumberInLexicographicalOrder.java
+++ Java/src/com/longluo/leetcode/trie/Problem440_findKthNumberInLexicographicalOrder.java
@@ -1,6 +1,8 @@
 package com.longluo.leetcode.trie;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * 440. 字典序的第K小数字
@@ -19,41 +21,116 @@ import java.util.Arrays;
  * 提示:
  * 1 <= k <= n <= 10^9
  * <p>
- * https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/
+ * https://leetcode.cn/problems/k-th-smallest-in-lexicographical-order/
  */
 public class Problem440_findKthNumberInLexicographicalOrder {
 
     // BF time: O(nlogn) space: O(n)
     // Memory Out
     public static int findKthNumber_str(int n, int k) {
-        if (n == 1) {
-            return 1;
-        }
-
-        String[] numStrs = new String[n + 1];
-        for (int i = 0; i <= n; i++) {
-            numStrs[i] = "" + i;
+        String[] numStrs = new String[n];
+        for (int i = 1; i <= n; i++) {
+            numStrs[i - 1] = String.valueOf(i - 1);
         }
 
         Arrays.sort(numStrs);
 
-        return Integer.parseInt(numStrs[k]);
+        return Integer.parseInt(numStrs[k - 1]);
     }
 
-    //
-    public static int findKthNumber(int n, int k) {
-        if (n == 1) {
-            return 1;
-        }
+    // PreOrder Tree
+    public static int findKthNumber_tree(int n, int k) {
 
 
         return 0;
+    }
+
+    private static void buildTree(int n) {
+        int idx = 1;
+
+        Node root = new Node(0);
+        for (int i = 1; i <= 9; i++) {
+
+        }
+    }
+
+    static class Node {
+        int idx;
+        List<Node> children;
+
+        public Node(int idx) {
+            this.idx = idx;
+            this.children = new ArrayList<>();
+        }
+    }
+
+    // DFS
+    // AC
+    static int index = 0;
+    static int ans = 0;
+
+    public static int findKthNumber_dfs(int n, int k) {
+        for (int i = 1; i <= 9; i++) {
+            int cnt = count(n, i, "");
+            if (k > cnt + index) {
+                index += cnt;
+                continue;
+            }
+
+            if (dfs(n, k, "" + i)) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+    private static boolean dfs(int n, int k, String cur) {
+        index++;
+
+        if (index == k) {
+            ans = Integer.valueOf(cur);
+            return true;
+        }
+
+        for (int i = 0; i <= 9; i++) {
+            int cnt = count(n, i, cur);
+            if (k > cnt + index) {
+                index += cnt;
+                continue;
+            }
+
+            if (Integer.valueOf(cur + i) <= n) {
+                if (dfs(n, k, cur + i)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static int count(int n, int i, String prefix) {
+        long cur = Long.valueOf(prefix + i);
+        int res = 0;
+        int start = 1;
+        while (cur <= n) {
+            res += start;
+            cur *= 10;
+            start *= 10;
+        }
+
+        if (n < (cur / 10 + start / 10 - 1)) {
+            res -= cur / 10 + start / 10 - 1 - n;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
         System.out.println("1 ?= " + findKthNumber_str(1, 1));
         System.out.println("10 ?= " + findKthNumber_str(13, 2));
 
-        System.out.println("10 ?= " + findKthNumber(13, 2));
+        System.out.println("10 ?= " + findKthNumber_dfs(13, 2));
     }
 }
