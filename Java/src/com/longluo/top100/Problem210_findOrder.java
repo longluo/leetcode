91f3bc5a42deea46ba807a865178630069c4a44f
longluo <calmbody@163.com>
2022-09-14 18:11:54 +0800

    210 Topo Sort BFS

--- Java/src/com/longluo/top100/Problem210_findOrder.java
+++ Java/src/com/longluo/top100/Problem210_findOrder.java
@@ -88,7 +88,64 @@ public class Problem210_findOrder {
         return new int[0];
     }
 
+    // Topo Sort time: O(v+e) space: O(n)
+    public static int[] findOrder_topo(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> inDegrees = new HashMap<>();
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+        // Build Graph
+        for (int[] edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+
+            graph.putIfAbsent(from, new HashSet<>());
+            graph.get(from).add(to);
+
+            inDegrees.put(to, inDegrees.getOrDefault(to, 0) + 1);
+        }
+
+        return bfs(graph, inDegrees, numCourses);
+    }
+
+    private static int[] bfs(Map<Integer, Set<Integer>> graph, Map<Integer, Integer> inDegreesMap, int nodeNum) {
+        int[] res = new int[nodeNum];
+
+        // count used for attended courses
+        int count = 0;
+
+        // visit non-pre courses first
+        Queue<Integer> toVisit = new ArrayDeque<>();
+        for (int i = 0; i < nodeNum; i++) {
+            if (!inDegreesMap.containsKey(i)) {
+                toVisit.offer(i);
+            }
+        }
+
+        while (!toVisit.isEmpty()) {
+            int curNode = toVisit.poll();
+
+            res[count] = curNode;
+            count++;
+
+            if (!graph.containsKey(curNode)) {
+                continue;
+            }
+
+            Set<Integer> neighbors = graph.get(curNode);
+            for (int x : neighbors) {
+                inDegreesMap.put(x, inDegreesMap.get(x) - 1);
+                if (inDegreesMap.get(x) == 0) {
+                    toVisit.offer(x);
+                }
+            }
+        }
+
+        return count == nodeNum ? res : new int[0];
+    }
+
     public static void main(String[] args) {
         System.out.println("[0, 1] ?= " + Arrays.toString(findOrder(2, new int[][]{{1, 0}})));
+
+        System.out.println("[0, 1] ?= " + Arrays.toString(findOrder_topo(2, new int[][]{{1, 0}})));
     }
 }
