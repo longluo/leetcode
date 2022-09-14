a95356d9f5492e9fa8fdece2b6679ba7ab97ba9f
longluo <calmbody@163.com>
2022-09-14 18:32:49 +0800

    207 Topo Sort by BFS

--- Java/src/com/longluo/top100/Problem207_courseSchedule.java
+++ Java/src/com/longluo/top100/Problem207_courseSchedule.java
@@ -1,7 +1,6 @@
 package com.longluo.top100;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * 207. 课程表
@@ -31,7 +30,7 @@ import java.util.Queue;
  * 0 <= ai, bi < numCourses
  * prerequisites[i] 中的所有课程对 互不相同
  * <p>
- * https://leetcode.com/problems/course-schedule/
+ * https://leetcode.cn/problems/course-schedule/
  */
 public class Problem207_courseSchedule {
 
@@ -69,9 +68,54 @@ public class Problem207_courseSchedule {
         return numCourses == 0;
     }
 
+    // Topo Sort BFS
+    public static boolean canFinish_topo(int numCourses, int[][] prerequisites) {
+        Map<Integer, Integer> inDegreeMap = new HashMap<>();
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+        for (int[] edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+
+            graph.putIfAbsent(from, new HashSet<>());
+            graph.get(from).add(to);
+            inDegreeMap.put(to, inDegreeMap.getOrDefault(to, 0) + 1);
+        }
+
+        int count = 0;
+        Queue<Integer> toVisit = new ArrayDeque<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!inDegreeMap.containsKey(i)) {
+                toVisit.offer(i);
+            }
+        }
+
+        while (!toVisit.isEmpty()) {
+            int curNode = toVisit.poll();
+            count++;
+
+            if (!graph.containsKey(curNode)) {
+                continue;
+            }
+
+            Set<Integer> neighbors = graph.get(curNode);
+            for (int x : neighbors) {
+                inDegreeMap.put(x, inDegreeMap.get(x) - 1);
+                if (inDegreeMap.get(x) == 0) {
+                    toVisit.offer(x);
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+
     public static void main(String[] args) {
         System.out.println("true ?= " + canFinish(1, new int[][]{{}}));
         System.out.println("true ?= " + canFinish(2, new int[][]{{1, 0}}));
         System.out.println("false ?= " + canFinish(2, new int[][]{{1, 0}, {0, 1}}));
+
+        System.out.println("false ?= " + canFinish_topo(2, new int[][]{{1, 0}, {0, 1}}));
     }
 }
