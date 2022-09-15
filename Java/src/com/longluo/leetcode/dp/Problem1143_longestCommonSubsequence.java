6e7869cbaa336a29752667f3bc5eb5268a2aed1e
longluo <calmbody@163.com>
2022-09-15 11:51:12 +0800

    1143 DP time: O(mn) space: O(mn)

--- Java/src/com/longluo/leetcode/dp/Problem1143_longestCommonSubsequence.java
+++ Java/src/com/longluo/leetcode/dp/Problem1143_longestCommonSubsequence.java
@@ -89,10 +89,32 @@ public class Problem1143_longestCommonSubsequence {
         return p == subStr.length() && q <= s.length();
     }
 
+    // DP time: O(mn) space: O(mn)
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
     public static void main(String[] args) {
         System.out.println("3 ?= " + longestCommonSubsequence_bf("abcde", "ace"));
         System.out.println("3 ?= " + longestCommonSubsequence_bf("abc", "abc"));
         System.out.println("0 ?= " + longestCommonSubsequence_bf("abc", "def"));
         System.out.println("1 ?= " + longestCommonSubsequence_bf("psnw", "vozsh"));
+
+        System.out.println("3 ?= " + longestCommonSubsequence("abcde", "ace"));
     }
 }
