f09e7f5dcfb8f685ac0914d6d020272cb3cd48e8
longluo <calmbody@163.com>
2022-09-20 15:32:49 +0800

    718 DP time: O(mn) space: O(mn) AC

--- Java/src/com/longluo/leetcode/slidingwindow/Problem718_maximumLengthOfRepeatedSubarray.java
+++ Java/src/com/longluo/leetcode/slidingwindow/Problem718_maximumLengthOfRepeatedSubarray.java
@@ -23,6 +23,7 @@ package com.longluo.leetcode.slidingwindow;
 public class Problem718_maximumLengthOfRepeatedSubarray {
 
     // BF time: O(n^3) space: O(1)
+    // TLE
     public static int findLength_bf(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
@@ -46,7 +47,31 @@ public class Problem718_maximumLengthOfRepeatedSubarray {
         return max;
     }
 
+    // DP time: O(mn) space: O(mn)
+    // AC
+    public static int findLength_dp(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+
+        int[][] dp = new int[len1][len2];
+
+        int ans = 0;
+
+        for (int i = 0; i < len1; i++) {
+            for (int j = 0; j < len2; j++) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i][j] = i >= 1 && j >= 1 ? dp[i - 1][j - 1] + 1 : 1;
+                }
+
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("3 ?= " + findLength_bf(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7}));
+        System.out.println("3 ?= " + findLength_dp(new int[]{1, 2, 3, 2, 1}, new int[]{3, 2, 1, 4, 7}));
     }
 }
