b4512693de1647e707fcc8023b644f8d6cbdd3eb
longluo <calmbody@163.com>
2022-09-24 21:14:13 +0800

    113 DFS time: O(n) space: O(n)

--- Java/src/com/longluo/leetcode/tree/Problem113_pathSum_ii.java
+++ Java/src/com/longluo/leetcode/tree/Problem113_pathSum_ii.java
@@ -1,7 +1,9 @@
 package com.longluo.leetcode.tree;
 
 import com.longluo.datastructure.TreeNode;
+import com.longluo.datastructure.TreeUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,16 +29,42 @@ import java.util.List;
  * -1000 <= Node.val <= 1000
  * -1000 <= targetSum <= 1000
  * <p>
- * https://leetcode-cn.com/problems/path-sum-ii/
+ * https://leetcode.cn/problems/path-sum-ii/
  */
 public class Problem113_pathSum_ii {
 
+    // DFS time: O(n) space: O(n)
+    // TODO: 2022/9/24  
     public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
 
-        return null;
+        dfs(ans, new ArrayList<>(), root, targetSum);
+        return ans;
+    }
+
+    private static void dfs(List<List<Integer>> res, List<Integer> path, TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.val);
+
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            res.add(new ArrayList<>(path));
+        }
+
+        dfs(res, path, root.left, targetSum - root.val);
+        dfs(res, path, root.right, targetSum - root.val);
+
+        path.remove(path.size() - 1);
     }
 
     public static void main(String[] args) {
-
+        TreeNode tst1 = TreeUtils.constructTree(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1});
+        TreeUtils.printTree(tst1);
+        System.out.println("[[5, 4, 11, 2], [5, 8, 4, 5]] ?= " + pathSum(tst1, 22));
     }
 }
