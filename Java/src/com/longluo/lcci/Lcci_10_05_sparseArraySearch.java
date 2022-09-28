2826def774b1c316e9194e5e338d6f329f6fd32f
longluo <calmbody@163.com>
2022-09-28 18:27:41 +0800

    LCCI 10.05 BinarySearch time: O(nlogn) space: O(1)

--- Java/src/com/longluo/lcci/Lcci_10_05_sparseArraySearch.java
+++ Java/src/com/longluo/lcci/Lcci_10_05_sparseArraySearch.java
@@ -33,7 +33,47 @@ public class Lcci_10_05_sparseArraySearch {
         return -1;
     }
 
+    // BinarySearch time: O(nlogn) space: O(1)
+    public static int findString_bs(String[] words, String s) {
+        int len = words.length;
+
+        int left = 0;
+        int right = len - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            String word = words[mid];
+
+            if (word.length() == 0) {
+                for (int i = 1; mid + i <= right && mid - i >= left; i++) {
+                    if (words[mid + i].length() > 0) {
+                        word = words[mid + i];
+                        mid = mid + i;
+                        break;
+                    } else if (words[mid - i].length() > 0) {
+                        word = words[mid - i];
+                        mid = mid - i;
+                        break;
+                    }
+                }
+            }
+
+            int ret = word.compareTo(s);
+            if (ret < 0) {
+                left = mid + 1;
+            } else if (ret > 0) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println("-1 ?= " + findString(new String[]{"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}, "ta"));
+        System.out.println("-1 ?= " + findString_bs(new String[]{"at", "", "", "", "ball", "", "", "car", "", "", "dad", "", ""}, "ta"));
     }
 }
