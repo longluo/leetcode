bf1f3fd89a7e8c9ced20c76da0544ed28e13f83c
longluo <calmbody@163.com>
2022-10-01 16:00:59 +0800

    面试题 01.06. 字符串压缩

--- Java/src/com/longluo/lcci/Lcci_01_06_compressString.java
+++ Java/src/com/longluo/lcci/Lcci_01_06_compressString.java
@@ -2,6 +2,7 @@ package com.longluo.lcci;
 
 /**
  * 面试题 01.06. 字符串压缩
+ * <p>
  * 字符串压缩。利用字符重复出现的次数，编写一种方法，实现基本的字符串压缩功能。比如，字符串aabcccccaaa会变为a2b1c5a3。
  * 若“压缩”后的字符串没有变短，则返回原先的字符串。你可以假设字符串中只包含大小写英文字母（a至z）。
  * <p>
@@ -16,9 +17,12 @@ package com.longluo.lcci;
  * <p>
  * 提示：
  * 字符串长度在[0, 50000]范围内。
+ * <p>
+ * https://leetcode.cn/problems/compress-string-lcci/
  */
 public class Lcci_01_06_compressString {
 
+    // simulate time: O(n) space: O(n)
     public static String compressString(String S) {
         if (S == null || S.length() <= 2) {
             return S;
