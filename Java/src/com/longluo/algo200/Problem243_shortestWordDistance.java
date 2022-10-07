9321e9d449727d748d2c23a88d174a57b9dc6447
longluo <calmbody@163.com>
2022-10-07 20:29:32 +0800

    243 HashMap time: O(n) space: O(n)

---
+++ Java/src/com/longluo/algo200/Problem243_shortestWordDistance.java
@@ -0,0 +1,58 @@
+package com.longluo.algo200;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 243. 最短单词距离
+ * <p>
+ * 给定一个字符串数组 wordDict 和两个已经存在于该数组中的不同的字符串 word1 和 word2 。返回列表中这两个单词之间的最短距离。
+ * <p>
+ * 示例 1:
+ * 输入: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "coding", word2 = "practice"
+ * 输出: 3
+ * <p>
+ * 示例 2:
+ * 输入: wordsDict = ["practice", "makes", "perfect", "coding", "makes"], word1 = "makes", word2 = "coding"
+ * 输出: 1
+ * <p>
+ * 提示:
+ * 1 <= wordsDict.length <= 3 * 10^4
+ * 1 <= wordsDict[i].length <= 10
+ * wordsDict[i] 由小写英文字母组成
+ * word1 和 word2 在 wordsDict 中
+ * word1 != word2
+ * <p>
+ * https://leetcode.cn/problems/shortest-word-distance/
+ */
+public class Problem243_shortestWordDistance {
+
+    // HashMap time: O(n) space: O(n)
+    public static int shortestDistance(String[] wordsDict, String word1, String word2) {
+        int len = wordsDict.length;
+
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            map.putIfAbsent(wordsDict[i], new ArrayList<>());
+            map.get(wordsDict[i]).add(i);
+        }
+
+        int ans = len;
+        List<Integer> word1Idx = map.get(word1);
+        List<Integer> word2Idx = map.get(word2);
+
+        for (int i = 0; i < word1Idx.size(); i++) {
+            for (int j = 0; j < word2Idx.size(); j++) {
+                ans = Math.min(ans, Math.abs(word1Idx.get(i) - word2Idx.get(j)));
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("3 ?= " + shortestDistance(new String[]{"practice", "makes", "perfect", "coding", "makes"}, "coding", "practice"));
+    }
+}
