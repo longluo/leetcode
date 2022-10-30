04379e6c64578921656b53379345b9760540bb76
longluo <calmbody@163.com>
2022-10-30 10:20:06 +0800

    6226. 摧毁一系列目标
    Module time: O(n + klogk) space: O(n)

--- Java/src/com/longluo/contest/biweekly_contest_90/Problem3.java
+++ Java/src/com/longluo/contest/biweekly_contest_90/Problem3.java
@@ -1,10 +1,8 @@
 package com.longluo.contest.biweekly_contest_90;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
- *
  * https://leetcode.cn/problems/destroy-sequential-targets/
  */
 public class Problem3 {
@@ -43,9 +41,38 @@ public class Problem3 {
         return ans;
     }
 
+    // Module time: O(n + klogk) space: O(n)
+    public static int destroyTargets_mod(int[] nums, int space) {
+        Map<Integer, int[]> map = new HashMap<>();
+
+        for (int x : nums) {
+            int mod = x % space;
+            map.putIfAbsent(mod, new int[]{0, x});
+
+            int[] value = map.get(mod);
+            value[0]++;
+            value[1] = Math.min(value[1], x);
+            map.put(mod, value);
+        }
+
+        List<int[]> list = new ArrayList<>(map.values());
+
+        Collections.sort(list, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return b[0] - a[0];
+        });
+
+        return list.get(0)[1];
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + destroyTargets(new int[]{3, 7, 8, 1, 1, 5}, 2));
         System.out.println("2 ?= " + destroyTargets(new int[]{1, 5, 3, 2, 2}, 10000));
         System.out.println("2 ?= " + destroyTargets(new int[]{6, 2, 5}, 100));
+
+        System.out.println("2 ?= " + destroyTargets_mod(new int[]{1, 5, 3, 2, 2}, 10000));
+        System.out.println("2 ?= " + destroyTargets_mod(new int[]{6, 2, 5}, 100));
     }
 }
