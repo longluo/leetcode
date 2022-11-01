f4cd0cc4f4cea50374f88046cc15d60f0947f21e
longluo <calmbody@163.com>
2022-11-01 23:06:38 +0800

    Weekly Contest 293 2274. 不含特殊楼层的最大连续楼层数

---
+++ Java/src/com/longluo/contest/weekly_contest_293/Problem2.java
@@ -0,0 +1,69 @@
+package com.longluo.contest.weekly_contest_293;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.cn/problems/maximum-consecutive-floors-without-special-floors/
+ */
+public class Problem2 {
+
+    // Memory LE
+    public static int maxConsecutive(int bottom, int top, int[] special) {
+        int floors = top - bottom + 1;
+
+        boolean[] marked = new boolean[floors];
+
+        for (int x : special) {
+            marked[x - bottom] = true;
+        }
+
+        int max = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < floors; i++) {
+            if (marked[i]) {
+                cnt = 0;
+                continue;
+            }
+
+            cnt++;
+            max = Math.max(max, cnt);
+        }
+
+        return max;
+    }
+
+    // TLE
+    public static int maxConsecutive_opt(int bottom, int top, int[] special) {
+        int len = special.length;
+
+        Arrays.sort(special);
+
+        int max = 0;
+        int cnt = 0;
+
+        for (int i = bottom, j = 0; i <= top; i++) {
+            if (j < len && i == special[j]) {
+                cnt = 0;
+                j++;
+                continue;
+            }
+
+            cnt++;
+            max = Math.max(max, cnt);
+        }
+
+        return max;
+    }
+
+    // TODO: 2022/11/1
+
+    public static void main(String[] args) {
+        System.out.println("0 ?= " + maxConsecutive(2, 2, new int[]{2}));
+        System.out.println("3 ?= " + maxConsecutive(2, 9, new int[]{4, 6}));
+        System.out.println("0 ?= " + maxConsecutive(6, 8, new int[]{7, 6, 8}));
+
+        System.out.println("3 ?= " + maxConsecutive_opt(2, 9, new int[]{4, 6}));
+        System.out.println("0 ?= " + maxConsecutive_opt(6, 8, new int[]{7, 6, 8}));
+    }
+}
