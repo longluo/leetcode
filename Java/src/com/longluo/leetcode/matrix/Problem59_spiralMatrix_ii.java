23899d949b87fc163b5d6cc99f7087b116fb815b
longluo <calmbody@163.com>
2022-11-01 15:39:11 +0800

    59. Spiral Matrix II Best Solution

--- Java/src/com/longluo/leetcode/matrix/Problem59_spiralMatrix_ii.java
+++ Java/src/com/longluo/leetcode/matrix/Problem59_spiralMatrix_ii.java
@@ -1,4 +1,4 @@
-package com.longluo.leetcode.array;
+package com.longluo.leetcode.matrix;
 
 import com.longluo.datastructure.ArrayUtils;
 
@@ -18,14 +18,18 @@ import com.longluo.datastructure.ArrayUtils;
  * 提示：
  * 1 <= n <= 20
  * <p>
- * https://leetcode-cn.com/problems/spiral-matrix-ii/
+ * https://leetcode.cn/problems/spiral-matrix-ii/
  */
 public class Problem59_spiralMatrix_ii {
 
+    // BF time: O(n^2) space: O(1)
     public static int[][] generateMatrix(int n) {
         int[][] ans = new int[n][n];
+
         int count = 1;
+
         for (int cycle = 0; cycle <= n / 2; cycle++) {
+            // Right
             for (int i = cycle; i < n - cycle; i++) {
                 ans[cycle][i] = count++;
                 if (count > n * n) {
@@ -33,6 +37,7 @@ public class Problem59_spiralMatrix_ii {
                 }
             }
 
+            // Down
             for (int j = cycle + 1; j < n - cycle; j++) {
                 ans[j][n - cycle - 1] = count++;
                 if (count > n * n) {
@@ -40,6 +45,7 @@ public class Problem59_spiralMatrix_ii {
                 }
             }
 
+            // Left
             for (int k = n - cycle - 2; k >= cycle; k--) {
                 ans[n - cycle - 1][k] = count++;
                 if (count > n * n) {
@@ -47,6 +53,7 @@ public class Problem59_spiralMatrix_ii {
                 }
             }
 
+            // Up
             for (int l = n - cycle - 2; l > cycle; l--) {
                 ans[l][cycle] = count++;
                 if (count > n * n) {
@@ -58,25 +65,31 @@ public class Problem59_spiralMatrix_ii {
         return ans;
     }
 
-    public static int[][] generateMatrix_2(int n) {
-        if (n == 0) {
-            return new int[][]{};
-        }
-
+    // Better time: O(n^2) space: O(1)
+    public static int[][] generateMatrix_opt(int n) {
         int[][] res = new int[n][n];
+
         int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
         boolean[][] visited = new boolean[n][n];
+
         int row = 0;
         int col = 0;
+
         int dirIdx = 0;
+
         for (int i = 1; i <= n * n; i++) {
             res[row][col] = i;
+
             visited[row][col] = true;
+
             int nextRow = row + dirs[dirIdx][0];
             int nextCol = col + dirs[dirIdx][1];
+
             if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || visited[nextRow][nextCol]) {
                 dirIdx = (dirIdx + 1) % 4;
             }
+
             row = row + dirs[dirIdx][0];
             col = col + dirs[dirIdx][1];
         }
@@ -84,14 +97,16 @@ public class Problem59_spiralMatrix_ii {
         return res;
     }
 
+    // 按层模拟 time: O(n^2) space: O(1)
     public static int[][] generateMatrix_3(int n) {
         int[][] res = new int[n][n];
+
+        int count = 1;
 
         int left = 0;
         int right = n - 1;
         int top = 0;
         int bottom = n - 1;
-        int count = 1;
 
         while (left <= right && top <= bottom) {
             for (int i = left; i <= right; i++) {
@@ -125,17 +140,55 @@ public class Problem59_spiralMatrix_ii {
         return res;
     }
 
+    // Best Code time: O(n^2) space: O(1)
+    public static int[][] generateMatrix_best(int n) {
+        int[][] matrix = new int[n][n];
+
+        int left = 0;
+        int right = n - 1;
+        int top = 0;
+        int bottom = n - 1;
+
+        int k = 1;
+
+        while (k <= n * n) {
+            for (int c = left; c <= right; c++, k++) {
+                matrix[top][c] = k;
+            }
+            top++;
+
+            for (int r = top; r <= bottom; r++, k++) {
+                matrix[r][right] = k;
+            }
+            right--;
+
+            for (int c = right; c >= left; c--, k++) {
+                matrix[bottom][c] = k;
+            }
+            bottom--;
+
+            for (int r = bottom; r >= top; r--, k++) {
+                matrix[r][left] = k;
+            }
+            left++;
+        }
+
+        return matrix;
+    }
+
     public static void main(String[] args) {
         System.out.println(" Method 1:");
-        System.out.println("{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}} ?= " + ArrayUtils.print2DArray(generateMatrix(3)));
-        System.out.println("{{1}} ?= " + ArrayUtils.print2DArray(generateMatrix(1)));
+        System.out.println("[[1, 2, 3],[8, 9, 4],[7, 6, 5]] ?= " + ArrayUtils.print2DArray(generateMatrix(3)));
+        System.out.println("[[1]] ?= " + ArrayUtils.print2DArray(generateMatrix(1)));
 
         System.out.println(" Method 2:");
-        System.out.println("{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}} ?= " + ArrayUtils.print2DArray(generateMatrix_2(3)));
-        System.out.println("{{1}} ?= " + ArrayUtils.print2DArray(generateMatrix_2(1)));
+        System.out.println("[[1, 2, 3],[8, 9, 4],[7, 6, 5]] ?= " + ArrayUtils.print2DArray(generateMatrix_opt(3)));
+        System.out.println("[[1]] ?= " + ArrayUtils.print2DArray(generateMatrix_opt(1)));
 
         System.out.println(" Method 3:");
-        System.out.println("{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}} ?= " + ArrayUtils.print2DArray(generateMatrix_3(3)));
-        System.out.println("{{1}} ?= " + ArrayUtils.print2DArray(generateMatrix_3(1)));
+        System.out.println("[[1, 2, 3],[8, 9, 4],[7, 6, 5]] ?= " + ArrayUtils.print2DArray(generateMatrix_3(3)));
+        System.out.println("[[1]] ?= " + ArrayUtils.print2DArray(generateMatrix_3(1)));
+
+        System.out.println("[[1, 2, 3],[8, 9, 4],[7, 6, 5]] ?= " + ArrayUtils.print2DArray(generateMatrix_best(3)));
     }
 }
