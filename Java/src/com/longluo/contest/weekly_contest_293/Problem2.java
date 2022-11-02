b8e9f1194cb2206714339535a297caa881cac3d7
longluo <calmbody@163.com>
2022-11-02 09:49:35 +0800

    Weekly Contest 293 2274. 不含特殊楼层的最大连续楼层数

--- Java/src/com/longluo/contest/weekly_contest_293/Problem2.java
+++ Java/src/com/longluo/contest/weekly_contest_293/Problem2.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Problem2 {
 
     // Memory LE
-    public static int maxConsecutive(int bottom, int top, int[] special) {
+    public static int maxConsecutive_bf(int bottom, int top, int[] special) {
         int floors = top - bottom + 1;
 
         boolean[] marked = new boolean[floors];
@@ -56,12 +56,29 @@ public class Problem2 {
         return max;
     }
 
-    // TODO: 2022/11/1
+    // Sort time: O(nlogn) space: O(logn)
+    public static int maxConsecutive(int bottom, int top, int[] special) {
+        int len = special.length;
+
+        Arrays.sort(special);
+
+        int max = Math.max(special[0] - bottom, top - special[len - 1]);
+
+        for (int i = 1; i < len; i++) {
+            max = Math.max(max, special[i] - special[i - 1] - 1);
+        }
+
+        return max;
+    }
 
     public static void main(String[] args) {
+        System.out.println("0 ?= " + maxConsecutive_bf(2, 2, new int[]{2}));
+        System.out.println("3 ?= " + maxConsecutive_bf(2, 9, new int[]{4, 6}));
+
         System.out.println("0 ?= " + maxConsecutive(2, 2, new int[]{2}));
         System.out.println("3 ?= " + maxConsecutive(2, 9, new int[]{4, 6}));
         System.out.println("0 ?= " + maxConsecutive(6, 8, new int[]{7, 6, 8}));
+        System.out.println("12 ?= " + maxConsecutive(28, 50, new int[]{35, 48}));
 
         System.out.println("3 ?= " + maxConsecutive_opt(2, 9, new int[]{4, 6}));
         System.out.println("0 ?= " + maxConsecutive_opt(6, 8, new int[]{7, 6, 8}));
