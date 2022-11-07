5aa397bc4d39054e26ae923dd0526c873c8328ec
longluo <calmbody@163.com>
2022-11-08 07:53:49 +0800

    1684. 统计一致字符串的数目 HashSet & Count Array

--- Java/src/com/longluo/leetcode/string/Problem1684_countConsistentStrings.java
+++ Java/src/com/longluo/leetcode/string/Problem1684_countConsistentStrings.java
@@ -5,8 +5,10 @@ import java.util.Set;
 
 /**
  * 1684. 统计一致字符串的数目
+ * <p>
  * 给你一个由不同字符组成的字符串allowed和一个字符串数组words。如果一个字符串的每一个字符都在allowed中，
  * 就称这个字符串是一致字符串。
+ * <p>
  * 请你返回 words数组中 一致字符串的数目。
  * <p>
  * 示例 1：
@@ -25,14 +27,17 @@ import java.util.Set;
  * 解释：字符串 "cc"，"acd"，"ac" 和 "d" 是一致字符串。
  * <p>
  * 提示：
- * 1 <= words.length <= 104
+ * 1 <= words.length <= 10^4
  * 1 <= allowed.length <= 26
  * 1 <= words[i].length <= 10
  * allowed 中的字符 互不相同 。
  * words[i] 和 allowed 只包含小写英文字母。
+ * <p>
+ * https://leetcode.cn/problems/count-the-number-of-consistent-strings/
  */
 public class Problem1684_countConsistentStrings {
 
+    // HashSet time: O(n) space: O(n)
     public static int countConsistentStrings(String allowed, String[] words) {
         if (allowed == null || allowed.length() == 0) {
             return 0;
@@ -63,9 +68,36 @@ public class Problem1684_countConsistentStrings {
         return true;
     }
 
+    // Count time: O(n) space: O(26)
+    public static int countConsistentStrings_count(String allowed, String[] words) {
+        int[] count = new int[26];
+        for (char ch : allowed.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        int ans = 0;
+        for (String s : words) {
+            boolean flag = true;
+            for (char ch : s.toCharArray()) {
+                if (count[ch - 'a'] == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("2 ?= " + countConsistentStrings("ab", new String[]{"ad", "bd", "aaab", "baa", "badab"}));
         System.out.println("7 ?= " + countConsistentStrings("abc", new String[]{"a", "b", "c", "ab", "ac", "bc", "abc"}));
         System.out.println("4 ?= " + countConsistentStrings("cad", new String[]{"cc", "acd", "b", "ba", "bac", "bad", "ac", "d"}));
+
+        System.out.println("4 ?= " + countConsistentStrings_count("cad", new String[]{"cc", "acd", "b", "ba", "bac", "bad", "ac", "d"}));
     }
 }
