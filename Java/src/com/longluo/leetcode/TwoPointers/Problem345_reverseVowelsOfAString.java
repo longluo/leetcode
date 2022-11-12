3036203596a2e66c327a8f6f3d8057d90c0797bb
longluo <calmbody@163.com>
2022-11-12 22:28:35 +0800

    345. 反转字符串中的元音字母

--- Java/src/com/longluo/leetcode/TwoPointers/Problem345_reverseVowelsOfAString.java
+++ Java/src/com/longluo/leetcode/TwoPointers/Problem345_reverseVowelsOfAString.java
@@ -1,22 +1,27 @@
-package com.longluo.leetcode.twopointers;
+package com.longluo.leetcode.TwoPointers;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 345. 反转字符串中的元音字母
  * <p>
- * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+ * 给你一个字符串 s ，仅反转字符串中的所有元音字母，并返回结果字符串。
+ * 元音字母包括 'a'、'e'、'i'、'o'、'u'，且可能以大小写两种形式出现。
  * <p>
  * 示例 1：
- * 输入："hello"
+ * 输入：s = "hello"
  * 输出："holle"
  * <p>
  * 示例 2：
- * 输入："leetcode"
+ * 输入：s = "leetcode"
  * 输出："leotcede"
  * <p>
  * 提示：
- * 元音字母不包含字母 "y" 。
+ * 1 <= s.length <= 3 * 10^5
+ * s 由 可打印的 ASCII 字符组成
  * <p>
- * https://leetcode-cn.com/problems/reverse-vowels-of-a-string/
+ * https://leetcode.com/problems/reverse-vowels-of-a-string/
  */
 public class Problem345_reverseVowelsOfAString {
 
@@ -59,8 +64,40 @@ public class Problem345_reverseVowelsOfAString {
         return false;
     }
 
+    // Two Pointers time: O(n) space: O(n)
+    public static String reverseVowels_tp(String s) {
+        Set<Character> vowels = new HashSet<>(Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        int len = s.length();
+
+        char[] array = s.toCharArray();
+
+        for (int i = 0, j = len - 1; i < j; ) {
+            if (!vowels.contains(array[i])) {
+                i++;
+                continue;
+            }
+
+            if (!vowels.contains(array[j])) {
+                j--;
+                continue;
+            }
+
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
+        }
+
+        return new String(array);
+    }
+
     public static void main(String[] args) {
         System.out.println("holle ?= " + reverseVowels("hello"));
         System.out.println("leotcede ?= " + reverseVowels("leetcode"));
+
+        System.out.println("holle ?= " + reverseVowels_tp("hello"));
+        System.out.println("leotcede ?= " + reverseVowels_tp("leetcode"));
     }
 }
