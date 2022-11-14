07dfdac0e018285cfe5978b2c5737e1bc312825d
longluo <calmbody@163.com>
2022-11-14 08:36:05 +0800

    947. 移除最多的同行或同列石头

--- Java/src/com/longluo/leetcode/dfs/Problem947_removeStones.java
+++ Java/src/com/longluo/leetcode/dfs/Problem947_removeStones.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 /**
  * 947. 移除最多的同行或同列石头
+ * <p>
  * n 块石头放置在二维平面中的一些整数坐标点上。每个坐标点上最多只能有一块石头。
  * 如果一块石头的 同行或者同列 上有其他石头存在，那么就可以移除这块石头。
  * 给你一个长度为 n 的数组 stones ，其中stones[i] = [xi, yi]表示第i块石头的位置，返回可以移除的石子的最大数量。
@@ -37,11 +38,68 @@ import java.util.*;
  * 1 <= stones.length <= 1000
  * 0 <= xi, yi <= 10^4
  * 不会有两块石头放在同一个坐标点上
+ * <p>
+ * https://leetcode.cn/problems/most-stones-removed-with-same-row-or-column/
  */
 public class Problem947_removeStones {
 
-    public int removeStones(int[][] stones) {
-        if (stones == null || stones.length == 0 || stones[0].length == 0) {
+    // DFS time: O(n^2) space: O(n)
+    public static int removeStones_dfs(int[][] stones) {
+        int n = stones.length;
+        if (n <= 1) {
+            return 0;
+        }
+
+        List<Integer>[] graph = new List[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            int[] u = stones[i];
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int[] v = stones[j];
+                if (u[0] == v[0] || u[1] == v[1]) {
+                    graph[i].add(j);
+                }
+            }
+        }
+
+        boolean[] visited = new boolean[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            dfs(graph, visited, i);
+            ans++;
+        }
+
+        return n - ans;
+    }
+
+    private static void dfs(List<Integer>[] graph, boolean[] visited, int start) {
+
+        visited[start] = true;
+
+        List<Integer> neighbors = graph[start];
+        for (int x : neighbors) {
+            if (visited[x]) {
+                continue;
+            }
+
+            dfs(graph, visited, x);
+        }
+    }
+
+    public static int removeStones_uf(int[][] stones) {
+        if (stones == null || stones.length <= 1) {
             return 0;
         }
 
@@ -53,7 +111,7 @@ public class Problem947_removeStones {
         return stones.length - unionFind.getCount();
     }
 
-    private class UnionFind {
+    static class UnionFind {
         private Map<Integer, Integer> parent;
         private int count;
 
@@ -92,8 +150,12 @@ public class Problem947_removeStones {
     }
 
     public static void main(String[] args) {
-//        System.out.println("5 ?= " + removeStones(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}}));
-//        System.out.println("3 ?= " + removeStones(new int[][]{{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}}));
-//        System.out.println("0 ?= " + removeStones(new int[][]{{0, 0}}));
+        System.out.println("0 ?= " + removeStones_uf(new int[][]{{0, 0}}));
+        System.out.println("5 ?= " + removeStones_uf(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}}));
+        System.out.println("3 ?= " + removeStones_uf(new int[][]{{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}}));
+
+        System.out.println("0 ?= " + removeStones_dfs(new int[][]{{0, 0}}));
+        System.out.println("5 ?= " + removeStones_dfs(new int[][]{{0, 0}, {0, 1}, {1, 0}, {1, 2}, {2, 1}, {2, 2}}));
+        System.out.println("3 ?= " + removeStones_dfs(new int[][]{{0, 0}, {0, 2}, {1, 1}, {2, 0}, {2, 2}}));
     }
 }
