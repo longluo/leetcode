67da0b0ad2ad16aaccb114a04ae39f030951b40a
longluo <calmbody@163.com>
2022-11-17 08:39:14 +0800

    223. 矩形面积 Simulate

--- Java/src/com/longluo/leetcode/math/Problem223_computeArea.java
+++ Java/src/com/longluo/leetcode/math/Problem223_computeArea.java
@@ -20,129 +20,62 @@ package com.longluo.leetcode.math;
  * 提示：
  * -10^4 <= ax1, ay1, ax2, ay2, bx1, by1, bx2, by2 <= 10^4
  * <p>
- * https://leetcode-cn.com/problems/rectangle-area/
+ * https://leetcode.cn/problems/rectangle-area/
  */
 public class Problem223_computeArea {
 
+    // Simulate time: O(1) space: O(1)
     public static int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int area1 = (ax2 - ax1) * (ay2 - ay1);
-        int area2 = (bx2 - bx1) * (by2 - by1);
-        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
-        int overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
-        int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
-        return area1 + area2 - overlapArea;
-    }
+        if (ax1 > bx1) {
+            return computeArea(bx1, by1, bx2, by2, ax1, ay1, ax2, ay2);
+        }
 
-    public static int computeArea_1(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
         int wA = Math.abs(ax2 - ax1);
         int hA = Math.abs(ay2 - ay1);
 
         int wB = Math.abs(bx2 - bx1);
         int hB = Math.abs(by2 - by1);
 
-        int ans = 0;
         int areaA = wA * hA;
         int areaB = wB * hB;
-        if (isAContainB(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)) {
-            return areaA;
-        } else if (isBContainA(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)) {
-            return areaB;
-        } else if (isASeparateB(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2)) {
+
+        // No Overlap
+        // 3 Conditions
+        if (ay2 <= by1 || ax2 <= bx1 || ay1 >= by2) {
             return areaA + areaB;
         } else {
-            int crossW = 0;
-            int crossH = 0;
-            if (bx1 >= ax1 && bx1 <= ax2 && bx2 > ax2 && by2 >= ay1 && by2 <= ay2) {
-                crossW = ax2 - bx1;
-                crossH = ay2 - by1;
-            } else if (bx1 >= ax1 && bx1 <= ax2 && bx2 > ax2 && by1 >= ay1 && by1 <= ay2) {
-                crossW = ax2 - bx1;
-                crossH = ay2 - by1;
-            } else if (bx2 >= ax1 && bx2 <= ax2 && bx1 < ax1 && by2 >= ay1 && by2 <= ay2) {
-                crossW = bx2 - ax1;
-                crossH = by2 - ay1;
-            } else if (bx2 >= ax1 && bx2 <= ax2 && bx1 < ax1 && by1 >= ay1 && by1 <= ay2) {
-                crossW = bx2 - ax1;
-                crossH = ay2 - by1;
-            } else if (bx1 <= ax1 && bx2 >= ax2 && by2 >= ay1 && by2 <= ay2) {
-                crossW = ax2 - ax1;
-                crossH = by2 - ay1;
-            } else if (bx1 <= ax1 && bx2 >= ax2 && by1 >= ay1 && by1 <= ay2) {
-                crossW = ax2 - ax1;
-                crossH = ay2 - by1;
-            } else if (bx1 >= ax1 && bx2 <= ax2 && by2 >= ay1 && by2 <= ay2) {
-                crossW = bx2 - bx1;
-                crossH = by2 - ay1;
-            } else if (bx1 >= ax1 && bx2 <= ax2 && by1 >= ay1 && by1 <= ay2) {
-                crossW = bx2 - bx1;
-                crossH = ay2 - by1;
-            } else if (by1 <= ay1 && by2 >= ay2 && bx1 >= ax1 && bx1 <= ax2) {
-                crossW = ax2 - bx1;
-                crossH = ay2 - ay1;
-            } else if (by1 >= ay1 && by2 <= ay2 && bx1 >= ax1 && bx1 <= ax2) {
-                crossW = ax2 - bx1;
-                crossH = by2 - by1;
-            } else if (by1 <= ay1 && by2 >= ay2 && bx2 >= ax1 && bx2 <= ax2) {
-                crossW = bx2 - ax1;
-                crossH = ay2 - ay1;
-            } else if (by1 >= ay1 && by2 <= ay2 && bx2 >= ax1 && bx2 <= ax2) {
-                crossW = ax2 - bx1;
-                crossH = by2 - by1;
-            }
-
-            return areaA + areaB - crossW * crossH;
+            int overlapW = Math.min(ax2, bx2) - bx1;
+            int overlapH = Math.min(ay2, by2) - Math.max(ay1, by1);
+            return areaA + areaB - overlapW * overlapH;
         }
     }
 
-    public static boolean isAContainB(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (ax1 <= bx1 && ay1 <= by1 && ax2 >= bx2 && ay2 >= by2) {
-            return true;
-        }
+    // Geometry time: O(1) space: O(1)
+    public static int computeArea_math(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int areaA = (ax2 - ax1) * (ay2 - ay1);
+        int areaB = (bx2 - bx1) * (by2 - by1);
 
-        return false;
+        int overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
+        int overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
+
+        int overlapArea = Math.max(overlapWidth, 0) * Math.max(overlapHeight, 0);
+
+        return areaA + areaB - overlapArea;
     }
 
-    public static boolean isBContainA(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (ax1 >= bx1 && ay1 >= by1 && ax2 <= bx2 && ay2 <= by2) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isASeparateB(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (bx1 >= ax2 || ax1 >= bx2 || by1 >= ay2 || ay1 >= by2) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isAcrossBRight(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (bx1 >= ax1 && bx1 <= ax2 && by2 >= ay1 && by2 <= ay2) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean isAcrossBLeft(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        if (bx2 >= ax1 && bx2 <= ax2 && by1 >= ay1 && by2 <= ay2) {
-            return true;
-        }
-
-        return false;
-    }
 
     public static void main(String[] args) {
         System.out.println("45 ?= " + computeArea(-3, 0, 3, 4, 0, -1, 9, 2));
         System.out.println("16 ?= " + computeArea(-2, -2, 2, 2, -2, -2, 2, 2));
-        System.out.println("24 ?= " + computeArea(-2, -2, 2, 2, -2, 2, 2, 4));
-        System.out.println("19 ?= " + computeArea(-2, -2, 2, 2, 1, 1, 3, 3));
-        System.out.println("19 ?= " + computeArea(-2, -2, 2, 2, -3, 1, -1, 3));
-        System.out.println("24 ?= " + computeArea(-2, -2, 2, 2, -3, -3, 3, -1));
-        System.out.println("24 ?= " + computeArea(-2, -2, 2, 2, 1, -3, 3, 3));
-        System.out.println("24 ?= " + computeArea(-3, -3, 3, -1, -2, -2, 2, 2));
-        System.out.println("45 ?= " + computeArea(-3, 0, 3, 4, 0, -1, 9, 2));
+
+        System.out.println("45 ?= " + computeArea_math(-3, 0, 3, 4, 0, -1, 9, 2));
+        System.out.println("16 ?= " + computeArea_math(-2, -2, 2, 2, -2, -2, 2, 2));
+        System.out.println("24 ?= " + computeArea_math(-2, -2, 2, 2, -2, 2, 2, 4));
+        System.out.println("19 ?= " + computeArea_math(-2, -2, 2, 2, 1, 1, 3, 3));
+        System.out.println("19 ?= " + computeArea_math(-2, -2, 2, 2, -3, 1, -1, 3));
+        System.out.println("24 ?= " + computeArea_math(-2, -2, 2, 2, -3, -3, 3, -1));
+        System.out.println("24 ?= " + computeArea_math(-2, -2, 2, 2, 1, -3, 3, 3));
+        System.out.println("24 ?= " + computeArea_math(-3, -3, 3, -1, -2, -2, 2, 2));
+        System.out.println("45 ?= " + computeArea_math(-3, 0, 3, 4, 0, -1, 9, 2));
     }
 }
