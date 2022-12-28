aa2ea0d290ae7fe7d13aca2e48551e2b420c498e
longluo <calmbody@163.com>
2022-12-28 09:30:20 +0800

    1750. 删除字符串两端相同字符后的最短长度

---
+++ Java/src/com/longluo/leetcode/TwoPointers/Problem1750_minimumLengthOfStringAfterDeletingSimilarEnds.java
@@ -0,0 +1,77 @@
+package com.longluo.leetcode.TwoPointers;
+
+/**
+ * 1750. 删除字符串两端相同字符后的最短长度
+ * <p>
+ * 给你一个只包含字符 'a'，'b' 和 'c' 的字符串 s ，你可以执行下面这个操作（5 个步骤）任意次：
+ * <p>
+ * 选择字符串 s 一个 非空 的前缀，这个前缀的所有字符都相同。
+ * 选择字符串 s 一个 非空 的后缀，这个后缀的所有字符都相同。
+ * 前缀和后缀在字符串中任意位置都不能有交集。
+ * 前缀和后缀包含的所有字符都要相同。
+ * 同时删除前缀和后缀。
+ * 请你返回对字符串 s 执行上面操作任意次以后（可能 0 次），能得到的 最短长度 。
+ * <p>
+ * 示例 1：
+ * 输入：s = "ca"
+ * 输出：2
+ * 解释：你没法删除任何一个字符，所以字符串长度仍然保持不变。
+ * <p>
+ * 示例 2：
+ * 输入：s = "cabaabac"
+ * 输出：0
+ * 解释：最优操作序列为：
+ * - 选择前缀 "c" 和后缀 "c" 并删除它们，得到 s = "abaaba" 。
+ * - 选择前缀 "a" 和后缀 "a" 并删除它们，得到 s = "baab" 。
+ * - 选择前缀 "b" 和后缀 "b" 并删除它们，得到 s = "aa" 。
+ * - 选择前缀 "a" 和后缀 "a" 并删除它们，得到 s = "" 。
+ * <p>
+ * 示例 3：
+ * 输入：s = "aabccabba"
+ * 输出：3
+ * 解释：最优操作序列为：
+ * - 选择前缀 "aa" 和后缀 "a" 并删除它们，得到 s = "bccabb" 。
+ * - 选择前缀 "b" 和后缀 "bb" 并删除它们，得到 s = "cca" 。
+ * <p>
+ * 提示：
+ * 1 <= s.length <= 10^5
+ * s 只包含字符 'a'，'b' 和 'c' 。
+ * <p>
+ * https://leetcode.cn/problems/minimum-length-of-string-after-deleting-similar-ends/
+ */
+public class Problem1750_minimumLengthOfStringAfterDeletingSimilarEnds {
+
+    // Two Pointers time: O(n) space: O(1)
+    public static int minimumLength(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftCh = s.charAt(left);
+
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+
+            while (left < right && s.charAt(left) == leftCh) {
+                left++;
+            }
+
+            while (left <= right && s.charAt(right) == leftCh) {
+                right--;
+            }
+        }
+
+        return left > right ? 0 : right - left + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1 ?= " + minimumLength("a"));
+        System.out.println("0 ?= " + minimumLength("aa"));
+        System.out.println("1 ?= " + minimumLength("aba"));
+        System.out.println("2 ?= " + minimumLength("ca"));
+        System.out.println("0 ?= " + minimumLength("cabaabac"));
+        System.out.println("3 ?= " + minimumLength("aabccabba"));
+        System.out.println("1 ?= " + minimumLength("bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb"));
+    }
+}
