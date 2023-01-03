3754e6d2486cb79a95e9a23b4f249d8dc250628d
longluo <calmbody@163.com>
2023-01-03 09:34:05 +0800

    944. 删列造序

---
+++ Java/src/com/longluo/leetcode/string/Problem944_deleteColumnsToMakeSorted.java
@@ -0,0 +1,79 @@
+package com.longluo.leetcode.string;
+
+/**
+ * 944. 删列造序
+ * <p>
+ * 给你由 n 个小写字母字符串组成的数组 strs，其中每个字符串长度相等。
+ * <p>
+ * 这些字符串可以每个一行，排成一个网格。例如，strs = ["abc", "bce", "cae"] 可以排列为：
+ * <p>
+ * abc
+ * bce
+ * cae
+ * 你需要找出并删除 不是按字典序升序排列的 列。在上面的例子（下标从 0 开始）中，
+ * 列 0（'a', 'b', 'c'）和列 2（'c', 'e', 'e'）都是按升序排列的，而列 1（'b', 'c', 'a'）不是，所以要删除列 1 。
+ * <p>
+ * 返回你需要删除的列数。
+ * <p>
+ * 示例 1：
+ * 输入：strs = ["cba","daf","ghi"]
+ * 输出：1
+ * 解释：网格示意如下：
+ * cba
+ * daf
+ * ghi
+ * 列 0 和列 2 按升序排列，但列 1 不是，所以只需要删除列 1 。
+ * <p>
+ * 示例 2：
+ * 输入：strs = ["a","b"]
+ * 输出：0
+ * 解释：网格示意如下：
+ * a
+ * b
+ * 只有列 0 这一列，且已经按升序排列，所以不用删除任何列。
+ * <p>
+ * 示例 3：
+ * 输入：strs = ["zyx","wvu","tsr"]
+ * 输出：3
+ * 解释：网格示意如下：
+ * zyx
+ * wvu
+ * tsr
+ * 所有 3 列都是非升序排列的，所以都要删除。
+ * <p>
+ * 提示：
+ * n == strs.length
+ * 1 <= n <= 100
+ * 1 <= strs[i].length <= 1000
+ * strs[i] 由小写英文字母组成
+ * <p>
+ * https://leetcode.cn/problems/delete-columns-to-make-sorted/
+ */
+public class Problem944_deleteColumnsToMakeSorted {
+
+    // Simulate time: O(mn) space: O(1)
+    public static int minDeletionSize(String[] strs) {
+        int n = strs.length;
+
+        int len = strs[0].length();
+
+        int ans = 0;
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("0 ?= " + minDeletionSize(new String[]{"a", "b"}));
+        System.out.println("1 ?= " + minDeletionSize(new String[]{"cba", "daf", "ghi"}));
+        System.out.println("3 ?= " + minDeletionSize(new String[]{"zyx", "wvu", "tsr"}));
+    }
+}
