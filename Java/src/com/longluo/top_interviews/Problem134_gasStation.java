2fa55b206f0545f335888ca938e439938e0930a9
longluo <calmbody@163.com>
2023-01-07 09:45:47 +0800

    134. 加油站

--- Java/src/com/longluo/top_interviews/Problem134_gasStation.java
+++ Java/src/com/longluo/top_interviews/Problem134_gasStation.java
@@ -1,18 +1,81 @@
 package com.longluo.top_interviews;
 
 /**
- *
- *
- * https://leetcode.com/problems/gas-station/
+ * 134. 加油站
+ * <p>
+ * 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+ * <p>
+ * 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。
+ * 你从其中的一个加油站出发，开始时油箱为空。
+ * <p>
+ * 给定两个整数数组 gas 和 cost ，如果你可以绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。
+ * 如果存在解，则 保证 它是 唯一 的。
+ * <p>
+ * 示例 1:
+ * 输入: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+ * 输出: 3
+ * 解释:
+ * 从 3 号加油站(索引为 3 处)出发，可获得 4 升汽油。此时油箱有 = 0 + 4 = 4 升汽油
+ * 开往 4 号加油站，此时油箱有 4 - 1 + 5 = 8 升汽油
+ * 开往 0 号加油站，此时油箱有 8 - 2 + 1 = 7 升汽油
+ * 开往 1 号加油站，此时油箱有 7 - 3 + 2 = 6 升汽油
+ * 开往 2 号加油站，此时油箱有 6 - 4 + 3 = 5 升汽油
+ * 开往 3 号加油站，你需要消耗 5 升汽油，正好足够你返回到 3 号加油站。
+ * 因此，3 可为起始索引。
+ * <p>
+ * 示例 2:
+ * 输入: gas = [2,3,4], cost = [3,4,3]
+ * 输出: -1
+ * 解释:
+ * 你不能从 0 号或 1 号加油站出发，因为没有足够的汽油可以让你行驶到下一个加油站。
+ * 我们从 2 号加油站出发，可以获得 4 升汽油。 此时油箱有 = 0 + 4 = 4 升汽油
+ * 开往 0 号加油站，此时油箱有 4 - 3 + 2 = 3 升汽油
+ * 开往 1 号加油站，此时油箱有 3 - 3 + 3 = 3 升汽油
+ * 你无法返回 2 号加油站，因为返程需要消耗 4 升汽油，但是你的油箱只有 3 升汽油。
+ * 因此，无论怎样，你都不可能绕环路行驶一周。
+ * <p>
+ * 提示:
+ * gas.length == n
+ * cost.length == n
+ * 1 <= n <= 10^5
+ * 0 <= gas[i], cost[i] <= 10^4
+ * <p>
+ * https://leetcode.cn/problems/gas-station/
  */
 public class Problem134_gasStation {
 
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    // Simulate time: O(n^2) space: O(1)
+    // TLE
+    public static int canCompleteCircuit(int[] gas, int[] cost) {
+        int len = gas.length;
 
-        return 0;
+        for (int i = 0; i < len; i++) {
+            if (gas[i] < cost[i]) {
+                continue;
+            }
+
+            int oil = gas[i];
+            boolean flag = true;
+            for (int j = i + 1; j <= len + i; j++) {
+                if (oil < cost[(j - 1) % len]) {
+                    flag = false;
+                    break;
+                }
+
+                oil = oil - cost[(j - 1) % len] + gas[j % len];
+            }
+
+            if (flag) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
-
+        System.out.println("3 ?= " + canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2}));
+        System.out.println("-1 ?= " + canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3}));
+        System.out.println("4 ?= " + canCompleteCircuit(new int[]{5, 1, 2, 3, 4}, new int[]{4, 4, 1, 5, 1}));
     }
 }
