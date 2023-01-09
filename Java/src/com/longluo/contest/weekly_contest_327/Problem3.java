c3c86ec5740891608c2d4baba7a7c0d1f73213ae
longluo <calmbody@163.com>
2023-01-09 08:40:10 +0800

    6284. 使字符串总不同字符的数目相等 HashMap

--- Java/src/com/longluo/contest/weekly_contest_327/Problem3.java
+++ Java/src/com/longluo/contest/weekly_contest_327/Problem3.java
@@ -4,7 +4,9 @@ package com.longluo.contest.weekly_contest_327;
  * https://leetcode.cn/contest/weekly-contest-327
  */
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -100,6 +102,61 @@ public class Problem3 {
         return false;
     }
 
+    // HashMap time: O(mn) space: O(m+n)
+    // TLE
+    public static boolean isItPossible_hashmap(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        Map<Character, Integer> mapA = new HashMap<>();
+        Map<Character, Integer> mapB = new HashMap<>();
+
+        for (char ch : word1.toCharArray()) {
+            mapA.put(ch, mapA.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : word2.toCharArray()) {
+            mapB.put(ch, mapB.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int cntA = mapA.size();
+                int cntB = mapB.size();
+
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    if (cntA == cntB) {
+                        return true;
+                    }
+
+                    continue;
+                }
+
+                if (mapA.get(word1.charAt(i)) == 1) {
+                    cntA--;
+                }
+
+                if (!mapA.containsKey(word2.charAt(j))) {
+                    cntA++;
+                }
+
+                if (!mapB.containsKey(word1.charAt(i))) {
+                    cntB++;
+                }
+
+                if (mapB.get(word2.charAt(j)) == 1) {
+                    cntB--;
+                }
+
+                if (cntA == cntB) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("false ?= " + isItPossible("ac", "b"));
         System.out.println("true ?= " + isItPossible("ab", "b"));
@@ -109,5 +166,10 @@ public class Problem3 {
         System.out.println("false ?= " + isItPossible_opt("ac", "b"));
         System.out.println("true ?= " + isItPossible_opt("ab", "b"));
         System.out.println("false ?= " + isItPossible_opt("a", "bb"));
+
+        System.out.println("false ?= " + isItPossible_hashmap("aa", "ab"));
+        System.out.println("false ?= " + isItPossible_hashmap("aa", "bcd"));
+        System.out.println("true ?= " + isItPossible_hashmap("ab", "b"));
+        System.out.println("false ?= " + isItPossible_hashmap("a", "bb"));
     }
 }
