864e70ce179d0fa019d0a151cea6cea492dcb643
longluo <calmbody@163.com>
2023-01-10 08:33:34 +0800

    100. 相同的树 BFS time: O(n) space: O(n)

--- Java/src/com/longluo/leetcode/Tree/Problem100_sameTree.java
+++ Java/src/com/longluo/leetcode/Tree/Problem100_sameTree.java
@@ -3,6 +3,9 @@ package com.longluo.leetcode.Tree;
 import com.longluo.datastructure.TreeNode;
 import com.longluo.datastructure.TreeUtils;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 100. 相同的树
  * <p>
@@ -43,10 +46,56 @@ public class Problem100_sameTree {
         return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 
+    // BFS time: O(n) space: O(n)
+    public static boolean isSameTree_bfs(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        Queue<TreeNode> queueA = new LinkedList<>();
+        Queue<TreeNode> queueB = new LinkedList<>();
+
+        queueA.offer(p);
+        queueB.offer(q);
+
+        while (!queueA.isEmpty() && !queueB.isEmpty()) {
+            if (queueA.size() != queueB.size()) {
+                return false;
+            }
+
+            int size = queueA.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curNodeA = queueA.poll();
+                TreeNode curNodeB = queueB.poll();
+
+                if (curNodeA == null && curNodeB == null) {
+                    continue;
+                }
+
+                if (curNodeA == null || curNodeB == null || curNodeA.val != curNodeB.val) {
+                    return false;
+                }
+
+                queueA.offer(curNodeA.left);
+                queueA.offer(curNodeA.right);
+
+                queueB.offer(curNodeB.left);
+                queueB.offer(curNodeB.right);
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         TreeNode tst1 = TreeUtils.constructTree(new Integer[]{1, 2, 3});
         TreeNode tst2 = TreeUtils.constructTree(new Integer[]{1, 2, 3});
 
         System.out.println("true ?= " + isSameTree(tst1, tst2));
+        System.out.println("true ?= " + isSameTree_bfs(tst1, tst2));
     }
 }
