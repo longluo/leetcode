338325d8f3384e99772ff0abf9bb73e68b99ac70
longluo <calmbody@163.com>
2023-01-30 08:25:25 +0800

    1137. N-th Tribonacci Number DP

--- Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
+++ Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
@@ -38,6 +38,7 @@ public class Problem1137_nThTribonacciNumber {
         return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     }
 
+    // DP time: O(n) space: O(n)
     public static int tribonacci_dp(int n) {
         if (n == 0) {
             return 0;
@@ -45,16 +46,17 @@ public class Problem1137_nThTribonacciNumber {
             return 1;
         }
 
-        int[] res = new int[n + 1];
+        int[] dp = new int[n + 1];
 
-        res[0] = 0;
-        res[1] = res[2] = 1;
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
 
         for (int i = 3; i <= n; i++) {
-            res[i] = res[i - 3] + res[i - 2] + res[i - 1];
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
 
-        return res[n];
+        return dp[n];
     }
 
     public static int tribonacci_iter(int n) {
