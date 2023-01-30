bb9d56c581da2925aa1da26e964da67de06daead
longluo <calmbody@163.com>
2023-01-30 08:22:44 +0800

    1137. N-th Tribonacci Number Recursion TLE

--- Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
+++ Java/src/com/longluo/leetcode/memoization/Problem1137_nThTribonacciNumber.java
@@ -22,12 +22,12 @@ package com.longluo.leetcode.memoization;
  * 0 <= n <= 37
  * 答案保证是一个 32 位整数，即 answer <= 2^31 - 1。
  * <p>
- * https://leetcode-cn.com/problems/n-th-tribonacci-number/
- * <p>
- * https://leetcode.com/problems/n-th-tribonacci-number/
+ * https://leetcode.cn/problems/n-th-tribonacci-number/
  */
 public class Problem1137_nThTribonacciNumber {
 
+    // Recursion time: O(n) space: O(n)
+    // TLE
     public static int tribonacci(int n) {
         if (n == 0) {
             return 0;
@@ -35,7 +35,18 @@ public class Problem1137_nThTribonacciNumber {
             return 1;
         }
 
+        return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+    }
+
+    public static int tribonacci_dp(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        }
+
         int[] res = new int[n + 1];
+
         res[0] = 0;
         res[1] = res[2] = 1;
 
@@ -66,18 +77,6 @@ public class Problem1137_nThTribonacciNumber {
         }
 
         return ans;
-    }
-
-    public static int tribonacci_recursive(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 1;
-        }
-
-        return tribonacci_recursive(n - 1) + tribonacci_recursive(n - 2) + tribonacci_recursive(n - 3);
     }
 
     static int[] nums = {0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, 121415, 223317, 410744, 755476, 1389537, 2555757, 4700770, 8646064, 15902591, 29249425, 53798080, 98950096, 181997601, 334745777, 615693474, 1132436852, 2082876103};
@@ -122,18 +121,19 @@ public class Problem1137_nThTribonacciNumber {
     }
 
     public static void main(String[] args) {
-        System.out.println("0 ?= " + tribonacci(0));
+        System.out.println("0 ?= " + tribonacci_dp(0));
         System.out.println("0 ?= " + tribonacci_iter(0));
-        System.out.println("1 ?= " + tribonacci(1));
+        System.out.println("1 ?= " + tribonacci_dp(1));
         System.out.println("1 ?= " + tribonacci_iter(1));
-        System.out.println("1 ?= " + tribonacci(2));
+        System.out.println("1 ?= " + tribonacci_dp(2));
         System.out.println("1 ?= " + tribonacci_iter(2));
-        System.out.println("2 ?= " + tribonacci(3));
+        System.out.println("2 ?= " + tribonacci_dp(3));
         System.out.println("2 ?= " + tribonacci_iter(3));
-        System.out.println("4 ?= " + tribonacci(4));
+        System.out.println("4 ?= " + tribonacci_dp(4));
         System.out.println("4 ?= " + tribonacci_iter(4));
-        System.out.println("1389537 ?= " + tribonacci(25));
+        System.out.println("1389537 ?= " + tribonacci_dp(25));
         System.out.println("1389537 ?= " + tribonacci_iter(25));
+        System.out.println("1389537 ?= " + tribonacci(25));
 
         for (int i = 0; i <= 37; i++) {
             System.out.print("," + tribonacci_iter(i));
