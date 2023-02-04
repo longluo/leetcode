978757eb49abc00ce15f88e39c429671968c4d26
longluo <calmbody@163.com>
2023-02-04 23:23:47 +0800

    Biweekly Contest 97 Problem 1 Method 2

--- Java/src/com/longluo/contest/biweekly_contest_97/Problem1.java
+++ Java/src/com/longluo/contest/biweekly_contest_97/Problem1.java
@@ -28,7 +28,37 @@ public class Problem1 {
         return ans;
     }
 
+    public static int[] separateDigits_opt(int[] nums) {
+        List<Integer> digits = new ArrayList<>();
+
+        for (int x : nums) {
+            List<Integer> values = getDigits(x);
+            for (int i = values.size() - 1; i >= 0; i--) {
+                digits.add(values.get(i));
+            }
+        }
+
+        int[] ans = new int[digits.size()];
+        for (int i = 0; i < digits.size(); i++) {
+            ans[i] = digits.get(i);
+        }
+
+        return ans;
+    }
+
+    private static List<Integer> getDigits(int num) {
+        List<Integer> list = new ArrayList<>();
+
+        while (num > 0) {
+            list.add(num % 10);
+            num /= 10;
+        }
+
+        return list;
+    }
+
     public static void main(String[] args) {
         System.out.println("[1,3,2,5,8,3,7,7] ?= " + Arrays.toString(separateDigits(new int[]{13, 25, 83, 77})));
+        System.out.println("[1,3,2,5,8,3,7,7] ?= " + Arrays.toString(separateDigits_opt(new int[]{13, 25, 83, 77})));
     }
 }
