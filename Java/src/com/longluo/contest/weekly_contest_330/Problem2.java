6f47fb2cc85ae60760bea677ec51f080d8267e59
longluo <calmbody@163.com>
2023-02-20 11:43:19 +0800

    Weekly Contest 330 2550. 猴子碰撞的方法数 Quick Power Still TLE

--- Java/src/com/longluo/contest/weekly_contest_330/Problem2.java
+++ Java/src/com/longluo/contest/weekly_contest_330/Problem2.java
@@ -23,9 +23,39 @@ public class Problem2 {
         return (int) (sum - 2);
     }
 
+    // Quick Power time: O(logn) space: O(1)
+    // TLE
+    public static int monkeyMove_quick(int n) {
+        int mod = 1_000_000_007;
+
+        int ans = (int) quickPower(2, n);
+
+        if (ans == 1) {
+            ans += mod;
+        }
+
+        return ans - 2;
+    }
+
+    private static long quickPower(int base, int power) {
+        if (power <= 1) {
+            return base;
+        }
+
+        int mod = 1_000_000_007;
+
+        if (power % 2 == 1) {
+            return base * quickPower(base, power / 2) * quickPower(base, power / 2) % mod;
+        } else {
+            return quickPower(base, power / 2) * quickPower(base, power / 2) % mod;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("6 ?= " + monkeyMove(3));
         System.out.println("14 ?= " + monkeyMove(4));
         System.out.println("1000000006 ?= " + monkeyMove(500000003));
+
+        System.out.println("1000000006 ?= " + monkeyMove_quick(500000003));
     }
 }
