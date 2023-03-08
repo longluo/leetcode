e309a2a327c136d3c7cce8956a96e6df52e287ef
longluo <calmbody@163.com>
2023-03-08 09:11:43 +0800

    剑指 Offer 47. 礼物的最大价值 DP

--- Java/src/com/longluo/offer/Offer47_maxValue.java
+++ Java/src/com/longluo/offer/Offer47_maxValue.java
@@ -2,6 +2,7 @@ package com.longluo.offer;
 
 /**
  * 剑指 Offer 47. 礼物的最大价值
+ * <p>
  * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
  * 你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。
  * 给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
@@ -19,18 +20,23 @@ package com.longluo.offer;
  * 提示：
  * 0 < grid.length <= 200
  * 0 < grid[0].length <= 200
+ * <p>
+ * https://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof/
  */
 public class Offer47_maxValue {
 
+    // DP time: O(n^2) space: O(n^2)
     public static int maxValue(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
 
         int[][] dp = new int[row][col];
+
         dp[0][0] = grid[0][0];
         for (int i = 1; i < row; i++) {
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
+
         for (int i = 1; i < col; i++) {
             dp[0][i] = dp[0][i - 1] + grid[0][i];
         }
