362b2c8ce4ae40fc1d8048c643348fbef0d11b5d
longluo <calmbody@163.com>
2023-03-12 22:05:19 +0800

    weekly contest 336 problem 3 O(n)

--- Java/src/com/longluo/contest/weekly_contest_336/Problem3.java
+++ Java/src/com/longluo/contest/weekly_contest_336/Problem3.java
@@ -4,6 +4,9 @@ package com.longluo.contest.weekly_contest_336;
  * https://leetcode.cn/contest/weekly-contest-336
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.cn/problems/count-the-number-of-beautiful-subarrays/
  */
@@ -14,7 +17,7 @@ public class Problem3 {
     public static long beautifulSubarrays(int[] nums) {
         int len = nums.length;
 
-        int ans = 0;
+        long ans = 0;
 
         int[] xors = new int[len + 1];
 
@@ -33,9 +36,34 @@ public class Problem3 {
         return ans;
     }
 
+    // Map + PrefixSums time: O(n) space: O(n)
+    public static long beautifulSubarrays_opt(int[] nums) {
+        int len = nums.length;
+
+        long[] xors = new long[len + 1];
+
+        for (int i = 0; i < len; i++) {
+            xors[i + 1] = nums[i] ^ xors[i];
+        }
+
+        long ans = 0;
+
+        Map<Long, Integer> cntMap = new HashMap<>();
+
+        for (int i = 0; i <= len; i++) {
+            ans += cntMap.getOrDefault(xors[i], 0);
+            cntMap.put(xors[i], cntMap.getOrDefault(xors[i], 0) + 1);
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("1 ?= " + beautifulSubarrays(new int[]{0}));
         System.out.println("0 ?= " + beautifulSubarrays(new int[]{1, 10, 4}));
         System.out.println("2 ?= " + beautifulSubarrays(new int[]{4, 3, 1, 2, 4}));
+
+        System.out.println("0 ?= " + beautifulSubarrays_opt(new int[]{1, 10, 4}));
+        System.out.println("2 ?= " + beautifulSubarrays_opt(new int[]{4, 3, 1, 2, 4}));
     }
 }
