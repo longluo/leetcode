899ef34a2fef366c0bb9dce6ccce72c434b576e7
longluo <calmbody@163.com>
2023-04-06 08:38:58 +0800

    1254. 统计封闭岛屿的数目

--- Java/src/com/longluo/studyplan/graph/Problem1254_numberOfClosedIslands.java
+++ Java/src/com/longluo/studyplan/graph/Problem1254_numberOfClosedIslands.java
@@ -35,7 +35,7 @@ import java.util.Queue;
  * 1 <= grid.length, grid[0].length <= 100
  * 0 <= grid[i][j] <=1
  * <p>
- * https://leetcode-cn.com/problems/number-of-closed-islands/
+ * https://leetcode.cn/problems/number-of-closed-islands/
  */
 public class Problem1254_numberOfClosedIslands {
 
@@ -119,6 +119,115 @@ public class Problem1254_numberOfClosedIslands {
                     visited[nextX][nextY] = true;
                 }
             }
+        }
+    }
+
+
+    public static int closedIsland_uf(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int lands = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    lands++;
+                }
+            }
+        }
+
+        UnionFind uf = new UnionFind(m * n, lands);
+
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 0) {
+                uf.initEdge(i * n);
+            }
+            if (grid[i][n - 1] == 0) {
+                uf.initEdge(i * n + n - 1);
+            }
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            if (grid[0][j] == 0) {
+                uf.initEdge(j);
+            }
+            if (grid[m - 1][j] == 0) {
+                uf.initEdge((m - 1) * n + j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    if (i > 0 && grid[i - 1][j] == 0) {
+                        uf.union(i * n + j, (i - 1) * n + j);
+                    }
+
+                    if (j > 0 && grid[i][j - 1] == 0) {
+                        uf.union(i * n + j, i * n + j - 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+
+    static class UnionFind {
+        private int[] parent;
+        private int[] rank;
+        private boolean[] onEdge;
+        private int count;
+
+        public UnionFind(int n, int count) {
+            parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+
+            rank = new int[n];
+            onEdge = new boolean[n];
+            this.count = count;
+        }
+
+        public void initEdge(int x) {
+            if (!onEdge[x]) {
+                parent[x] = 0;
+                onEdge[x] = true;
+                count--;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                    onEdge[rootx] |= onEdge[rooty];
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                    onEdge[rooty] |= onEdge[rootx];
+                } else {
+                    parent[rooty] = rootx;
+                    onEdge[rootx] |= onEdge[rooty];
+                    rank[rootx]++;
+                }
+                count--;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public int getCount() {
+            return count;
         }
     }
 
