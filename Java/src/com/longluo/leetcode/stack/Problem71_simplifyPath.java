d5182c94ba2f589a4a10880cc14cf7c78315c504
longluo <calmbody@163.com>
2023-04-12 17:28:55 +0800

    71. 简化路径 Stack time: O(n) space: O(n)

--- Java/src/com/longluo/leetcode/stack/Problem71_simplifyPath.java
+++ Java/src/com/longluo/leetcode/stack/Problem71_simplifyPath.java
@@ -10,12 +10,16 @@ import java.util.Stack;
  * 给你一个字符串 path ，表示指向某一文件或目录的 Unix 风格 绝对路径 （以 '/' 开头），请你将其转化为更加简洁的规范路径。
  * 在 Unix 风格的文件系统中，一个点（.）表示当前目录本身；此外，两个点 （..） 表示将目录切换到上一级（指向父目录）；
  * 两者都可以是复杂相对路径的组成部分。任意多个连续的斜杠（即，'//'）都被视为单个斜杠 '/' 。
+ * <p>
  * 对于此问题，任何其他格式的点（例如，'...'）均被视为文件/目录名称。
+ * <p>
  * 请注意，返回的 规范路径 必须遵循下述格式：
+ * <p>
  * 始终以斜杠 '/' 开头。
  * 两个目录名之间必须只有一个斜杠 '/' 。
  * 最后一个目录名（如果存在）不能 以 '/' 结尾。
  * 此外，路径仅包含从根目录到目标文件或目录的路径上的目录（即，不含 '.' 或 '..'）。
+ * <p>
  * 返回简化后得到的 规范路径 。
  * <p>
  * 示例 1：
@@ -42,16 +46,18 @@ import java.util.Stack;
  * path 由英文字母，数字，'.'，'/' 或 '_' 组成。
  * path 是一个有效的 Unix 风格绝对路径。
  * <p>
- * https://leetcode-cn.com/problems/simplify-path/
+ * https://leetcode.cn/problems/simplify-path/
  */
 public class Problem71_simplifyPath {
 
+    // Stack time: O(n) space: O(n)
     public static String simplifyPath(String path) {
         if (path == null || path.length() <= 1) {
             return "/";
         }
 
         Stack<String> pathStack = new Stack<>();
+
         StringBuilder ans = new StringBuilder();
         path = path.replaceAll("\\/\\/", "/");
         String[] folders = path.split("\\/");
