be806a3984a49446848ea2606f078b0c67ebc25f
longluo <calmbody@163.com>
2023-04-14 22:02:43 +0800

    1178. 猜字谜

--- Java/src/com/longluo/leetcode/BitManipulation/Problem1178_numberOfValidWordsForEachPuzzle.java
+++ Java/src/com/longluo/leetcode/BitManipulation/Problem1178_numberOfValidWordsForEachPuzzle.java
@@ -1,4 +1,4 @@
-package com.longluo.leetcode.bitmanipulation;
+package com.longluo.leetcode.BitManipulation;
 
 import java.util.*;
 
@@ -6,9 +6,11 @@ import java.util.*;
  * 1178. 猜字谜
  * <p>
  * 外国友人仿照中国字谜设计了一个英文版猜字谜小游戏，请你来猜猜看吧。
+ * <p>
  * 字谜的迷面 puzzle 按字符串形式给出，如果一个单词 word 符合下面两个条件，那么它就可以算作谜底：
  * 单词 word 中包含谜面 puzzle 的第一个字母。
  * 单词 word 中的每一个字母都可以在谜面 puzzle 中找到。
+ * <p>
  * 例如，如果字谜的谜面是 "abcdefg"，
  * 那么可以作为谜底的单词有 "faced", "cabbage", 和 "baggage"；而 "beefed"（不含字母 "a"）以及 "based"（其中的 "s" 没有出现在谜面中）都不能作为谜底。
  * 返回一个答案数组 answer，数组中的每个元素 nswer[i]是在给出的单词列表words中可以作为字谜迷面puzzles[i]所对应的谜底的单词数目。
@@ -34,10 +36,11 @@ import java.util.*;
  * words[i][j], puzzles[i][j] 都是小写英文字母。
  * 每个 puzzles[i] 所包含的字符都不重复。
  * <p>
- * https://leetcode-cn.com/problems/number-of-valid-words-for-each-puzzle/
+ * https://leetcode.cn/problems/number-of-valid-words-for-each-puzzle/
  */
 public class Problem1178_numberOfValidWordsForEachPuzzle {
 
+    // TLE
     public static List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
         List<Integer> ans = new ArrayList<>();
         if (words == null || words.length == 0) {
