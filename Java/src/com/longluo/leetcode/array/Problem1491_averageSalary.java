6d782b22066ceb447802d493e0b4e20c6fabe230
longluo <calmbody@163.com>
2023-05-01 08:51:14 +0800

    1491. 去掉最低工资和最高工资后的工资平均值 API

--- Java/src/com/longluo/leetcode/array/Problem1491_averageSalary.java
+++ Java/src/com/longluo/leetcode/array/Problem1491_averageSalary.java
@@ -1,11 +1,12 @@
 package com.longluo.leetcode.array;
 
+import java.util.Arrays;
+
 /**
  * 1491. 去掉最低工资和最高工资后的工资平均值
  * <p>
  * 给你一个整数数组 salary ，数组里每个数都是 唯一 的，其中 salary[i] 是第 i 个员工的工资。
  * 请你返回去掉最低工资和最高工资以后，剩下员工工资的平均值。
- * <p>
  * <p>
  * 示例 1：
  * 输入：salary = [4000,3000,1000,2000]
@@ -33,15 +34,19 @@ package com.longluo.leetcode.array;
  * salary[i] 是唯一的。
  * 与真实值误差在 10^-5 以内的结果都将视为正确答案。
  * <p>
- * https://leetcode-cn.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/
+ * https://leetcode.cn/problems/average-salary-excluding-the-minimum-and-maximum-salary/
  */
 public class Problem1491_averageSalary {
 
+    // Simulate time: O(n) space: O(1)
     public static double average(int[] salary) {
         int sum = 0;
+
         int len = salary.length;
+
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
+
         for (int num : salary) {
             sum += num;
             min = Math.min(min, num);
@@ -54,7 +59,18 @@ public class Problem1491_averageSalary {
         return (double) sum / (len - 2);
     }
 
-    public static void main(String[] args) {
+    // API time: O(n) space: O(1)
+    public static double average_api(int[] salary) {
+        int sum = Arrays.stream(salary).sum();
 
+        int min = Arrays.stream(salary).min().getAsInt();
+        int max = Arrays.stream(salary).max().getAsInt();
+
+        return (double) (sum - max - min)/ (salary.length - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2500.0 ?= " + average(new int[]{4000, 3000, 1000, 2000}));
+        System.out.println("2500.0 ?= " + average_api(new int[]{4000, 3000, 1000, 2000}));
     }
 }
