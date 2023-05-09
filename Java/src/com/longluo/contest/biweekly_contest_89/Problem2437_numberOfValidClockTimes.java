2c9e63c3d20e5d30db4ed5f29b63ae6137153938
longluo <calmbody@163.com>
2023-05-09 15:58:25 +0800

    2437. 有效时间的数目

--- Java/src/com/longluo/contest/biweekly_contest_89/Problem2437_numberOfValidClockTimes.java
+++ Java/src/com/longluo/contest/biweekly_contest_89/Problem2437_numberOfValidClockTimes.java
@@ -1,7 +1,45 @@
 package com.longluo.contest.biweekly_contest_89;
 
-public class Problem1 {
+/**
+ * https://leetcode.cn/contest/biweekly-contest-89/
+ */
 
+/**
+ * 2437. 有效时间的数目
+ * <p>
+ * 给你一个长度为 5 的字符串 time ，表示一个电子时钟当前的时间，格式为 "hh:mm" 。
+ * 最早 可能的时间是 "00:00" ，最晚 可能的时间是 "23:59" 。
+ * <p>
+ * 在字符串 time 中，被字符 ? 替换掉的数位是 未知的 ，被替换的数字可能是 0 到 9 中的任何一个。
+ * <p>
+ * 请你返回一个整数 answer ，将每一个 ? 都用 0 到 9 中一个数字替换后，可以得到的有效时间的数目。
+ * <p>
+ * 示例 1：
+ * 输入：time = "?5:00"
+ * 输出：2
+ * 解释：我们可以将 ? 替换成 0 或 1 ，得到 "05:00" 或者 "15:00" 。注意我们不能替换成 2 ，因为时间 "25:00" 是无效时间。所以我们有两个选择。
+ * <p>
+ * 示例 2：
+ * 输入：time = "0?:0?"
+ * 输出：100
+ * 解释：两个 ? 都可以被 0 到 9 之间的任意数字替换，所以我们总共有 100 种选择。
+ * <p>
+ * 示例 3：
+ * 输入：time = "??:??"
+ * 输出：1440
+ * 解释：小时总共有 24 种选择，分钟总共有 60 种选择。所以总共有 24 * 60 = 1440 种选择。
+ * <p>
+ * 提示：
+ * time 是一个长度为 5 的有效字符串，格式为 "hh:mm" 。
+ * "00" <= hh <= "23"
+ * "00" <= mm <= "59"
+ * 字符串中有的数位是 '?' ，需要用 0 到 9 之间的数字替换。
+ * <p>
+ * https://leetcode.cn/problems/number-of-valid-clock-times/
+ */
+public class Problem2437_numberOfValidClockTimes {
+
+    // Simulate
     public static int countTime(String time) {
         String hour = time.substring(0, 2);
         String minute = time.substring(3);
