16e1fa4101b464625c2dfd1fde5f95599bdebf65
longluo <calmbody@163.com>
2023-05-09 16:05:46 +0800

    T2 LCP 18. 早餐组合

--- Java/src/com/longluo/LCCUP/LCCUP_2020_Fall_Personal/T2_LCP18_breakfastNumber.java
+++ Java/src/com/longluo/LCCUP/LCCUP_2020_Fall_Personal/T2_LCP18_breakfastNumber.java
@@ -1,11 +1,13 @@
-package com.longluo.LCCUP;
+package com.longluo.LCCUP.LCCUP_2020_Fall_Personal;
 
 import java.util.Arrays;
 
 /**
  * LCP 18. 早餐组合
  * <p>
- * 小扣在秋日市集选择了一家早餐摊位，一维整型数组 staple 中记录了每种主食的价格，一维整型数组 drinks 中记录了每种饮料的价格。
+ * 小扣在秋日市集选择了一家早餐摊位，一维整型数组 staple 中记录了每种主食的价格，
+ * 一维整型数组 drinks 中记录了每种饮料的价格。
+ * <p>
  * 小扣的计划选择一份主食和一款饮料，且花费不超过 x 元。请返回小扣共有多少种购买方案。
  * <p>
  * 注意：答案需要以 1e9 + 7 (1000000007) 为底取模，如：计算初始结果为：1000000008，请返回 1
@@ -44,7 +46,26 @@ import java.util.Arrays;
  * <p>
  * https://leetcode.cn/problems/2vYnGI/
  */
-public class LCP18_breakfastNumber {
+public class T2_LCP18_breakfastNumber {
+
+    // BF time: O(mn) space: O(1)
+    // TLE
+    public static int breakfastNumber_bf(int[] staple, int[] drinks, int x) {
+        int MOD = 1_000_000_007;
+
+        int ans = 0;
+
+        for (int i = 0; i < staple.length; i++) {
+            for (int j = 0; j < drinks.length; j++) {
+                if (staple[i] + drinks[j] <= x) {
+                    ans++;
+                    ans %= MOD;
+                }
+            }
+        }
+
+        return ans % MOD;
+    }
 
     // Sort + Two Pointers time: O(nlogn + n) space: O(logn)
     public static int breakfastNumber(int[] staple, int[] drinks, int x) {
@@ -119,5 +140,6 @@ public class LCP18_breakfastNumber {
         System.out.println("6 ?= " + breakfastNumber(new int[]{10, 20, 5}, new int[]{5, 5, 2}, 15));
         System.out.println("8 ?= " + breakfastNumber(new int[]{2, 1, 1}, new int[]{8, 9, 5, 1}, 9));
         System.out.println("8 ?= " + breakfastNumber_bs(new int[]{2, 1, 1}, new int[]{8, 9, 5, 1}, 9));
+        System.out.println("8 ?= " + breakfastNumber_bf(new int[]{2, 1, 1}, new int[]{8, 9, 5, 1}, 9));
     }
 }
