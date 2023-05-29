395b29d1b050aa3a6a3a507cb953b55e3f5d6e37
longluo <calmbody@163.com>
2023-05-29 09:05:21 +0800

    1603. 设计停车系统 Simulate

--- Java/src/com/longluo/studyplan/programming_skills/Problem1603_designParkingSystem.java
+++ Java/src/com/longluo/studyplan/programming_skills/Problem1603_designParkingSystem.java
@@ -7,6 +7,7 @@ package com.longluo.studyplan.programming_skills;
  * <p>
  * 请你实现 ParkingSystem 类：
  * ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+ * <p>
  * bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
  * <p>
  * 示例 1：
