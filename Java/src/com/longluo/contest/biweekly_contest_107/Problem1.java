ab752562b70d6df7a454cba103a1568681b66ee7
longluo <calmbody@163.com>
2023-06-24 23:20:51 +0800

    biweekly-contest-107 problem 1 HashSet

--- Java/src/com/longluo/contest/biweekly_contest_107/Problem1.java
+++ Java/src/com/longluo/contest/biweekly_contest_107/Problem1.java
@@ -1,5 +1,8 @@
 package com.longluo.contest.biweekly_contest_107;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * https://leetcode.cn/contest/biweekly-contest-107
  */
@@ -36,9 +39,36 @@ public class Problem1 {
         return ans;
     }
 
+    public static int maximumNumberOfStringPairs_set(String[] words) {
+        Set<String> set = new HashSet<>();
+
+        for (String word : words) {
+            set.add(word);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (!set.contains(words[i]) || words[i].charAt(0) == words[i].charAt(1)) {
+                continue;
+            }
+
+            String reverse = new StringBuilder(words[i]).reverse().toString();
+
+            if (set.contains(reverse)) {
+                ans++;
+                set.remove(words[i]);
+                set.remove(reverse);
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println("0 ?= " + maximumNumberOfStringPairs(new String[]{"aa", "ab"}));
         System.out.println("1 ?= " + maximumNumberOfStringPairs(new String[]{"ab", "ba", "cc"}));
         System.out.println("2 ?= " + maximumNumberOfStringPairs(new String[]{"cd", "ac", "dc", "ca", "zz"}));
+        System.out.println("2 ?= " + maximumNumberOfStringPairs_set(new String[]{"cd", "ac", "dc", "ca", "zz"}));
     }
 }
