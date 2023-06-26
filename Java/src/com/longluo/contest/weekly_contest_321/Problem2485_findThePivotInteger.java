dc63cbd3db6048c4194343e6058060de911bcfd7
longluo <calmbody@163.com>
2023-06-26 09:28:37 +0800

    2485. 找出中枢整数

--- Java/src/com/longluo/contest/weekly_contest_321/Problem2485_findThePivotInteger.java
+++ Java/src/com/longluo/contest/weekly_contest_321/Problem2485_findThePivotInteger.java
@@ -5,9 +5,35 @@ package com.longluo.contest.weekly_contest_321;
  */
 
 /**
+ * 2485. 找出中枢整数
+ * <p>
+ * 给你一个正整数 n ，找出满足下述条件的 中枢整数 x ：
+ * <p>
+ * 1 和 x 之间的所有元素之和等于 x 和 n 之间所有元素之和。
+ * <p>
+ * 返回中枢整数 x 。如果不存在中枢整数，则返回 -1 。题目保证对于给定的输入，至多存在一个中枢整数。
+ * <p>
+ * 示例 1：
+ * 输入：n = 8
+ * 输出：6
+ * 解释：6 是中枢整数，因为 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21 。
+ * <p>
+ * 示例 2：
+ * 输入：n = 1
+ * 输出：1
+ * 解释：1 是中枢整数，因为 1 = 1 。
+ * <p>
+ * 示例 3：
+ * 输入：n = 4
+ * 输出：-1
+ * 解释：可以证明不存在满足题目要求的整数。
+ * <p>
+ * 提示：
+ * 1 <= n <= 1000
+ * <p>
  * https://leetcode.cn/problems/find-the-pivot-integer/
  */
-public class Problem1 {
+public class Problem2485_findThePivotInteger {
 
     // Math time: O(n) space: O(1)
     public static int pivotInteger(int n) {
