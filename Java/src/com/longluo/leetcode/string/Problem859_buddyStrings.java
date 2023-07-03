f2766a39fa211f3e1789741cf4c98738b951a116
longluo <calmbody@163.com>
2023-07-03 09:17:41 +0800

    859. 亲密字符串 Simulate

--- Java/src/com/longluo/leetcode/string/Problem859_buddyStrings.java
+++ Java/src/com/longluo/leetcode/string/Problem859_buddyStrings.java
@@ -5,6 +5,7 @@ package com.longluo.leetcode.string;
  * <p>
  * 给你两个字符串 s 和 goal ，只要我们可以通过交换 s 中的两个字母得到与 goal 相等的结果，就返回 true ；
  * 否则返回 false 。
+ * <p>
  * 交换字母的定义是：取两个下标 i 和 j （下标从 0 开始）且满足 i != j ，接着交换 s[i] 和 s[j] 处的字符。
  * 例如，在 "abcd" 中交换下标 0 和下标 2 的元素可以生成 "cbad" 。
  * <p>
@@ -12,8 +13,8 @@ package com.longluo.leetcode.string;
  * 输入：s = "ab", goal = "ba"
  * 输出：true
  * 解释：你可以交换 s[0] = 'a' 和 s[1] = 'b' 生成 "ba"，此时 s 和 goal 相等。
- * 示例 2：
  * <p>
+ * 示例 2：
  * 输入：s = "ab", goal = "ab"
  * 输出：false
  * 解释：你只能交换 s[0] = 'a' 和 s[1] = 'b' 生成 "ba"，此时 s 和 goal 不相等。
@@ -31,7 +32,7 @@ package com.longluo.leetcode.string;
  * 1 <= s.length, goal.length <= 2 * 10^4
  * s 和 goal 由小写英文字母组成
  * <p>
- * https://leetcode-cn.com/problems/buddy-strings/
+ * https://leetcode.cn/problems/buddy-strings/
  */
 public class Problem859_buddyStrings {
 
@@ -43,8 +44,10 @@ public class Problem859_buddyStrings {
         }
 
         int len = s.length();
+
         int first = -1;
         int second = -1;
+
         if (s.equals(goal)) {
             int[] count = new int[26];
             for (int i = 0; i < len; i++) {
@@ -54,6 +57,7 @@ public class Problem859_buddyStrings {
                     return true;
                 }
             }
+
             return false;
         } else {
             for (int i = 0; i < len; i++) {
