5d2357f6be4be30af33b302c88bc4c36f4519759
longluo <calmbody@163.com>
2025-11-09 10:05:43 +0800

    weekly contest 363 problem 2

---
+++ Java/src/com/longluo/contest/weekly_contest_363/Problem2.java
@@ -0,0 +1,30 @@
+package com.longluo.contest.weekly_contest_363;
+
+import java.util.*;
+
+/**
+ * https://leetcode.cn/contest/weekly-contest-363
+ */
+public class Problem2 {
+
+    public static int countWays(List<Integer> nums) {
+        int n = nums.size();
+
+        int[][] dp = new int[n + 1][2];
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+
+        int ans = 0;
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
