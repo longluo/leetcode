4de5e609b154536634e3c250641e4bd577ff8570
longluo <calmbody@163.com>
2025-11-26 23:20:20 +0800

    biweekly contest 99 problem 3

---
+++ Java/src/com/longluo/contest/biweekly_contest_99/Problem3.java
@@ -0,0 +1,17 @@
+package com.longluo.contest.biweekly_contest_99;
+
+/**
+ * https://leetcode.cn/contest/biweekly-contest-99
+ */
+public class Problem3 {
+
+    public static int countWays(int[][] ranges) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("2 ?= " + countWays(new int[]{1, 2, 3, 4}, 0));
+//        System.out.println("7 ?= " + countWays(new int[]{1, 1, 2, 2, 3, 3, 5}, 2));
+    }
+}
